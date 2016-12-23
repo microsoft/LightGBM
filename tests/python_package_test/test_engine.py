f588ddd5ac59223fc7ea28d105bff2ede6423b02
wxchan <chenwenxuan722@hotmail.com>
2016-12-23 11:44:51 +0800

    use unittest

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -1,77 +1,105 @@
 # coding: utf-8
-# pylint: disable = invalid-name, C0111
-import json
+# pylint: skip-file
+import os, unittest, math
+import numpy as np
 import lightgbm as lgb
-import pandas as pd
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import log_loss, mean_squared_error, mean_absolute_error
+from sklearn.datasets import load_breast_cancer, load_boston, load_digits, load_iris
+from sklearn.model_selection import train_test_split
 
-# load or create your dataset
-df_train = pd.read_csv('../../examples/regression/regression.train', header=None, sep='\t')
-df_test = pd.read_csv('../../examples/regression/regression.test', header=None, sep='\t')
+def test_module(params = {'objective' : 'regression', 'metric' : 'l2'},
+                X_y=load_boston(True), feval=mean_squared_error,
+                stratify=None, num_round=100, return_data=False,
+                return_model=False, init_model=None, custom_eval=None):
+    X, y = X_y
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,
+                                                        stratify=stratify,
+                                                        random_state=42)
+    lgb_train = lgb.Dataset(X_train, y_train, free_raw_data=not return_model)
+    lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train, free_raw_data=not return_model)
+    if return_data: return lgb_train, lgb_eval
+    evals_result = {}
+    params['verbose'] = 0
+    gbm = lgb.train(params, lgb_train,
+                    num_boost_round=num_round,
+                    valid_sets=lgb_eval,
+                    valid_names='eval',
+                    feval=custom_eval,
+                    evals_result=evals_result,
+                    early_stopping_rounds=10,
+                    init_model=init_model)
+    if return_model: return gbm
+    else: return evals_result, feval(y_test, gbm.predict(X_test, gbm.best_iteration))
 
-y_train = df_train[0]
-y_test = df_test[0]
-X_train = df_train.drop(0, axis=1)
-X_test = df_test.drop(0, axis=1)
+class TestBasic(unittest.TestCase):
 
-# create dataset for lightgbm
-lgb_train = lgb.Dataset(X_train, y_train, free_raw_data=False)
-lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train,free_raw_data=False)
+    def test_binary(self):
+        X_y= load_breast_cancer(True)
+        params = {
+            'objective' : 'binary',
+            'metric' : 'binary_logloss'
+        }
+        evals_result, ret = test_module(params, X_y, log_loss, stratify=X_y[1])
+        self.assertLess(ret, 0.15)
+        self.assertAlmostEqual(min(evals_result['eval']['logloss']), ret, places=5)
+    
+    def test_regreesion(self):
+        evals_result, ret = test_module()
+        ret **= 0.5
+        self.assertLess(ret, 4)
+        self.assertAlmostEqual(min(evals_result['eval']['l2']), ret, places=5)
 
+    def test_multiclass(self):
+        X_y = load_digits(10, True)
+        params = {
+            'objective' : 'multiclass',
+            'metric' : 'multi_logloss',
+            'num_class' : 10
+        }
+        def multi_logloss(y_true, y_pred):
+            return np.mean([-math.log(y_pred[i][y]) for i, y in enumerate(y_true)])
+        evals_result, ret = test_module(params, X_y, multi_logloss, stratify=X_y[1])
+        self.assertLess(ret, 0.2)
+        self.assertAlmostEqual(min(evals_result['eval']['multi_logloss']), ret, places=5)
 
-# specify your configurations as a dict
-params = {
-    'task' : 'train',
-    'boosting_type' : 'gbdt',
-    'objective' : 'regression',
-    'metric' : {'l2', 'auc'},
-    'num_leaves' : 31,
-    'learning_rate' : 0.05,
-    'feature_fraction' : 0.9,
-    'bagging_fraction' : 0.8,
-    'bagging_freq': 5,
-    'verbose' : 0
-}
+    def test_continue_train_and_other(self):
+        params = {
+            'objective' : 'regression',
+            'metric' : 'l1'
+        }
+        model_name = 'model.txt'
+        gbm = test_module(params, num_round=20, return_model=True)
+        gbm.save_model(model_name)
+        evals_result, ret = test_module(params, feval=mean_absolute_error,
+                                        num_round=80, init_model=model_name,
+                                        custom_eval=(lambda p, d: ('mae', mean_absolute_error(p, d.get_label()), False)))
+        self.assertLess(ret, 3)
+        self.assertAlmostEqual(min(evals_result['eval']['l1']), ret, places=5)
+        for l1, mae in zip(evals_result['eval']['l1'], evals_result['eval']['mae']):
+            self.assertAlmostEqual(l1, mae, places=5)
+        self.assertIn('tree_info', gbm.dump_model())
+        self.assertIsInstance(gbm.feature_importance(), np.ndarray)
+        os.remove(model_name)
 
-# train
-init_gbm = lgb.train(params,
-                lgb_train,
-                num_boost_round=5,
-                valid_sets=lgb_eval)
+    @unittest.skip("Can't pass yet")
+    def test_continue_train_multiclass(self):
+        X_y = load_iris(True)
+        params = {
+            'objective' : 'multiclass',
+            'metric' : 'multi_error',
+            'num_class' : 3
+        }
+        gbm, lgb_train, lgb_eval = test_module(params, X_y, num_round=20, return_model=True, stratify=X_y[1])
+        gbm.save_model('model.txt')
+        gbm = lgb.train(params, lgb_train,
+                        num_boost_round=80,
+                        valid_sets=lgb_eval,
+                        init_model=gbm,
+                        early_stopping_rounds=10)
 
-print('Start continue train')
+    def test_cv(self):
+        lgb_train, lgb_eval = test_module(return_data=True)
+        lgb.cv({'verbose':0}, lgb_train, num_boost_round=200, nfold=5,
+                metrics='l1', verbose_eval=True)
 
-gbm = lgb.train(params,
-                lgb_train,
-                num_boost_round=100,
-                valid_sets=lgb_eval,
-                early_stopping_rounds=10,
-                init_model=init_gbm)
-
-
-# save model to file
-gbm.save_model('model.txt')
-
-# predict
-y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
-# eval
-print('The rmse of prediction is:', mean_squared_error(y_test, y_pred) ** 0.5)
-
-# dump model to json (and save to file)
-model_json = gbm.dump_model()
-
-with open('model.json', 'w+') as f:
-    json.dump(model_json, f, indent=4)
-
-# feature importances
-print('Feature importances:', gbm.feature_importance())
-print('Feature importances:', gbm.feature_importance("gain"))
-
-print('Start test cv')
-
-lgb.cv(params,
-        lgb_train,
-        num_boost_round=100,
-        nfold=5,
-        verbose_eval=5,
-        init_model=init_gbm)
+unittest.main()
