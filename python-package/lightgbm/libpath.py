b5e042d9d1392aec523b82585526e16ab4800fff
Guolin Ke <i@yumumu.me>
2017-06-04 00:12:35 +0800

    change the dll search order in windows (cmake-build first)

--- python-package/lightgbm/libpath.py
+++ python-package/lightgbm/libpath.py
@@ -21,9 +21,8 @@ def find_lib_path():
                 os.path.join(curr_path, './lib/'),
                 os.path.join(sys.prefix, 'lightgbm')]
     if os.name == 'nt':
-        dll_path.append(os.path.join(curr_path, '../../windows/x64/Dll/'))
-        dll_path.append(os.path.join(curr_path, './windows/x64/Dll/'))
         dll_path.append(os.path.join(curr_path, '../../Release/'))
+        dll_path.append(os.path.join(curr_path, '../../windows/x64/DLL/'))
         dll_path = [os.path.join(p, 'lib_lightgbm.dll') for p in dll_path]
     else:
         dll_path = [os.path.join(p, 'lib_lightgbm.so') for p in dll_path]
