b3bdf8a70eab48966a05a734aa8024276451e61f
wxchan <chenwenxuan722@hotmail.com>
2017-06-22 15:28:52 +0800

    add if-else task

--- .travis/test.sh
+++ .travis/test.sh
@@ -25,8 +25,7 @@ fi
 conda install --yes numpy scipy scikit-learn pandas matplotlib
 pip install pytest
 
-if [[ $TRAVIS_OS_NAME == "linux" ]]; then 
-    conda install --yes atlas
+if [[ ${TASK} == "gpu" ]]; then 
     conda install --yes -c conda-forge boost=1.63.0
 fi
 
@@ -40,16 +39,23 @@ fi
 
 mkdir build && cd build
 
-if [[ ${TASK} == "regular" ]]; then
-    cmake ..
-elif [[ ${TASK} == "mpi" ]]; then
+if [[ ${TASK} == "mpi" ]]; then
     cmake -DUSE_MPI=ON ..
 elif [[ ${TASK} == "gpu" ]]; then
     cmake -DUSE_GPU=ON -DBOOST_ROOT="$HOME/miniconda/" -DOpenCL_INCLUDE_DIR=$AMDAPPSDK/include/ ..
     sed -i 's/std::string device_type = "cpu";/std::string device_type = "gpu";/' ../include/LightGBM/config.h
+else
+    cmake ..
 fi
 
 make _lightgbm || exit -1
+
+if [[ ${TASK} == "if-else" ]]; then
+    cd $TRAVIS_BUILD_DIR/tests/cpp_test && ../../lightgbm config=train.conf && ../../lightgbm config=predict.conf output_result=origin.pred || exit -1
+    cd $TRAVIS_BUILD_DIR/build && make lightgbm || exit -1
+    cd $TRAVIS_BUILD_DIR/tests/cpp_test && ../../lightgbm config=predict.conf output_result=ifelse.pred && python test.py || exit -1
+    exit 0
+fi
 
 cd $TRAVIS_BUILD_DIR/python-package && python setup.py install --precompile || exit -1
 cd $TRAVIS_BUILD_DIR && pytest . || exit -1
