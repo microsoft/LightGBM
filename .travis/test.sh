5f8786949cf1ff3e66992068f4e17c4fa37d0a9e
wxchan <chenwenxuan722@hotmail.com>
2017-06-30 21:17:56 +0800

    clean codes

--- .travis/test.sh
+++ .travis/test.sh
@@ -16,50 +16,49 @@ fi
 conda install --yes numpy scipy scikit-learn pandas matplotlib
 pip install pytest
 
-if [[ ${TASK} == "sdist" ]]; then
-    LGB_VER=$(head -n 1 VERSION.txt)
-    cd $TRAVIS_BUILD_DIR/python-package && python setup.py sdist || exit -1
-    cd $TRAVIS_BUILD_DIR/python-package/dist && pip install lightgbm-$LGB_VER.tar.gz -v || exit -1
-    cd $TRAVIS_BUILD_DIR && pytest tests/python_package_test || exit -1
-    exit 0
-elif [[ ${TASK} == "bdist" ]]; then
-    LGB_VER=$(head -n 1 VERSION.txt)
-    if [[ $TRAVIS_OS_NAME == "osx" ]]; then
-        cd $TRAVIS_BUILD_DIR/python-package && python setup.py bdist_wheel --plat-name=macosx --universal || exit -1
-        mv dist/lightgbm-${LGB_VER}-py2.py3-none-macosx.whl dist/lightgbm-${LGB_VER}-py2.py3-none-macosx_10_9_x86_64.macosx_10_10_x86_64.macosx_10_11_x86_64.macosx_10_12_x86_64.whl
-    else
-        cd $TRAVIS_BUILD_DIR/python-package && python setup.py bdist_wheel --plat-name=manylinux1_x86_64 --universal || exit -1
+# set up gpu environment
+if [[ ${TASK} == "gpu" ]]; then
+    if [[ $TRAVIS_OS_NAME == "linux" ]]; then
+        bash .travis/amd_sdk.sh;
+        tar -xjf AMD-SDK.tar.bz2;
+        AMDAPPSDK=${HOME}/AMDAPPSDK;
+        export OPENCL_VENDOR_PATH=${AMDAPPSDK}/etc/OpenCL/vendors;
+        mkdir -p ${OPENCL_VENDOR_PATH};
+        sh AMD-APP-SDK*.sh --tar -xf -C ${AMDAPPSDK};
+        echo libamdocl64.so > ${OPENCL_VENDOR_PATH}/amdocl64.icd;
+        export LD_LIBRARY_PATH=${AMDAPPSDK}/lib/x86_64:${LD_LIBRARY_PATH};
+        chmod +x ${AMDAPPSDK}/bin/x86_64/clinfo;
+        ${AMDAPPSDK}/bin/x86_64/clinfo;
+        export LIBRARY_PATH="$HOME/miniconda/lib:$LIBRARY_PATH"
+        export LD_RUN_PATH="$HOME/miniconda/lib:$LD_RUN_PATH"
+        export CPLUS_INCLUDE_PATH="$HOME/miniconda/include:$AMDAPPSDK/include/:$CPLUS_INCLUDE_PATH"
+        export PATH="$AMDAPPSDK/include/:$PATH"
+        conda install --yes -c conda-forge boost=1.63.0
     fi
-    cd $TRAVIS_BUILD_DIR/python-package && pip install dist/*.whl || exit -1
-    cd $TRAVIS_BUILD_DIR && pytest tests/python_package_test || exit -1
-    exit 0
-fi
-
-if [[ $TRAVIS_OS_NAME == "linux" ]] && [[ ${TASK} == "gpu" ]]; then
-    bash .travis/amd_sdk.sh;
-    tar -xjf AMD-SDK.tar.bz2;
-    AMDAPPSDK=${HOME}/AMDAPPSDK;
-    export OPENCL_VENDOR_PATH=${AMDAPPSDK}/etc/OpenCL/vendors;
-    mkdir -p ${OPENCL_VENDOR_PATH};
-    sh AMD-APP-SDK*.sh --tar -xf -C ${AMDAPPSDK};
-    echo libamdocl64.so > ${OPENCL_VENDOR_PATH}/amdocl64.icd;
-    export LD_LIBRARY_PATH=${AMDAPPSDK}/lib/x86_64:${LD_LIBRARY_PATH};
-    chmod +x ${AMDAPPSDK}/bin/x86_64/clinfo;
-    ${AMDAPPSDK}/bin/x86_64/clinfo;
-    export LIBRARY_PATH="$HOME/miniconda/lib:$LIBRARY_PATH"
-    export LD_RUN_PATH="$HOME/miniconda/lib:$LD_RUN_PATH"
-    export CPLUS_INCLUDE_PATH="$HOME/miniconda/include:$AMDAPPSDK/include/:$CPLUS_INCLUDE_PATH"
-    export PATH="$AMDAPPSDK/include/:$PATH"
-    conda install --yes -c conda-forge boost=1.63.0
-fi
-
-if [[ ${METHOD} == "sdist" ]]; then
     export BOOST_ROOT="$HOME/miniconda/"
+    sed -i 's/std::string device_type = "cpu";/std::string device_type = "gpu";/' $TRAVIS_BUILD_DIR/include/LightGBM/config.h
+fi
+
+if [[ ${METHOD} == "dist" ]]; then
     LGB_VER=$(head -n 1 VERSION.txt)
-    sed -i 's/std::string device_type = "cpu";/std::string device_type = "gpu";/' ../include/LightGBM/config.h
-    cd $TRAVIS_BUILD_DIR/python-package && python setup.py sdist || exit -1
-    cd $TRAVIS_BUILD_DIR/python-package/dist && pip install lightgbm-$LGB_VER.tar.gz -v --install-option=--gpu || exit -1
-    cd $TRAVIS_BUILD_DIR && pytest tests/python_package_test || exit -1
+    cd $TRAVIS_BUILD_DIR/python-package
+    if [[ ${DIST} == "binary" ]]; then
+        if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+            python setup.py bdist_wheel --plat-name=macosx --universal || exit -1
+            mv dist/lightgbm-${LGB_VER}-py2.py3-none-macosx.whl dist/lightgbm-${LGB_VER}-py2.py3-none-macosx_10_9_x86_64.macosx_10_10_x86_64.macosx_10_11_x86_64.macosx_10_12_x86_64.whl
+        else
+            python setup.py bdist_wheel --plat-name=manylinux1_x86_64 --universal || exit -1
+        fi
+        pip install dist/*.whl || exit -1
+    elif [[ ${DIST} == "source" ]]; then
+        python setup.py sdist || exit -1
+        if [[ ${TASK} == "gpu" ]]; then 
+            pip install dist/lightgbm-$LGB_VER.tar.gz -v --install-option=--gpu || exit -1
+        else
+            pip install dist/lightgbm-$LGB_VER.tar.gz -v || exit -1
+        fi
+    fi
+    pytest $TRAVIS_BUILD_DIR/tests/python_package_test || exit -1
     exit 0
 fi
 
@@ -69,7 +68,6 @@ if [[ ${TASK} == "mpi" ]]; then
     cmake -DUSE_MPI=ON ..
 elif [[ ${TASK} == "gpu" ]]; then
     cmake -DUSE_GPU=ON -DBOOST_ROOT="$HOME/miniconda/" ..
-    sed -i 's/std::string device_type = "cpu";/std::string device_type = "gpu";/' ../include/LightGBM/config.h
 else
     cmake ..
 fi
