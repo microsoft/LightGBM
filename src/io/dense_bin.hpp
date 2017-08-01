9f67ae354352fb8453be8a321005e473c573d8aa
Guolin Ke <i@yumumu.me>
2017-08-01 20:39:33 +0800

    fix bugs.

--- src/io/dense_bin.hpp
+++ src/io/dense_bin.hpp
@@ -205,6 +205,10 @@ public:
     data_size_t* default_indices = gt_indices;
     data_size_t* default_count = &gt_count;
     if (missing_type == MissingType::NaN) {
+      if (default_bin <= threshold) {
+        default_indices = lte_indices;
+        default_count = &lte_count;
+      }
       data_size_t* missing_default_indices = gt_indices;
       data_size_t* missing_default_count = &gt_count;
       if (default_left) {
