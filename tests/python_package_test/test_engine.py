27a52cb8f7137ad9f1747b4ff640fd47010d5ee3
jmh <mark@jmhou.com>
2017-07-31 23:33:10 -0400

    added test involving subsets of subsets of lgb.Dataset objects

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -463,7 +463,12 @@ class TestEngine(unittest.TestCase):
         X = np.random.normal(size=n * 2).reshape(n, 2)
         y = np.random.normal(size=n)
         tmp_dat = lgb.Dataset(X, y)
+        # take subsets and train
         tmp_dat_train = tmp_dat.subset(np.arange(int(n * .8)))
         tmp_dat_val = tmp_dat.subset(np.arange(int(n * .8), n))
         params = {'objective': 'regression_l2', 'metric': 'rmse'}
         gbm = lgb.train(params, tmp_dat_train, num_boost_round=20, valid_sets=[tmp_dat_train, tmp_dat_val])
+        # now take subsets of the subsets and train
+        tmp_dat_train2 = tmp_dat_train.subset(np.arange(n * .8 * .9))
+        tmp_dat_val2 = tmp_dat_val.subset(np.arange(n * .2* .9))
+        gbm = lgb.train(params, tmp_dat_train2, num_boost_round=20, valid_sets=[tmp_dat_train2, tmp_dat_val2])
