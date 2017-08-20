cc83cd6757824f1ee161260bc57384470a7a077e
Guolin Ke <i@yumumu.me>
2017-08-20 20:35:04 +0800

    support constant tree (one-leaf tree) (#851)

--- src/boosting/rf.hpp
+++ src/boosting/rf.hpp
@@ -127,8 +127,7 @@ public:
         if (!class_need_train_[cur_tree_id] && models_.size() < static_cast<size_t>(num_tree_per_iteration_)) {
           double output = class_default_output_[cur_tree_id];
           objective_function_->ConvertOutput(&output, &output);
-          new_tree->Split(0, 0, 0, 0, 0,
-                          output, output, 0, 0, -1, MissingType::None, true);
+          new_tree->AsConstantTree(output);
           train_score_updater_->AddScore(output, cur_tree_id);
           for (auto& score_updater : valid_score_updater_) {
             score_updater->AddScore(output, cur_tree_id);
