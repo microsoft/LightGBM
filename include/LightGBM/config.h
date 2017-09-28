cc11525d261c29a0d965cbcbbda0aa884d35b46d
ChenZhiyong <chenzhiyong115@gmail.com>
2017-09-28 12:29:18 +0800

    refine categorical split (#919)
    * refine categorical split
    
    * add test

--- include/LightGBM/config.h
+++ include/LightGBM/config.h
@@ -224,7 +224,12 @@ public:
   int gpu_device_id = -1;
   /*! \brief Set to true to use double precision math on GPU (default using single precision) */
   bool gpu_use_dp = false;
+  int max_cat_group = 64;
+  int min_data_per_group = 10;
   int max_cat_threshold = 256;
+  double cat_smooth_ratio = 0.01;
+  double min_cat_smooth = 5;
+  double max_cat_smooth = 100;
   LIGHTGBM_EXPORT void Set(const std::unordered_map<std::string, std::string>& params) override;
 };
 
@@ -463,8 +468,8 @@ struct ParameterAlias {
       "snapshot_freq", "verbosity", "sparse_threshold", "enable_load_from_binary_file",
       "max_conflict_rate", "poisson_max_delta_step", "gaussian_eta",
       "histogram_pool_size", "output_freq", "is_provide_training_metric", "machine_list_filename",
-      "zero_as_missing", "max_cat_threshold",
-      "init_score_file", "valid_init_score_file", "is_predict_contrib"
+      "zero_as_missing", "init_score_file", "valid_init_score_file", "is_predict_contrib",
+      "max_cat_threshold", "max_cat_group", "cat_smooth_ratio", "min_cat_smooth", "max_cat_smooth", "min_data_per_group"
     });
     std::unordered_map<std::string, std::string> tmp_map;
     for (const auto& pair : *params) {
