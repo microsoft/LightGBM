61b06294a88dc0d4d4ccb24e00b3574bc534c965
StrikerRUS <nekit94-12@hotmail.com>
2017-10-05 02:46:01 +0300

    fixed fake anchors

--- python-package/lightgbm/sklearn.py
+++ python-package/lightgbm/sklearn.py
@@ -196,7 +196,7 @@ class LGBMModel(_LGBMModelBase):
 
             Note
             ----
-            **kwargs is not supported in sklearn, it may cause unexpected issues.
+            \*\*kwargs is not supported in sklearn, it may cause unexpected issues.
 
         Attributes
         ----------
