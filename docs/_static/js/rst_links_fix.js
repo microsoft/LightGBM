7a2d3bf0da19c24a5b125991c750eac64be7a779
StrikerRUS <nekit94-12@hotmail.com>
2017-10-11 12:13:02 +0300

    fixed max-width of the ReadTheDocs theme

--- docs/_static/js/rst_links_fix.js
+++ docs/_static/js/rst_links_fix.js
@@ -1,3 +1,4 @@
 $(function() {
     $('a[href^="./"][href*=".rst"]').attr('href', (i, val) => { return val.replace('.rst', '.html'); });  /* Replace '.rst' with '.html' in all internal links like './[Something].rst[#anchor]' */
+    $('.wy-nav-content').each(function () { this.style.setProperty('max-width', 'none', 'important'); });
 });
