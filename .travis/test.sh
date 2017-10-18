60d34e0cfb7511325aac7aef0cd7a197053923cb
Nikita Titov <nekit94-12@hotmail.com>
2017-10-18 19:03:44 +0300

    added validation of python-package folder

--- .travis/test.sh
+++ .travis/test.sh
@@ -26,9 +26,11 @@ esac
 cd $TRAVIS_BUILD_DIR
 
 if [[ ${TASK} == "check-docs" ]]; then
-    cd docs
     sudo apt-get install linkchecker
     pip install rstcheck sphinx sphinx_rtd_theme  # html5validator
+    cd python-package
+    rstcheck --report warning `find . -type f -name "*.rst"` || exit -1
+    cd ../docs
     rstcheck --report warning --ignore-directives=autoclass,autofunction `find . -type f -name "*.rst"` || exit -1
     make html || exit -1
     find ./_build/html/ -type f -name '*.html' -exec \
