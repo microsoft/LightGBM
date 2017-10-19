983a84f1a0433bdcc88d0926c4dec089ae238dd7
StrikerRUS <nekit94-12@hotmail.com>
2017-10-19 20:31:24 +0300

    docs config cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -130,56 +130,5 @@ html_static_path = ['_static']
 htmlhelp_basename = 'LightGBMdoc'
 
 
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-# latex_documents = [
-#    (master_doc, 'LightGBM.tex', 'LightGBM Documentation',
-#     'Microsoft Corporation', 'manual'),
-# ]
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-# man_pages = [
-#     (master_doc, 'lightgbm', 'LightGBM Documentation',
-#      [author], 1)
-# ]
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-# texinfo_documents = [
-#     (master_doc, 'LightGBM', 'LightGBM Documentation',
-#      author, 'LightGBM', 'One line description of project.',
-#      'Miscellaneous'),
-# ]
-
-
 def setup(app):
     app.add_javascript("js/rst_links_fix.js")
