d110d6fbf08a561a5a59e07906952525d07d1b38
Nikita Titov <nekit94-08@mail.ru>
2017-12-25 19:27:06 +0300

    [python] added class_weight parameter in sklearn wrapper (#1114)
    * added class_weight parameter in sklearn wrapper
    
    * added note about alternative parameters in case of binary classification task

--- python-package/lightgbm/compat.py
+++ python-package/lightgbm/compat.py
@@ -63,6 +63,7 @@ try:
     from sklearn.base import BaseEstimator
     from sklearn.base import RegressorMixin, ClassifierMixin
     from sklearn.preprocessing import LabelEncoder
+    from sklearn.utils.class_weight import compute_sample_weight
     from sklearn.utils.multiclass import check_classification_targets
     from sklearn.utils.validation import check_X_y, check_array, check_consistent_length
     try:
@@ -83,6 +84,7 @@ try:
     _LGBMCheckArray = check_array
     _LGBMCheckConsistentLength = check_consistent_length
     _LGBMCheckClassificationTargets = check_classification_targets
+    _LGBMComputeSampleWeight = compute_sample_weight
 except ImportError:
     SKLEARN_INSTALLED = False
     _LGBMModelBase = object
@@ -96,6 +98,7 @@ except ImportError:
     _LGBMCheckArray = None
     _LGBMCheckConsistentLength = None
     _LGBMCheckClassificationTargets = None
+    _LGBMComputeSampleWeight = None
 
 
 # DeprecationWarning is not shown by default, so let's create our own with higher level
