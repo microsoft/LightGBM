a733766adb90228817f69e9fb3116a18293c9b89
StrikerRUS <nekit94-12@hotmail.com>
2017-12-31 16:35:28 +0300

    boost_root as pip option

--- .travis/test.sh
+++ .travis/test.sh
@@ -87,10 +87,9 @@ if [[ ${TASK} == "gpu" ]]; then
     conda install --yes -c conda-forge boost=1.63.0
     if [[ ${METHOD} == "pip" ]]; then
         export PATH="$AMDAPPSDK/include/:$PATH"
-        export BOOST_ROOT="$HOME/miniconda/envs/test-env/"
         sed -i 's/const std::string kDefaultDevice = "cpu";/const std::string kDefaultDevice = "gpu";/' ../include/LightGBM/config.h
         cd $TRAVIS_BUILD_DIR/python-package && python setup.py sdist || exit -1
-        cd $TRAVIS_BUILD_DIR/python-package/dist && pip install lightgbm-$LGB_VER.tar.gz -v --install-option=--gpu || exit -1
+        cd $TRAVIS_BUILD_DIR/python-package/dist && pip install lightgbm-$LGB_VER.tar.gz -v --install-option=--gpu --install-option="--boost-root=$HOME/miniconda/envs/test-env/" || exit -1
         cd $TRAVIS_BUILD_DIR && pytest tests/python_package_test || exit -1
         exit 0
     fi
