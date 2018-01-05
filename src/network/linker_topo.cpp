15cba6b846b6a9a61c2692077eef57717e4f4eb0
Guolin Ke <i@yumumu.me>
2018-01-05 15:21:42 +0800

    unfinished

--- src/network/linker_topo.cpp
+++ src/network/linker_topo.cpp
@@ -44,13 +44,13 @@ BruckMap BruckMap::Construct(int rank, int num_machines) {
 
 RecursiveHalvingMap::RecursiveHalvingMap() {
   k = 0;
-  need_pairwise = true;
 }
 
-RecursiveHalvingMap::RecursiveHalvingMap(int in_k, bool in_need_pairwise) {
-  need_pairwise = in_need_pairwise;
+RecursiveHalvingMap::RecursiveHalvingMap(int in_k, RecursiveHalvingNodeType _type, bool _is_power_of_2) {
+  type = _type;
   k = in_k;
-  if (!need_pairwise) {
+  is_power_of_2 = _is_power_of_2;
+  if (type != RecursiveHalvingNodeType::Other) {
     for (int i = 0; i < k; ++i) {
       // defalut set as -1
       ranks.push_back(-1);
@@ -75,7 +75,7 @@ RecursiveHalvingMap RecursiveHalvingMap::Construct(int rank, int num_machines) {
   }
 
   if ((1 << k) == num_machines) {
-    RecursiveHalvingMap rec_map(k, false);
+    RecursiveHalvingMap rec_map(k, RecursiveHalvingNodeType::Normal, true);
     // if num_machines = 2^k, don't need to group machines
     for (int i = 0; i < k; ++i) {
       // communication direction, %2 == 0 is positive
@@ -94,7 +94,82 @@ RecursiveHalvingMap RecursiveHalvingMap::Construct(int rank, int num_machines) {
     }
     return rec_map;
   } else {
-    return RecursiveHalvingMap(k, true);
+    // if num_machines != 2^k, need to group machines
+
+    int lower_power_of_2 = 1 << k;
+
+    int rest = num_machines - lower_power_of_2;
+
+    std::vector<RecursiveHalvingNodeType> node_type(num_machines);
+    for (int i = 0; i < num_machines; ++i) {
+      node_type[i] = RecursiveHalvingNodeType::Normal;
+    }
+    // group, two machine in one group, total "rest" groups will have 2 machines.
+    for (int i = 0; i < rest; ++i) {
+      int right = num_machines - i * 2 - 1;
+      int left = num_machines - i * 2 - 2;
+      // let left machine as group leader
+      node_type[left] = RecursiveHalvingNodeType::GroupLeader;
+      node_type[right] = RecursiveHalvingNodeType::Other;
+    }
+    int group_cnt = 0;
+    // cache block information for groups, group with 2 machines will have double block size
+    std::vector<int> group_block_start(lower_power_of_2);
+    std::vector<int> group_block_len(lower_power_of_2, 0);
+    // convert from group to node leader
+    std::vector<int> group_to_node(lower_power_of_2);
+    // convert from node to group
+    std::vector<int> node_to_group(num_machines);
+
+    for (int i = 0; i < num_machines; ++i) {
+      // meet new group
+      if (node_type[i] == RecursiveHalvingNodeType::Normal || node_type[i] == RecursiveHalvingNodeType::GroupLeader) {
+        group_to_node[group_cnt++] = i;
+      }
+      node_to_group[i] = group_cnt - 1;
+      // add block len for this group
+      group_block_len[group_cnt - 1]++;
+    }
+    // calculate the group block start
+    group_block_start[0] = 0;
+    for (int i = 1; i < lower_power_of_2; ++i) {
+      group_block_start[i] = group_block_start[i - 1] + group_block_len[i - 1];
+    }
+
+    RecursiveHalvingMap rec_map(k, node_type[rank], false);
+    if (node_type[rank] == RecursiveHalvingNodeType::Other) {
+      rec_map.neighbor = rank - 1;
+      // not need to construct
+      return rec_map;
+    }
+    if (node_type[rank] == RecursiveHalvingNodeType::GroupLeader) {
+      rec_map.neighbor = rank + 1;
+    }
+    const int cur_group_idx = node_to_group[rank];
+    for (int i = 0; i < k; ++i) {
+      const int dir = ((cur_group_idx / distance[i]) % 2 == 0) ? 1 : -1;
+      const int next_node_idx = group_to_node[(cur_group_idx + dir * distance[i])];
+      rec_map.ranks[i] = next_node_idx;
+      // get receive block informations
+      const int recv_block_start = cur_group_idx / distance[i];
+      rec_map.recv_block_start[i] = group_block_start[recv_block_start * distance[i]];
+      int recv_block_len = 0;
+      // accumulate block len
+      for (int j = 0; j < distance[i]; ++j) {
+        recv_block_len += group_block_len[recv_block_start * distance[i] + j];
+      }
+      rec_map.recv_block_len[i] = recv_block_len;
+      // get send block informations
+      const int send_block_start = (cur_group_idx + dir * distance[i]) / distance[i];
+      rec_map.send_block_start[i] = group_block_start[send_block_start * distance[i]];
+      int send_block_len = 0;
+      // accumulate block len
+      for (int j = 0; j < distance[i]; ++j) {
+        send_block_len += group_block_len[send_block_start * distance[i] + j];
+      }
+      rec_map.send_block_len[i] = send_block_len;
+    }
+    return rec_map;
   }
 }
 
