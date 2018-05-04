cc260fe9b82c5b237ee51fe6eeb9aa1adaadd970
StrikerRUS <nekit94-12@hotmail.com>
2018-05-04 23:41:48 +0300

    attempt to fix macOS builds

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+    rm '/usr/local/include/c++'
+#    brew cask uninstall oclint  #  Reserve variant to deal with conflict link
     if [[ ${TASK} == "mpi" ]]; then
         brew install open-mpi
     else
         brew install gcc
     fi
-    brew link --overwrite gcc
+#    brew link --overwrite gcc  # Previous variant to deal with conflict link
     wget -O conda.sh https://repo.continuum.io/miniconda/Miniconda${PYTHON_VERSION:0:1}-latest-MacOSX-x86_64.sh
 else
     if [[ ${TASK} != "pylint" ]] && [[ ${TASK} != "check-docs" ]]; then
