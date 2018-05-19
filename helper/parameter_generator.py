489c32901a0d8000c888e5b7a92342399e3650aa
Guolin Ke <i@yumumu.me>
2018-05-19 21:28:44 +0800

    fix pylint

--- helper/parameter_generator.py
+++ helper/parameter_generator.py
@@ -21,7 +21,7 @@ def GetParameterInfos(config_hpp="..\\include\\LightGBM\\config.h"):
         elif cur_key is not None:
             line = line.strip()
             if line.startswith("//"):
-                tokens=line.split("//")[1].split("=")
+                tokens = line.split("//")[1].split("=")
                 key = tokens[0].strip()
                 val = '='.join(tokens[1:]).strip()
                 if key not in cur_info:
@@ -41,7 +41,7 @@ def GetParameterInfos(config_hpp="..\\include\\LightGBM\\config.h"):
                 if len(tokens) == 2:
                     if "default" not in cur_info:
                         cur_info["default"] = [tokens[1][:-1].strip()]
-                    has_eqsgn =  True
+                    has_eqsgn = True
                 tokens = line.split()
                 cur_info["inner_type"] = [tokens[0].strip()]
                 if "name" not in cur_info:
@@ -54,12 +54,14 @@ def GetParameterInfos(config_hpp="..\\include\\LightGBM\\config.h"):
     config_hpp_file.close()
     return (keys, member_infos)
 
+
 def GetNames(infos):
     names = []
     for x in infos:
         for y in x:
             names.append(y["name"][0])
     return names
+
 
 def GetAlias(infos):
     pairs = []
@@ -72,24 +74,26 @@ def GetAlias(infos):
                     pairs.append([name2.strip(), name])
     return pairs
 
+
 def SetOneVarFromString(name, type, checks):
     ret = ""
-    univar_mapper = { "int": "GetInt", "double": "GetDouble", "bool": "GetBool", "std::string":"GetString" }
-    if not "vector" in type:
-        ret += "  %s(params, \"%s\", &%s);\n" %(univar_mapper[type], name, name)
+    univar_mapper = {"int": "GetInt", "double": "GetDouble", "bool": "GetBool", "std::string": "GetString"}
+    if "vector" not in type:
+        ret += "  %s(params, \"%s\", &%s);\n" % (univar_mapper[type], name, name)
         if len(checks) > 0:
             for check in checks:
-                ret += "  CHECK(%s %s);\n" %(name, check)
+                ret += "  CHECK(%s %s);\n" % (name, check)
         ret += "\n"
     else:
-        ret += "  if (GetString(params, \"%s\", &tmp_str)) {\n" %(name)
+        ret += "  if (GetString(params, \"%s\", &tmp_str)) {\n" % (name)
         type2 = type.split("<")[1][:-1]
         if type2 == "std::string":
-            ret  += "    %s = Common::Split(tmp_str.c_str(), ',');\n" %(name)
+            ret  += "    %s = Common::Split(tmp_str.c_str(), ',');\n" % (name)
         else:
-            ret += "    %s = Common::StringToArray<%s>(tmp_str, ',');\n" %(name, type2)
+            ret += "    %s = Common::StringToArray<%s>(tmp_str, ',');\n" % (name, type2)
         ret += "  }\n\n"
     return ret
+
 
 def GenParameterCode(config_hpp="..\\include\\LightGBM\\config.h", config_out_cpp="..\\src\\io\\config_auto.cpp"):
     keys, infos = GetParameterInfos(config_hpp)
@@ -99,12 +103,12 @@ def GenParameterCode(config_hpp="..\\include\\LightGBM\\config.h", config_out_cp
     # alias table
     str_to_write += "std::unordered_map<std::string, std::string> Config::alias_table({\n"
     for pair in alias:
-        str_to_write += "  {\"%s\", \"%s\"}, \n" %(pair[0], pair[1])
+        str_to_write += "  {\"%s\", \"%s\"}, \n" % (pair[0], pair[1])
     str_to_write += "});\n\n"
     # names
     str_to_write += "std::unordered_set<std::string> Config::parameter_set({\n"
     for name in names:
-        str_to_write += "  \"%s\", \n" %(name)
+        str_to_write += "  \"%s\", \n" % (name)
     str_to_write += "});\n\n"
     # from strings
     str_to_write += "void Config::GetMembersFromString(const std::unordered_map<std::string, std::string>& params) {\n"
@@ -113,11 +117,11 @@ def GenParameterCode(config_hpp="..\\include\\LightGBM\\config.h", config_out_cp
         for y in x:
             if "[doc-only]" in y:
                 continue
-            type=y["inner_type"][0]
-            name=y["name"][0]
+            type = y["inner_type"][0]
+            name = y["name"][0]
             checks = []
             if "check" in y:
-                checks=y["check"]
+                checks = y["check"]
             tmp = SetOneVarFromString(name, type, checks)
             str_to_write += tmp
     # tails
@@ -128,17 +132,18 @@ def GenParameterCode(config_hpp="..\\include\\LightGBM\\config.h", config_out_cp
         for y in x:
             if "[doc-only]" in y:
                 continue
-            type=y["inner_type"][0]
-            name=y["name"][0]
+            type = y["inner_type"][0]
+            name = y["name"][0]
             if "vector" in type:
-                str_to_write += "  str_buf << \"%s=\" << Common::Join(%s,\",\") << \"\\n\";\n" %(name, name)
+                str_to_write += "  str_buf << \"%s=\" << Common::Join(%s,\",\") << \"\\n\";\n" % (name, name)
             else:
-                str_to_write += "  str_buf << \"%s=\" << %s << \"\\n\";\n" %(name, name)
+                str_to_write += "  str_buf << \"%s=\" << %s << \"\\n\";\n" % (name, name)
     # tails
     str_to_write += "  return str_buf.str();\n"
     str_to_write += "}\n\n"
     str_to_write += "}\n"
     with open(config_out_cpp, "w") as config_out_cpp_file:
         config_out_cpp_file.write(str_to_write)
+
 
 GenParameterCode()
