5619cf64413acb6aa2be8a46375b0e134171ac21
Guolin Ke <guolin.ke@outlook.com>
2018-06-30 08:26:37 +0800

    use conda

--- .vsts-ci/test.sh
+++ .vsts-ci/test.sh
@@ -9,13 +9,13 @@ cd ${BUILD_REPOSITORY_LOCALPATH}
 
 if [[ $TASK == "check-docs" ]]; then
     if [[ ${AGENT_OS} == "linux" ]]; then
-        sudo apt-get install linkchecker
+        sudo apt-get install linkchecker -y
     fi
     if [[ ${PYTHON_VERSION} == "2.7" ]]; then
-        pip install mock
+        conda install -y mock
     fi
-    pip install sphinx "sphinx_rtd_theme>=0.3"  # html5validator
-    pip install rstcheck
+    conda install -y sphinx "sphinx_rtd_theme>=0.3"  # html5validator
+    conda install -y rstcheck
     cd ${BUILD_REPOSITORY_LOCALPATH}/python-package
     rstcheck --report warning `find . -type f -name "*.rst"` || exit -1
     cd ${BUILD_REPOSITORY_LOCALPATH}/docs
@@ -31,13 +31,13 @@ if [[ $TASK == "check-docs" ]]; then
 fi
 
 if [[ $TASK == "pylint" ]]; then
-    pip install pycodestyle
+    conda install -y pycodestyle
     pycodestyle --ignore=E501,W503 --exclude=./compute,./docs,./.nuget . || exit -1
     exit 0
 fi
 
 if [[ $TASK == "if-else" ]]; then
-    pip install numpy
+    conda install -y numpy
     mkdir build && cd build && cmake .. && make lightgbm || exit -1
     cd ${BUILD_REPOSITORY_LOCALPATH}/tests/cpp_test && ../../lightgbm config=train.conf convert_model_language=cpp convert_model=../../src/boosting/gbdt_prediction.cpp && ../../lightgbm config=predict.conf output_result=origin.pred || exit -1
     cd ${BUILD_REPOSITORY_LOCALPATH}/build && make lightgbm || exit -1
@@ -45,11 +45,11 @@ if [[ $TASK == "if-else" ]]; then
     exit 0
 fi
 
-pip install numpy nose scipy scikit-learn pandas matplotlib graphviz pytest
+conda install -y numpy nose scipy scikit-learn pandas matplotlib graphviz pytest
 
 if [[ $TASK == "sdist" ]]; then
     cd ${BUILD_REPOSITORY_LOCALPATH}/python-package && python setup.py sdist || exit -1
-    pip install ${BUILD_REPOSITORY_LOCALPATH}/python-package/dist/lightgbm-$LGB_VER.tar.gz -v || exit -1
+    conda install -y ${BUILD_REPOSITORY_LOCALPATH}/python-package/dist/lightgbm-$LGB_VER.tar.gz -v || exit -1
     pytest ${BUILD_REPOSITORY_LOCALPATH}/tests/python_package_test || exit -1
     exit 0
 elif [[ $TASK == "bdist" ]]; then
@@ -59,18 +59,18 @@ elif [[ $TASK == "bdist" ]]; then
     else
         cd ${BUILD_REPOSITORY_LOCALPATH}/python-package && python setup.py bdist_wheel --plat-name=manylinux1_x86_64 --universal || exit -1
     fi
-    pip install ${BUILD_REPOSITORY_LOCALPATH}/python-package/dist/*.whl || exit -1
+    conda install -y ${BUILD_REPOSITORY_LOCALPATH}/python-package/dist/*.whl || exit -1
     pytest ${BUILD_REPOSITORY_LOCALPATH}/tests/python_package_test || exit -1
     exit 0
 fi
 
 if [[ $TASK == "gpu" ]]; then
-    sudo apt-get install libboost-all-dev
+    conda install --yes -c conda-forge boost
     sed -i 's/std::string device_type = "cpu";/std::string device_type = "gpu";/' ${BUILD_REPOSITORY_LOCALPATH}/include/LightGBM/config.h
     grep -q 'std::string device_type = "gpu"' ${BUILD_REPOSITORY_LOCALPATH}/include/LightGBM/config.h || exit -1  # make sure that changes were really done
     if [[ $METHOD == "pip" ]]; then
         cd ${BUILD_REPOSITORY_LOCALPATH}/python-package && python setup.py sdist || exit -1
-        pip install ${BUILD_REPOSITORY_LOCALPATH}/python-package/dist/lightgbm-$LGB_VER.tar.gz -v --install-option=--gpu --install-option="--opencl-include-dir=$AMDAPPSDK/include/" || exit -1
+        conda install -y ${BUILD_REPOSITORY_LOCALPATH}/python-package/dist/lightgbm-$LGB_VER.tar.gz -v --install-option=--gpu -DBOOST_ROOT=$HOME/miniconda/envs/$CONDA_ENV/ --install-option="--opencl-include-dir=$AMDAPPSDK/include/" || exit -1
         pytest ${BUILD_REPOSITORY_LOCALPATH}/tests/python_package_test || exit -1
         exit 0
     fi
@@ -80,11 +80,11 @@ mkdir build && cd build
 
 if [[ $TASK == "mpi" ]]; then
     cd ${BUILD_REPOSITORY_LOCALPATH}/python-package && python setup.py sdist || exit -1
-    pip install ${BUILD_REPOSITORY_LOCALPATH}/python-package/dist/lightgbm-$LGB_VER.tar.gz -v --install-option=--mpi || exit -1
+    conda install -y ${BUILD_REPOSITORY_LOCALPATH}/python-package/dist/lightgbm-$LGB_VER.tar.gz -v --install-option=--mpi || exit -1
     cd ${BUILD_REPOSITORY_LOCALPATH}/build
     cmake -DUSE_MPI=ON ..
 elif [[ $TASK == "gpu" ]]; then
-    cmake -DUSE_GPU=ON -DOpenCL_INCLUDE_DIR=$AMDAPPSDK/include/ ..
+    cmake -DUSE_GPU=ON -DBOOST_ROOT=$HOME/miniconda/envs/$CONDA_ENV/ -DOpenCL_INCLUDE_DIR=$AMDAPPSDK/include/ ..
 else
     cmake ..
 fi
