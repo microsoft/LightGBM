41eb3c6ae41efca4c7c6be4f751bc6a668ee6a46
James Lamb <jaylamb20@gmail.com>
2018-07-07 17:27:16 -0500

    Fixed issues with documentation

--- R-package/R/lightgbm.R
+++ R-package/R/lightgbm.R
@@ -1,6 +1,51 @@
-#' Simple interface for training an lightgbm model.
-#' Its documentation is combined with lgb.train.
-#'
+
+#' @name lgb_shared_params
+#' @title Shared parameter docs
+#' @description Parameter docs shared by \code{lgb.train}, \code{lgb.cv}, and \code{lightgbm}
+#' @param callbacks list of callback functions
+#'        List of callback functions that are applied at each iteration.
+#' @param data a \code{lgb.Dataset} object, used for training
+#' @param early_stopping_rounds int
+#'        Activates early stopping.
+#'        Requires at least one validation data and one metric
+#'        If there's more than one, will check all of them except the training data
+#'        Returns the model with (best_iter + early_stopping_rounds)
+#'        If early stopping occurs, the model will have 'best_iter' field
+#' @param eval_freq evaluation output frequency, only effect when verbose > 0
+#' @param init_model path of model file of \code{lgb.Booster} object, will continue training from this model
+#' @param nrounds number of training rounds
+#' @param params List of parameters
+#' @param verbose verbosity for output, if <= 0, also will disable the print of evaluation during training
+NULL
+
+
+#' @title Train a LightGBM model
+#' @name lightgbm
+#' @description Simple interface for training an LightGBM model.
+#' @inheritParams lgb_shared_params
+#' @param label Vector of labels, used if \code{data} is not an \code{\link{lgb.Dataset}}
+#' @param weight vector of response values. If not NULL, will set to dataset
+#' @param save_name File name to use when writing the trained model to disk. Should end in ".model".
+#' @param ... Additional arguments passed to \code{\link{lgb.train}}. For example
+#'     \itemize{
+#'        \item{valids}{a list of \code{lgb.Dataset} objects, used for validation}
+#'        \item{obj}{objective function, can be character or custom objective function. Examples include 
+#'                   \code{regression}, \code{regression_l1}, \code{huber},
+#'                    \code{binary}, \code{lambdarank}, \code{multiclass}, \code{multiclass}}
+#'        \item{eval}{evaluation function, can be (a list of) character or custom eval function}
+#'        \item{record}{Boolean, TRUE will record iteration message to \code{booster$record_evals}}
+#'        \item{colnames}{feature names, if not null, will use this to overwrite the names in dataset}
+#'        \item{categorical_feature}{list of str or int. type int represents index, type str represents feature names}
+#'        \item{reset_data}{Boolean, setting it to TRUE (not the default value) will transform the booster model 
+#'                          into a predictor model which frees up memory and the original datasets}
+#'         \item{boosting}{Boosting type. \code{"gbdt"} or \code{"dart"}}
+#'         \item{num_leaves}{number of leaves in one tree. defaults to 127}
+#'         \item{max_depth}{Limit the max depth for tree model. This is used to deal with 
+#'                          overfit when #data is small. Tree still grow by leaf-wise.}
+#'          \item{num_threads}{Number of threads for LightGBM. For the best speed, set this to
+#'                             the number of real CPU cores, not the number of threads (most 
+#'                             CPU using hyper-threading to generate 2 threads per CPU core).}
+#'     }
 #' @export
 lightgbm <- function(data,
                      label = NULL,
