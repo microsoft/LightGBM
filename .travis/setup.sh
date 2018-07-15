16d9b34c749a8d4719c5f345fcceed26286dd804
StrikerRUS <nekit94-12@hotmail.com>
2018-07-15 23:59:45 +0300

    prevent case when CMake is downgraded by libomp

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,8 +2,8 @@
 
 if [[ $TRAVIS_OS_NAME == "osx" ]]; then
     if  [[ $TASK == "clang" ]]; then
-        brew reinstall cmake --verbose --HEAD  # CMake >=3.12 is needed
         brew install libomp
+        brew reinstall cmake --verbose --HEAD  # CMake >=3.12 is needed
     else
         rm '/usr/local/include/c++'
 #        brew cask uninstall oclint  #  reserve variant to deal with conflict link
