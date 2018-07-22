3e3ea44e3c0a0eaa987ac4797d944f14e374a530
StrikerRUS <nekit94-12@hotmail.com>
2018-07-23 00:23:35 +0300

    compile with Clang at Travis on Linux

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,6 +3,9 @@
 if [[ $TRAVIS_OS_NAME == "osx" ]] && [[ $COMPILER == "gcc" ]]; then
     export CXX=g++-8
     export CC=gcc-8
+elif [[ $TRAVIS_OS_NAME == "linux" ]] && [[ $COMPILER == "clang" ]]; then
+    export CXX=clang++
+    export CC=clang
 fi
 
 conda create -q -n test-env python=$PYTHON_VERSION
