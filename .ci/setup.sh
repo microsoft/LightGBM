a1c72b88b4a45fd064d7b68728fad7311dd13675
Nikita Titov <nekit94-08@mail.ru>
2018-10-02 13:11:08 +0300

    make sure that we use appropriate compiler

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -29,10 +29,10 @@ else  # Linux
         sudo apt-get install libomp-dev
     elif [[ $AZURE == "true" ]] && [[ $COMPILER == "gcc" ]] && [[ $TASK != "gpu" ]]; then
         # downgrade gcc version
-        sudo apt-get remove -y gcc
+        sudo apt-get remove -y gcc || exit -1
         sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
         sudo apt-get update
-        sudo apt-get install --no-install-recommends -y g++-4.8
+        sudo apt-get install --no-install-recommends -y g++-4.8 || exit -1
         sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 100
         sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 100
     fi
