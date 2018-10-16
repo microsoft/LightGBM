f3dce7e6e7e0a8566195939fe680b5f6619711ed
Nikita Titov <nekit94-08@mail.ru>
2018-10-16 18:02:50 +0300

    [docs] corrected misleading note about best_iteration (#1758)
    * removed misleading note about best_iteration
    
    * Update engine.py
    
    * Update Python-Intro.rst
    
    * Updated Engine.py
    
    * Updated Python-Intro.rst
    
    * add article 'the best', break huge line and remove excess empty line

--- python-package/lightgbm/engine.py
+++ python-package/lightgbm/engine.py
@@ -66,8 +66,9 @@ def train(params, train_set, num_boost_round=100,
         to continue training.
         Requires at least one validation data and one metric.
         If there's more than one, will check all of them. But the training data is ignored anyway.
-        If early stopping occurs, the model will add ``best_iteration`` field.
-    evals_result : dict or None, optional (default=None)
+        The index of iteration that has the best performance will be saved in the ``best_iteration`` field
+        if early stopping logic is enabled by setting ``early_stopping_rounds``.
+    evals_result: dict or None, optional (default=None)
         This dictionary used to store all evaluation results of all the items in ``valid_sets``.
 
         Example
