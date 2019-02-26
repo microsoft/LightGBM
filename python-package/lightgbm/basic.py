219c943dd7983ee006772cf2cff52a1207bc8002
remcob-gr <39093316+remcob-gr@users.noreply.github.com>
2019-02-26 02:58:54 +0000

    Add ability to move features from one data set to another in memory (#2006)
    * Initial attempt to implement appending features in-memory to another data set
    
    The intent is for this to enable munging files together easily, without needing to round-trip via numpy or write multiple copies to disk.
    In turn, that enables working more efficiently with data sets that were written separately.
    
    * Implement Dataset.dump_text, and fix small bug in appending of group bin boundaries.
    
    Dumping to text enables us to compare results, without having to worry about issues like features being reordered.
    
    * Add basic tests for validation logic for add_features_from.
    
    * Remove various internal mapping items from dataset text dumps
    
    These are too sensitive to the exact feature order chosen, which is not visible to the user.
    Including them in tests appears unnecessary, as the data dumping code should provide enough coverage.
    
    * Add test that add_features_from results in identical data sets according to dump_text.
    
    * Add test that booster behaviour after using add_features_from matches that of training on the full data
    
    This checks:
    - That training after add_features_from works at all
    - That add_features_from does not cause training to misbehave
    
    * Expose feature_penalty and monotone_types/constraints via get_field
    
    These getters allow us to check that add_features_from does the right thing with these vectors.
    
    * Add tests that add_features correctly handles feature_penalty and monotone_constraints.
    
    * Ensure add_features_from properly frees the added dataset and add unit test for this
    
    Since add_features_from moves the feature group pointers from the added dataset to the dataset being added to, the added dataset is invalid after the call.
    We must ensure we do not try and access this handle.
    
    * Remove some obsolete TODOs
    
    * Tidy up DumpTextFile by using a single iterator for each feature
    
    This iterators were also passed around as raw pointers without being freed, which is now fixed.
    
    * Factor out offsetting logic in AddFeaturesFrom
    
    * Remove obsolete TODO
    
    * Remove another TODO
    
    This one is debatable, test code can be a bit messy and duplicate-heavy, factoring it out tends to end badly.
    Leaving this for now, will revisit if adding more tests later on becomes a mess.
    
    * Add documentation for newly-added methods.
    
    * Fix whitespace issues identified by pylint.
    
    * Fix a few more whitespace issues.
    
    * Fix doc comments
    
    * Implement deep copying for feature groups.
    
    * Replace awkward std::move usage by emplace_back, and reduce vector size to num_features rather than num_total_features.
    
    * Copy feature groups in addFeaturesFrom, rather than moving them.
    
    * Fix bugs in FeatureGroup copy constructor and ensure source dataset remains usable
    
    * Add reserve to PushVector and PushOffset
    
    * Move definition of Clone into class body
    
    * Fix PR review issues
    
    * Fix for loop increment style.
    
    * Fix test failure
    
    * Some more docstring fixes.
    
    * Remove blank line

--- python-package/lightgbm/basic.py
+++ python-package/lightgbm/basic.py
@@ -107,6 +107,14 @@ def cint32_array_to_numpy(cptr, length):
         raise RuntimeError('Expected int pointer')
 
 
+def cint8_array_to_numpy(cptr, length):
+    """Convert a ctypes int pointer array to a numpy array."""
+    if isinstance(cptr, ctypes.POINTER(ctypes.c_int8)):
+        return np.fromiter(cptr, dtype=np.int8, count=length)
+    else:
+        raise RuntimeError('Expected int pointer')
+
+
 def c_str(string):
     """Convert a Python string to C string."""
     return ctypes.c_char_p(string.encode('utf-8'))
@@ -166,6 +174,7 @@ C_API_DTYPE_FLOAT32 = 0
 C_API_DTYPE_FLOAT64 = 1
 C_API_DTYPE_INT32 = 2
 C_API_DTYPE_INT64 = 3
+C_API_DTYPE_INT8 = 4
 
 """Matrix is row major in Python"""
 C_API_IS_ROW_MAJOR = 1
@@ -180,7 +189,9 @@ C_API_PREDICT_CONTRIB = 3
 FIELD_TYPE_MAPPER = {"label": C_API_DTYPE_FLOAT32,
                      "weight": C_API_DTYPE_FLOAT32,
                      "init_score": C_API_DTYPE_FLOAT64,
-                     "group": C_API_DTYPE_INT32}
+                     "group": C_API_DTYPE_INT32,
+                     "feature_penalty": C_API_DTYPE_FLOAT64,
+                     "monotone_constraints": C_API_DTYPE_INT8}
 
 PANDAS_DTYPE_MAPPER = {'int8': 'int', 'int16': 'int', 'int32': 'int',
                        'int64': 'int', 'uint8': 'int', 'uint16': 'int',
@@ -700,6 +711,8 @@ class Dataset(object):
         self._predictor = None
         self.pandas_categorical = None
         self.params_back_up = None
+        self.feature_penalty = None
+        self.monotone_constraints = None
 
     def __del__(self):
         try:
@@ -1179,6 +1192,8 @@ class Dataset(object):
             return cfloat32_array_to_numpy(ctypes.cast(ret, ctypes.POINTER(ctypes.c_float)), tmp_out_len.value)
         elif out_type.value == C_API_DTYPE_FLOAT64:
             return cfloat64_array_to_numpy(ctypes.cast(ret, ctypes.POINTER(ctypes.c_double)), tmp_out_len.value)
+        elif out_type.value == C_API_DTYPE_INT8:
+            return cint8_array_to_numpy(ctypes.cast(ret, ctypes.POINTER(ctypes.c_int8)), tmp_out_len.value)
         else:
             raise TypeError("Unknown type")
 
@@ -1382,6 +1397,30 @@ class Dataset(object):
             self.weight = self.get_field('weight')
         return self.weight
 
+    def get_feature_penalty(self):
+        """Get the feature penalty of the Dataset.
+
+        Returns
+        -------
+        feature_penalty : numpy array or None
+            Feature penalty for each feature in the Dataset.
+        """
+        if self.feature_penalty is None:
+            self.feature_penalty = self.get_field('feature_penalty')
+        return self.feature_penalty
+
+    def get_monotone_constraints(self):
+        """Get the monotone constraints of the Dataset.
+
+        Returns
+        -------
+        monotone_constraints : numpy array or None
+            Monotone constraints: -1, 0 or 1, for each feature in the Dataset.
+        """
+        if self.monotone_constraints is None:
+            self.monotone_constraints = self.get_field('monotone_constraints')
+        return self.monotone_constraints
+
     def get_init_score(self):
         """Get the initial score of the Dataset.
 
@@ -1493,6 +1532,46 @@ class Dataset(object):
             else:
                 break
         return ref_chain
+
+    def add_features_from(self, other):
+        """Add features from other Dataset to the current Dataset.
+
+        Both Datasets must be constructed before calling this method.
+
+        Parameters
+        ----------
+        other : Dataset
+            The Dataset to take features from.
+
+        Returns
+        -------
+        self : Dataset
+            Dataset with the new features added.
+        """
+        if self.handle is None or other.handle is None:
+            raise ValueError('Both source and target Datasets must be constructed before adding features')
+        _safe_call(_LIB.LGBM_DatasetAddFeaturesFrom(self.handle, other.handle))
+        return self
+
+    def dump_text(self, filename):
+        """Save Dataset to a text file.
+
+        This format cannot be loaded back in by LightGBM, but is useful for debugging purposes.
+
+        Parameters
+        ----------
+        filename : string
+            Name of the output file.
+
+        Returns
+        -------
+        self : Dataset
+            Returns self.
+        """
+        _safe_call(_LIB.LGBM_DatasetDumpText(
+            self.construct().handle,
+            c_str(filename)))
+        return self
 
 
 class Booster(object):
