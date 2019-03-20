7a89d0054bcf0db9f96af3c53a078b6aa3152efe
Nikita Titov <nekit94-08@mail.ru>
2019-03-20 09:18:35 +0300

    removed Python 3.4 support (#2051)

--- python-package/setup.py
+++ python-package/setup.py
@@ -331,7 +331,6 @@ if __name__ == "__main__":
                        'Programming Language :: Python :: 2',
                        'Programming Language :: Python :: 2.7',
                        'Programming Language :: Python :: 3',
-                       'Programming Language :: Python :: 3.4',
                        'Programming Language :: Python :: 3.5',
                        'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7',
