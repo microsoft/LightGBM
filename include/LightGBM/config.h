f01b2aca13e787e2945eba7c2bd85e31c65923f8
Guolin Ke <guolin.ke@outlook.com>
2019-05-16 09:47:49 +0800

    first metric only in earlystopping for cli (#2172)
    * first metric only in earlystopping for cli
    
    * code clean
    
    * added note about CLI only usage
    
    * removed note about CLI only usage

--- include/LightGBM/config.h
+++ include/LightGBM/config.h
@@ -260,6 +260,9 @@ struct Config {
   // desc = ``<= 0`` means disable
   int early_stopping_round = 0;
 
+  // desc = set this to ``true``, if you want to use only the first metric for early stopping
+  bool first_metric_only = false;
+
   // alias = max_tree_output, max_leaf_output
   // desc = used to limit the max output of tree leaves
   // desc = ``<= 0`` means no constraint
