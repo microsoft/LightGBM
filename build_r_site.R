05404c022f73459924b9a7d43788a1d2c4854270
StrikerRUS <nekit94-12@hotmail.com>
2019-05-19 19:06:48 +0300

    made one line - one argument in build_reference() call

--- build_r_site.R
+++ build_r_site.R
@@ -9,6 +9,9 @@ devtools::document()
 clean_site()
 init_site()
 build_home(preview = FALSE, quiet = FALSE)
-build_reference(lazy = FALSE, document = FALSE,
-                examples = TRUE, run_dont_run = FALSE,
-                seed = 42, preview = FALSE)
+build_reference(lazy = FALSE,
+                document = FALSE,
+                examples = TRUE,
+                run_dont_run = FALSE,
+                seed = 42,
+                preview = FALSE)
