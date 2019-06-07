9387197dc16418c4591ee7fdabfff8f22c067601
Kenichi Matsui <matsuken92@gmail.com>
2019-06-07 12:41:51 +0900

    Simplifying if condition.

--- python-package/lightgbm/callback.py
+++ python-package/lightgbm/callback.py
@@ -8,7 +8,6 @@ import warnings
 from operator import gt, lt
 
 from .compat import range_
-from .basic import LightGBMError
 
 
 class EarlyStopException(Exception):
@@ -228,10 +227,9 @@ def early_stopping(stopping_rounds, first_metric_only=False, verbose=True):
             metric_key = env.evaluation_result_list[i][1]
             if metric_key.split(" ")[0] == "train":
                 continue  # train metric doesn't used on early stopping.
-            if first_metric_only:
-                if eval_metric is not None:
-                    if metric_key != "valid {}".format(eval_metric) and metric_key != eval_metric and eval_metric != "":
-                        continue
+            if first_metric_only and eval_metric is not None and metric_key != "valid {}".format(eval_metric) and \
+                    metric_key != eval_metric and eval_metric != "":
+                continue
             score = env.evaluation_result_list[i][2]
             if best_score_list[i] is None or cmp_op[i](score, best_score[i]):
                 best_score[i] = score
