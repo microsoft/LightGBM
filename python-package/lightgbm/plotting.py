7e1f705bbf414c7af067e29d35ce74b70d96de63
Charles Auguste <auguste@dubquantdev801.ire.susq.com>
2019-06-24 14:37:50 +0100

    Important information is now in bold.

--- python-package/lightgbm/plotting.py
+++ python-package/lightgbm/plotting.py
@@ -385,17 +385,17 @@ def _to_graphviz(tree_info, show_info, feature_names, precision=None, constraint
         if 'split_index' in root:  # non-leaf
             name = 'split{0}'.format(root['split_index'])
             if feature_names is not None:
-                label = '{0} = '.format(feature_names[root['split_feature']])
+                label = '<B>{0}</B> = '.format(feature_names[root['split_feature']])
             else:
-                label = '{0} = '.format(root['split_feature'])
+                label = '<B>{0}</B> = '.format(root['split_feature'])
             label += r'{0}'.format(_float2str(root['threshold'], precision))
             for info in show_info:
                 if info in {'split_gain', 'internal_value', 'internal_weight'}:
-                    label += r'\n{0}: {1}'.format(info, _float2str(root[info], precision))
+                    label += r'<br/>{0}: {1}'.format(info, _float2str(root[info], precision))
                 elif info == 'internal_count':
-                    label += r'\n{0}: {1}'.format(info, root[info])
+                    label += r'<br/>{0}: {1}'.format(info, root[info])
                 elif info == "data_percentage":
-                    label += r'\n{0}% of data'.format(_float2str(root['internal_count'] / total_count * 100, 2))
+                    label += r'<br/>{0}% of data'.format(_float2str(root['internal_count'] / total_count * 100, 2))
 
             fillcolor = "white"
             style = ""
@@ -406,7 +406,7 @@ def _to_graphviz(tree_info, show_info, feature_names, precision=None, constraint
                 if constraints[feature_names[root['split_feature']]] == -1:
                     fillcolor = "#ffdddd"
                     style = "filled" + style
-
+            label = "<" + label + ">"
             graph.node(name, label=label, shape="rectangle", style=style, fillcolor=fillcolor)
             if root['decision_type'] == '<=':
                 l_dec, r_dec = '&#8804;', '>'
@@ -419,11 +419,12 @@ def _to_graphviz(tree_info, show_info, feature_names, precision=None, constraint
         else:  # leaf
             name = 'leaf{0}'.format(root['leaf_index'])
             label = 'leaf {0}: '.format(root['leaf_index'])
-            label += r'{0}'.format(_float2str(root['leaf_value'], precision))
+            label += r'<B>{0}</B>'.format(_float2str(root['leaf_value'], precision))
             if 'leaf_count' in show_info:
-                label += r'\nleaf_count: {0}'.format(root['leaf_count'])
+                label += r'<br/>leaf_count: {0}'.format(root['leaf_count'])
             if 'leaf_weight' in show_info:
-                label += r'\nleaf_weight: {0}'.format(_float2str(root['leaf_weight'], precision))
+                label += r'<br/>leaf_weight: {0}'.format(_float2str(root['leaf_weight'], precision))
+            label = "<" + label + ">"
             graph.node(name, label=label)
         if parent is not None:
             graph.edge(parent, name, decision)
