4f7ec679cb9ebadd4523c5aebd1b3490d55e2fd8
Charles Auguste <auguste@dubquantdev801.ire.susq.com>
2019-06-28 12:04:18 +0100

    Increased the readability for the gain.

--- python-package/lightgbm/plotting.py
+++ python-package/lightgbm/plotting.py
@@ -396,7 +396,7 @@ def _to_graphviz(tree_info, show_info, feature_names, precision=None, constraint
                         output = "gain"
                     elif info == "internal_value":
                         output = "value"
-                    label += r'<br/>{0}: {1}'.format(output, _float2str(root[info], precision))
+                    label += r'<br/>{1} {0}'.format(output, _float2str(root[info], precision))
                 elif info == 'internal_count':
                     label += r'<br/>{0}: {1}'.format(info, root[info])
                 elif info == "data_percentage":
