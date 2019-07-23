bed2de1da3c30f4abdd43e821319621e8659cddc
StrikerRUS <nekit94-12@hotmail.com>
2019-07-23 23:59:59 +0300

    removed temp fix for conda

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -57,4 +57,4 @@ if [[ $TRAVIS == "true" ]] || [[ $OS_NAME == "macos" ]]; then
     sh conda.sh -b -p $CONDA
 fi
 conda config --set always_yes yes --set changeps1 no
-conda install -q -y conda=4.6.14  # temp fix, change to update later
+conda update -q -y conda
