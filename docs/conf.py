c23eea3f915d238f54da8bce58148e563d2b9aef
StrikerRUS <nekit94-12@hotmail.com>
2019-07-31 03:56:05 +0300

    limit number of files to scan for autosummary generation

--- docs/conf.py
+++ docs/conf.py
@@ -81,11 +81,11 @@ autodoc_default_options = {
     "show-inheritance": True,
 }
 
+# Generate autosummary pages. Output should be set with: `:toctree: pythonapi/`
+autosummary_generate = ['Python-API.rst']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# Generate autosummary pages. Output should be set with: `:toctree: pythonapi/`
-autosummary_generate = True
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
