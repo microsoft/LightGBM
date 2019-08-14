93d92ebc94a2cb1a9ced2a175945f01eecb9f8ae
btrotta <btrotta@users.noreply.github.com>
2019-08-14 20:10:21 +1000

    Fix style issues.

--- src/io/dataset_loader.cpp
+++ src/io/dataset_loader.cpp
@@ -2,6 +2,7 @@
  * Copyright (c) 2016 Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE file in the project root for license information.
  */
+
 #include <LightGBM/dataset_loader.h>
 #include <LightGBM/network.h>
 #include <LightGBM/utils/array_args.h>
