ff2b5bd31cd1b0b9f453d44cbef0264ba96aa176
Guolin Ke <guolin.ke@outlook.com>
2019-08-20 17:10:43 +0800

    Update test_sklearn.py

--- tests/python_package_test/test_sklearn.py
+++ tests/python_package_test/test_sklearn.py
@@ -82,8 +82,8 @@ class TestSklearn(unittest.TestCase):
                 eval_group=[q_test], eval_at=[1, 3], early_stopping_rounds=10, verbose=False,
                 callbacks=[lgb.reset_parameter(learning_rate=lambda x: max(0.01, 0.1 - 0.01 * x))])
         self.assertLessEqual(gbm.best_iteration_, 25)
-        self.assertGreater(gbm.best_score_['valid_0']['ndcg@1'], 0.6173)
-        self.assertGreater(gbm.best_score_['valid_0']['ndcg@3'], 0.6479)
+        self.assertGreater(gbm.best_score_['valid_0']['ndcg@1'], 0.60)
+        self.assertGreater(gbm.best_score_['valid_0']['ndcg@3'], 0.60)
 
     def test_regression_with_custom_objective(self):
         def objective_ls(y_true, y_pred):
