1b6a6b449656ec87da45251b1f59e11a8f5fffa0
James Lamb <jaylamb20@gmail.com>
2019-10-25 08:07:13 -0700

    Update R-package/R/aliases.R
    Co-Authored-By: Nikita Titov <nekit94-08@mail.ru>

--- R-package/R/aliases.R
+++ R-package/R/aliases.R
@@ -3,7 +3,7 @@
 
 # [description] List of respected parameter aliases. Wrapped in a function to take advantage of
 #               lazy evaluation (so it doesn't matter what order R sources files during installation).
-# [return] A named list, where each key is a main LightGBM parameter and  each value is a character
+# [return] A named list, where each key is a main LightGBM parameter and each value is a character
 #          vector of corresponding aliases.
 .PARAMETER_ALIASES <- function(){
     return(list(
