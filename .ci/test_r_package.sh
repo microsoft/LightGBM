7285448e10bfbee18b56ba452d9ad8fb09aab090
James Lamb <jaylamb20@gmail.com>
2019-11-11 20:40:22 -0800

    add update back in

--- .ci/test_r_package.sh
+++ .ci/test_r_package.sh
@@ -17,6 +17,7 @@ if [[ $OS_NAME == "linux" ]]; then
     sudo apt-key adv \
         --keyserver keyserver.ubuntu.com \
         --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+    sudo apt-get update
     sudo apt-get install \
         --no-install-recommends \
         -y \
