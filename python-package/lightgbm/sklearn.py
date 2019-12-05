148fc4fb82222dfcd23dbed2ec110db7113ae742
StrikerRUS <nekit94-12@hotmail.com>
2019-12-05 17:59:06 +0300

    simplified condition

--- python-package/lightgbm/sklearn.py
+++ python-package/lightgbm/sklearn.py
@@ -589,7 +589,7 @@ class LGBMModel(_LGBMModelBase):
                                                    valid_weight, valid_init_score, valid_group, params)
                 valid_sets.append(valid_set)
 
-        if init_model is not None and isinstance(init_model, LGBMModel):
+        if isinstance(init_model, LGBMModel):
             init_model = init_model.booster_
 
         self._Booster = train(params, train_set,
