d100277649ecbd40ed7b9b35ecf7a0df86ddc10a
Nikita Titov <nekit94-08@mail.ru>
2019-12-19 14:00:29 +0300

    specify the last supported version of scikit-learn (#2637)

--- .ci/test.sh
+++ .ci/test.sh
@@ -83,7 +83,7 @@ if [[ $TASK == "r-package" ]]; then
     exit 0
 fi
 
-conda install -q -y -n $CONDA_ENV joblib matplotlib numpy pandas psutil pytest python-graphviz scikit-learn scipy
+conda install -q -y -n $CONDA_ENV joblib matplotlib numpy pandas psutil pytest python-graphviz "scikit-learn<=0.21.3" scipy
 
 if [[ $OS_NAME == "macos" ]] && [[ $COMPILER == "clang" ]]; then
     # fix "OMP: Error #15: Initializing libiomp5.dylib, but found libomp.dylib already initialized." (OpenMP library conflict due to conda's MKL)
