0b142ecd2d7d1620db3eb0c4f90fe25cda5cdf5c
James Lamb <jaylamb20@gmail.com>
2020-01-03 00:11:04 -0600

    fixed linting issues

--- R-package/tests/testthat/test_learning_to_rank.R
+++ R-package/tests/testthat/test_learning_to_rank.R
@@ -15,8 +15,8 @@ test_that("learning-to-rank with lgb.train() works as expected", {
         )
         , init_score = inverse.rle(
             list(
-                lengths = c(rep(500.0, 12L), 513)
-                , values = seq_len(13L) * 2
+                lengths = c(rep(500.0, 12L), 513.0)
+                , values = seq_len(13L) * 2.0
             )
         )
     )
@@ -24,8 +24,8 @@ test_that("learning-to-rank with lgb.train() works as expected", {
         objective = "lambdarank"
         , metric = "ndcg"
         , ndcg_at = "1,2,3"
-        , metric_freq = 1
-        , max_position = 3
+        , metric_freq = 1L
+        , max_position = 3L
         , label_gain = "0,1,3,7"
     )
     model <- lgb.train(
@@ -38,8 +38,8 @@ test_that("learning-to-rank with lgb.train() works as expected", {
     dumped_model <- jsonlite::fromJSON(
         model$dump_model()
     )
-    expect_equal(dumped_model[['objective']], 'lambdarank')
-    expect_equal(fitted_model[['max_feature_idx']], ncol(train$data) - 1)
+    expect_equal(dumped_model[["objective"]], "lambdarank")
+    expect_equal(dumped_model[["max_feature_idx"]], ncol(train$data) - 1L)
 })
 
 test_that("learning-to-rank with lgb.cv() works as expected", {
@@ -57,8 +57,8 @@ test_that("learning-to-rank with lgb.cv() works as expected", {
         )
         , init_score = inverse.rle(
             list(
-                lengths = c(rep(500.0, 12L), 513)
-                , values = seq_len(13L) * 2
+                lengths = c(rep(500.0, 12L), 513.0)
+                , values = seq_len(13L) * 2.0
             )
         )
     )
@@ -66,29 +66,29 @@ test_that("learning-to-rank with lgb.cv() works as expected", {
         objective = "lambdarank"
         , metric = "ndcg"
         , ndcg_at = "1,2,3"
-        , metric_freq = 1
-        , max_position = 3
+        , metric_freq = 1L
+        , max_position = 3L
         , label_gain = "0,1,3,7"
     )
-    nfold <- 4
+    nfold <- 4L
     model <- lgb.cv(
         params = params
         , data = dtrain
-        , nrounds = 10
+        , nrounds = 10L
         , nfold = nfold
-        , min_data = 1
-        , learning_rate = 1
-        , early_stopping_rounds = 10
+        , min_data = 1L
+        , learning_rate = 1.0
+        , early_stopping_rounds = 10L
     )
-    expect_is(model, 'lgb.CVBooster')
+    expect_is(model, "lgb.CVBooster")
     expect_equal(length(model$boosters), nfold)
 
     # check details of each booster
-    for (bst in model$boosters){
+    for (bst in model$boosters) {
         dumped_model <- jsonlite::fromJSON(
             bst$booster$dump_model()
         )
-        expect_equal(dumped_model[['objective']], 'lambdarank')
-        expect_equal(dumped_model[['max_feature_idx']], ncol(train$data) - 1)
+        expect_equal(dumped_model[["objective"]], "lambdarank")
+        expect_equal(dumped_model[["max_feature_idx"]], ncol(train$data) - 1L)
     }
 })
