eb7ce671066db085df17ec9275ac5336cc077512
StrikerRUS <nekit94-12@hotmail.com>
2020-01-26 23:01:33 +0300

    start to support Python 3.8

--- python-package/setup.py
+++ python-package/setup.py
@@ -304,4 +304,5 @@ if __name__ == "__main__":
                        'Programming Language :: Python :: 3.5',
                        'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7',
+                       'Programming Language :: Python :: 3.8',
                        'Topic :: Scientific/Engineering :: Artificial Intelligence'])
