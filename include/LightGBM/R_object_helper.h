9048b45b1e46d74ee92504cb531cae4492f13548
James Lamb <jaylamb20@gmail.com>
2020-03-04 11:15:47 -0600

    reverted some indentation changes

--- include/LightGBM/R_object_helper.h
+++ include/LightGBM/R_object_helper.h
@@ -5,8 +5,10 @@
  * \brief A simple wrapper for accessing data in R object.
  *
  * \note
- * Due to license issue, we cannot include R's header file, so use this simple wrapper instead.
- * However, if R changes the way it defines objects, this file will need to be updated as well.
+ * We previously did not want to use R's headers because of license concerns. This is no longer a concern:
+ * https://github.com/microsoft/LightGBM/issues/629#issuecomment-474995635
+ * For now, this wrapper is lightgbm's interface from R to C.
+ * If R changes the way it defines objects, this file will need to be updated as well.
  */
 #ifndef R_OBJECT_HELPER_H_
 #define R_OBJECT_HELPER_H_
@@ -17,48 +19,47 @@
 // use .Internal(internalsID()) to uuid
 #define R_INTERNALS_UUID "2fdf6c18-697a-4ba7-b8ef-11c0d92f1327"
 
-
 #ifdef R_VER_ABOVE_35
-#define NAMED_BITS 16
-struct lgbm_sxpinfo {
-  unsigned int type : 5;
-  unsigned int scalar : 1;
-  unsigned int obj : 1;
-  unsigned int alt : 1;
-  unsigned int gp : 16;
-  unsigned int mark : 1;
-  unsigned int debug : 1;
-  unsigned int trace : 1;
-  unsigned int spare : 1;
-  unsigned int gcgen : 1;
-  unsigned int gccls : 3;
-  unsigned int named : NAMED_BITS;
-  unsigned int extra : 32 - NAMED_BITS;
-};
+  #define NAMED_BITS 16
+  struct lgbm_sxpinfo {
+    unsigned int type : 5;
+    unsigned int scalar : 1;
+    unsigned int obj : 1;
+    unsigned int alt : 1;
+    unsigned int gp : 16;
+    unsigned int mark : 1;
+    unsigned int debug : 1;
+    unsigned int trace : 1;
+    unsigned int spare : 1;
+    unsigned int gcgen : 1;
+    unsigned int gccls : 3;
+    unsigned int named : NAMED_BITS;
+    unsigned int extra : 32 - NAMED_BITS;
+  };
 
-// 64bit pointer
-#if INTPTR_MAX == INT64_MAX
-typedef int64_t R_xlen_t;
+  // 64bit pointer
+  #if INTPTR_MAX == INT64_MAX
+    typedef int64_t R_xlen_t;
+  #else
+    typedef int R_xlen_t;
+  #endif
+
 #else
-typedef int R_xlen_t;
-#endif
+  struct lgbm_sxpinfo {
+    unsigned int type : 5;
+    unsigned int obj : 1;
+    unsigned int named : 2;
+    unsigned int gp : 16;
+    unsigned int mark : 1;
+    unsigned int debug : 1;
+    unsigned int trace : 1;
+    unsigned int spare : 1;
+    unsigned int gcgen : 1;
+    unsigned int gccls : 3;
+  };
 
-#else
-struct lgbm_sxpinfo {
-  unsigned int type : 5;
-  unsigned int obj : 1;
-  unsigned int named : 2;
-  unsigned int gp : 16;
-  unsigned int mark : 1;
-  unsigned int debug : 1;
-  unsigned int trace : 1;
-  unsigned int spare : 1;
-  unsigned int gcgen : 1;
-  unsigned int gccls : 3;
-};
-
-typedef int R_xlen_t;
-#endif
+  typedef int R_xlen_t;
+#endif // R_VER_ABOVE_35
 
 struct lgbm_primsxp {
   int offset;
@@ -122,71 +123,71 @@ typedef struct VECTOR_SER {
 
 typedef union { VECTOR_SER s; double align; } SEXPREC_ALIGN;
 
-#define DATAPTR(x)  ((reinterpret_cast<SEXPREC_ALIGN*>(x)) + 1)
+#define DATAPTR(x)     ((reinterpret_cast<SEXPREC_ALIGN*>(x)) + 1)
 
-#define R_CHAR_PTR(x)     (reinterpret_cast<char*>DATAPTR(x))
+#define R_CHAR_PTR(x)  (reinterpret_cast<char*>DATAPTR(x))
 
-#define R_INT_PTR(x)  (reinterpret_cast<int*> DATAPTR(x))
+#define R_INT_PTR(x)   (reinterpret_cast<int*> DATAPTR(x))
 
-#define R_INT64_PTR(x)  (reinterpret_cast<int64_t*> DATAPTR(x))
+#define R_INT64_PTR(x) (reinterpret_cast<int64_t*> DATAPTR(x))
 
-#define R_REAL_PTR(x)     (reinterpret_cast<double*> DATAPTR(x))
+#define R_REAL_PTR(x)  (reinterpret_cast<double*> DATAPTR(x))
 
-#define R_AS_INT(x) (*(reinterpret_cast<int*> DATAPTR(x)))
+#define R_AS_INT(x)    (*(reinterpret_cast<int*> DATAPTR(x)))
 
-#define R_AS_INT64(x) (*(reinterpret_cast<int64_t*> DATAPTR(x)))
+#define R_AS_INT64(x)  (*(reinterpret_cast<int64_t*> DATAPTR(x)))
 
-#define R_IS_NULL(x) ((*reinterpret_cast<LGBM_SE>(x)).sxpinfo.type == 0)
+#define R_IS_NULL(x)   ((*reinterpret_cast<LGBM_SE>(x)).sxpinfo.type == 0)
 
 // 64bit pointer
 #if INTPTR_MAX == INT64_MAX
 
-#define R_ADDR(x)  (reinterpret_cast<int64_t*> DATAPTR(x))
+  #define R_ADDR(x)  (reinterpret_cast<int64_t*> DATAPTR(x))
 
-inline void R_SET_PTR(LGBM_SE x, void* ptr) {
-  if (ptr == nullptr) {
-    R_ADDR(x)[0] = (int64_t)(NULL);
-  } else {
-    R_ADDR(x)[0] = (int64_t)(ptr);
-  }
-}
-
-inline void* R_GET_PTR(LGBM_SE x) {
-  if (R_IS_NULL(x)) {
-    return nullptr;
-  } else {
-    auto ret = reinterpret_cast<void*>(R_ADDR(x)[0]);
-    if (ret == NULL) {
-      ret = nullptr;
+  inline void R_SET_PTR(LGBM_SE x, void* ptr) {
+    if (ptr == nullptr) {
+      R_ADDR(x)[0] = (int64_t)(NULL);
+    } else {
+      R_ADDR(x)[0] = (int64_t)(ptr);
     }
-    return ret;
   }
-}
+
+  inline void* R_GET_PTR(LGBM_SE x) {
+    if (R_IS_NULL(x)) {
+      return nullptr;
+    } else {
+      auto ret = reinterpret_cast<void*>(R_ADDR(x)[0]);
+      if (ret == NULL) {
+        ret = nullptr;
+      }
+      return ret;
+    }
+  }
 
 #else
 
-#define R_ADDR(x)  (reinterpret_cast<int32_t*> DATAPTR(x))
+  #define R_ADDR(x) (reinterpret_cast<int32_t*> DATAPTR(x))
 
-inline void R_SET_PTR(LGBM_SE x, void* ptr) {
-  if (ptr == nullptr) {
-    R_ADDR(x)[0] = (int32_t)(NULL);
-  } else {
-    R_ADDR(x)[0] = (int32_t)(ptr);
-  }
-}
-
-inline void* R_GET_PTR(LGBM_SE x) {
-  if (R_IS_NULL(x)) {
-    return nullptr;
-  } else {
-    auto ret = reinterpret_cast<void*>(R_ADDR(x)[0]);
-    if (ret == NULL) {
-      ret = nullptr;
+  inline void R_SET_PTR(LGBM_SE x, void* ptr) {
+    if (ptr == nullptr) {
+      R_ADDR(x)[0] = (int32_t)(NULL);
+    } else {
+      R_ADDR(x)[0] = (int32_t)(ptr);
     }
-    return ret;
   }
-}
 
-#endif
+  inline void* R_GET_PTR(LGBM_SE x) {
+    if (R_IS_NULL(x)) {
+      return nullptr;
+    } else {
+      auto ret = reinterpret_cast<void*>(R_ADDR(x)[0]);
+      if (ret == NULL) {
+        ret = nullptr;
+      }
+      return ret;
+    }
+  }
+
+#endif // INTPTR_MAX == INT64_MAX
 
 #endif  // R_OBJECT_HELPER_H_
