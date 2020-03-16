0d805fb829c6a7ca3cd50cad637cc9e484ae30c5
Guolin Ke <guolin.ke@outlook.com>
2020-03-16 09:58:12 +0800

    Update basic.py

--- python-package/lightgbm/basic.py
+++ python-package/lightgbm/basic.py
@@ -1097,9 +1097,10 @@ class Dataset(object):
         """
         if self.handle is None:
             if self.reference is not None:
-                if self.get_params() != self.reference.get_params():
+                reference_params = self.reference.get_params()
+                if self.get_params() != reference_params:
                     warnings.warn('Overriding the parameters from Reference Dataset.')
-                    self._update_params(self.reference.get_params())
+                    self._update_params(reference_params)
                 if self.used_indices is None:
                     # create valid
                     self._lazy_init(self.data, label=self.label, reference=self.reference,
