169a7341147de02d1e854e4b00c6a6134a88808a
Chip-Kerchner <chip.kerchner@ibm.com>
2020-04-02 18:32:22 +0000

    Initial CUDA work

--- include/LightGBM/cuda/vector_cudahost.h
+++ include/LightGBM/cuda/vector_cudahost.h
@@ -1,17 +1,7 @@
-/*
- * ibmGBT: IBM CUDA Accelerated LightGBM 
- *
- * IBM Confidential
- * (C) Copyright IBM Corp. 2019. All Rights Reserved.
- *
- * The source code for this program is not published or otherwise
- * divested of its trade secrets, irrespective of what has been
- * deposited with the U.S. Copyright Office.
- *
- * US Government Users Restricted Rights - Use, duplication or
- * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+/*!
+ * Copyright (c) 2020 IBM Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE file in the project root for license information.
  */
-
 #ifndef LGBM_CUDA_VECTOR_CH_H
 #define LGBM_CUDA_VECTOR_CH_H
 
