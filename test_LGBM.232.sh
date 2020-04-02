32825e525ca5758230275c8245b4abd9320dd89e
Chip-Kerchner <chip.kerchner@ibm.com>
2020-04-02 13:44:01 +0000

    Initial CUDA work

---
+++ test_LGBM.232.sh
@@ -0,0 +1,5 @@
+python -m unittest tests/python_package_test/test_basic.py
+python -m unittest tests/python_package_test/test_consistency.py
+python -m unittest tests/python_package_test/test_engine.py
+python -m unittest tests/python_package_test/test_plotting.py
+python -m unittest tests/python_package_test/test_sklearn.py
