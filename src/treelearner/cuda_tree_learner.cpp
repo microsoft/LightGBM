c7c22a57dc0e55f2697870766716c50409ff3736
Chip-Kerchner <chip.kerchner@ibm.com>
2020-05-15 17:39:29 +0000

    Initial CUDA work

--- src/treelearner/cuda_tree_learner.cpp
+++ src/treelearner/cuda_tree_learner.cpp
@@ -87,7 +87,7 @@ void PrintHistograms(hist_t* h, size_t size) {
   double total_hess = 0;
   for (size_t i = 0; i < size; ++i) {
     printf("%03lu=%9.3g,%9.3g\t", i, GET_GRAD(h, i), GET_HESS(h, i));
-    if ((i & 2) == 2)
+    if ((i & 3) == 3)
         printf("\n");
     total_hess += GET_HESS(h, i);
   }
@@ -1098,7 +1098,7 @@ void CUDATreeLearner::FindBestSplits() {
     if (larger_leaf_splits_ == nullptr || larger_leaf_splits_->leaf_index() < 0) { continue; }
     printf("CUDATreeLearner::FindBestSplits() Feature %d bin_size=%zd larger leaf:\n", feature_index, bin_size);
 
-    PrintHistograms(larger_leaf_histogram_array_[feature_index].RawData() - kHistOffset1, bin_size);
+    PrintHistograms(larger_leaf_histogram_array_[feature_index].RawData() - kHistOffset, bin_size);
   }
 #endif
 }
