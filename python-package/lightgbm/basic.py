deb316a454b146ca2be07a093a9c2d42848bdada
Guolin Ke <guolin.ke@outlook.com>
2020-05-18 08:39:02 +0800

    Apply suggestions from code review

--- python-package/lightgbm/basic.py
+++ python-package/lightgbm/basic.py
@@ -25,6 +25,7 @@ def _log_callback(msg):
     """Redirect logs from native library into Python console."""
     print("{0:s}".format(decode_string(msg)), end='')
 
+
 def _load_lib():
     """Load LightGBM library."""
     lib_path = find_lib_path()
@@ -32,7 +33,7 @@ def _load_lib():
         return None
     lib = ctypes.cdll.LoadLibrary(lib_path[0])
     lib.LGBM_GetLastError.restype = ctypes.c_char_p
-    callback= ctypes.CFUNCTYPE(None, ctypes.c_char_p)
+    callback = ctypes.CFUNCTYPE(None, ctypes.c_char_p)
     lib.callback = callback(_log_callback)
     if lib.LGBM_RegisterLogCallback(lib.callback) != 0:
         raise LightGBMError(decode_string(lib.LGBM_GetLastError()))
