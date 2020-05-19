daceeb81ff529ac9f0ca6fbf2ea3e9f1543f8db2
James Lamb <jaylamb20@gmail.com>
2020-05-19 16:38:55 -0500

    only use main metrics

--- R-package/R/metrics.R
+++ R-package/R/metrics.R
@@ -3,21 +3,15 @@
 #
 # [return] A named logical vector, where each key is a metric name and each value is a boolean.
 #          TRUE if higher values of the metric are desirable, FALSE if lower values are desirable.
+#          Note that only the 'main' metrics are stored here, not aliases, since only the 'main' metrics
+#          are returned from the C++ side. For example, if you use `metric = "mse"` in your code,
+#          the metric name `"l2"` will be returned.
 .METRICS_HIGHER_BETTER <- function() {
     return(c(
         "l1" = FALSE
         , "l2" = FALSE
-        , "mae" = FALSE
         , "mape" = FALSE
-        , "mean_absolute_percentage_error" = FALSE
-        , "mse" = FALSE
-        , "mean_squared_error" = FALSE
-        , "mean_absolute_error" = FALSE
-        , "regression_l1" = FALSE
-        , "regression_l2" = FALSE
-        , "regression" = FALSE
         , "rmse" = FALSE
-        , "root_mean_squared_error" = FALSE
         , "quantile" = FALSE
         , "huber" = FALSE
         , "fair" = FALSE
@@ -26,31 +20,16 @@
         , "gamma_deviance" = FALSE
         , "tweedie" = FALSE
         , "ndcg" = TRUE
-        , "lambdarank" = TRUE
-        , "rank_xendcg" = TRUE
-        , "xendcg" = TRUE
-        , "xe_ndcg" = TRUE
-        , "xe_ndcg_mart" = TRUE
-        , "xendcg_mart" = TRUE
         , "map" = TRUE
-        , "mean_average_precision" = TRUE
         , "auc" = TRUE
         , "binary_logloss" = FALSE
         , "binary_error" = FALSE
         , "auc_mu" = TRUE
         , "multi_logloss" = FALSE
         , "multiclass" = FALSE
-        , "softmax" = FALSE
-        , "multiclassova" = FALSE
-        , "multiclass_ova" = FALSE
-        , "ova" = FALSE
-        , "ovr" = FALSE
         , "multi_error" = FALSE
         , "cross_entropy" = FALSE
-        , "xentropy" = FALSE
         , "cross_entropy_lambda" = FALSE
-        , "xentlambda" = FALSE
         , "kullback_leibler" = FALSE
-        , "kldiv" = FALSE
     ))
 }
