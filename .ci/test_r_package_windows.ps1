09b87d9a8f85ec0e485547a29cd73453d8dc6f6b
James Lamb <jaylamb20@gmail.com>
2020-05-22 22:59:54 -0500

    put back check-output

--- .ci/test_r_package_windows.ps1
+++ .ci/test_r_package_windows.ps1
@@ -67,7 +67,7 @@ if ($env:COMPILER -ne "MSVC") {
 
 Write-Output "Installing dependencies"
 $packages = "c('data.table', 'jsonlite', 'Matrix', 'processx', 'R6', 'testthat'), dependencies = c('Imports', 'Depends', 'LinkingTo')"
-Rscript --vanilla -e "options(install.packages.check.source = 'no'); install.packages($packages, repos = '$env:CRAN_MIRROR', type = 'binary', lib = '$env:R_LIB_PATH')"
+Rscript --vanilla -e "options(install.packages.check.source = 'no'); install.packages($packages, repos = '$env:CRAN_MIRROR', type = 'binary', lib = '$env:R_LIB_PATH')"  ; Check-Output $?
 
 Write-Output "Building R package"
 
