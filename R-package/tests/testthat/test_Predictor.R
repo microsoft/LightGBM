12e32caa5651fb15ec6208ec5566e9d6bd58301c
mayer79 <mayermichael79@gmail.com>
2020-06-02 20:20:18 +0200

    Explicit integers in test_Preditor to avoid TravisCI failure

--- R-package/tests/testthat/test_Predictor.R
+++ R-package/tests/testthat/test_Predictor.R
@@ -1,7 +1,7 @@
 context("Predictor")
 
 test_that("predictions do not fail for integer input", {
-    X <- as.matrix(as.integer(iris[, "Species"]), ncol = 1)
+    X <- as.matrix(as.integer(iris[, "Species"]), ncol = 1L)
     y <- iris[["Sepal.Length"]]
     dtrain <- lgb.Dataset(X, label = y)
     fit <- lgb.train(
@@ -9,7 +9,7 @@ test_that("predictions do not fail for integer input", {
         , objective = "regression"
         , verbose = -1L
     )
-    X_double <- X_integer <- X[c(1, 51, 101), , drop = FALSE]
+    X_double <- X_integer <- X[c(1L, 51L, 101L), , drop = FALSE]
     storage.mode(X_double) <- "double"
     pred_integer <- predict(fit, X_integer)
     pred_double <- predict(fit, X_double)
