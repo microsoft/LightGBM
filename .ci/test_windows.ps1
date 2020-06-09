bf226e3ad42ec21b1b86ccf8367f1cfd3a4c6eaa
Itamar Turner-Trauring <itamar@itamarst.org>
2020-06-09 11:13:58 -0400

    Try newer OpenCL runtime

--- .ci/test_windows.ps1
+++ .ci/test_windows.ps1
@@ -49,7 +49,16 @@ elseif ($env:TASK -eq "sdist") {
 }
 elseif ($env:TASK -eq "bdist") {
   # Install the Intel CPU runtime, so we can run tests against OpenCL
-  choco install opencl-intel-cpu-runtime -y
+  curl -o opencl_runtime_18.1_x64_setup.msi http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13794/opencl_runtime_18.1_x64_setup.msi
+  $msiarglist = "/i opencl_runtime_18.1_x64_setup.msi /quiet /norestart /log msi.log"
+  $return = Start-Process msiexec -ArgumentList $msiarglist -Wait -passthru
+  Get-Content msi.log
+  If (@(0,3010) -contains $return.exitcode) {
+    Write-Output "OpenCL install successful"
+  } else {
+    Write-Output "OpenCL install failed, aborting"
+    exit 1
+  }
   RefreshEnv
   Write-Output "Current OpenCL drivers:"
   Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenCL\Vendors
