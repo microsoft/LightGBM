8df1e5b2acdc83fe2cef14f67fdb4a7f09f6a057
German I Ramirez-Espinoza <gire@home>
2020-07-13 19:51:25 +0800

    Move bug-fix test to the test_metrics unit-test

--- tests/python_package_test/test_sklearn.py
+++ tests/python_package_test/test_sklearn.py
@@ -540,6 +540,17 @@ class TestSklearn(unittest.TestCase):
         self.assertIn('l2', gbm.evals_result_['training'])
         self.assertIn('mape', gbm.evals_result_['training'])
 
+        # non-default metric with multiple metrics in eval_metric for LGBMClassifier
+        X_classification, y_classification = load_breast_cancer(True)
+        params = {'n_estimators': 2, 'verbose': -1, 'objective': 'binary', 'metric': 'binary_logloss'}
+        params_fit = {'X': X_classification, 'y': y_classification,
+                      'eval_set': (X_classification, y_classification), 'verbose': False}
+        gbm = lgb.LGBMClassifier(**params).fit(eval_metric=['fair', 'error'], **params_fit)
+        self.assertEqual(len(gbm.evals_result_['training']), 3)
+        self.assertIn('fair', gbm.evals_result_['training'])
+        self.assertIn('binary_error', gbm.evals_result_['training'])
+        self.assertIn('binary_logloss', gbm.evals_result_['training'])
+
         # default metric for non-default objective
         gbm = lgb.LGBMRegressor(objective='regression_l1', **params).fit(**params_fit)
         self.assertEqual(len(gbm.evals_result_['training']), 1)
@@ -922,14 +933,3 @@ class TestSklearn(unittest.TestCase):
         self.assertEqual(len(init_gbm.evals_result_['valid_0']['multi_logloss']), 5)
         self.assertLess(gbm.evals_result_['valid_0']['multi_logloss'][-1],
                         init_gbm.evals_result_['valid_0']['multi_logloss'][-1])
-
-    def test_eval_metrics_lgbmclassifier(self):
-        X, y = load_breast_cancer(True)
-        params = {'n_estimators': 2, 'verbose': -1, 'objective': 'binary', 'metric': 'binary_logloss'}
-        params_fit = {'X': X, 'y': y, 'eval_set': (X, y), 'verbose': False}
-
-        gbm = lgb.LGBMClassifier(**params).fit(eval_metric=['fair', 'error'], **params_fit)
-        self.assertEqual(len(gbm.evals_result_['training']), 3)
-        self.assertIn('fair', gbm.evals_result_['training'])
-        self.assertIn('binary_error', gbm.evals_result_['training'])
-        self.assertIn('binary_logloss', gbm.evals_result_['training'])
