27eeb9eb0431ae0a93b461a4c1531145c0b32310
James Lamb <jaylamb20@gmail.com>
2020-08-02 23:08:00 -0500

    [R-package] improvements to lgb.convert() functions (fixes #2678, #2681)

--- R-package/R/lgb.convert.R
+++ R-package/R/lgb.convert.R
@@ -1,15 +1,65 @@
+# [description] get all column classes of a data.table or data.frame.
+#               This function collapses the result of class() into a single string
+.get_column_classes <- function(df) {
+    return(
+        vapply(
+            X = df
+            , FUN = function(x) {paste0(class(x), collapse = ",")}
+            , FUN.VALUE = character(1L)
+        )
+    )
+}
+
+# [description] check a data frame or data table for columns tthat are any
+#               type other than numeric and integer. This is used by lgb.convert()
+#               and lgb.convert_with_rules() too warn if more action is needed by users
+#               before a dataset can be converted to a lgb.Dataset.
+.warn_for_unconverted_columns <- function(df, function_name) {
+    column_classes <- .get_column_classes(df)
+    unconverted_columns <- column_classes[!(column_classes %in% c("numeric", "integer"))]
+    if (length(unconverted_columns) > 0L) {
+        col_detail_string <- paste0(
+            paste0(
+                names(unconverted_columns)
+                , " ("
+                , unconverted_columns
+                , ")"
+            )
+            , collapse = ", "
+        )
+        msg <- paste0(
+            function_name
+            , ": "
+            , length(unconverted_columns)
+            , " columns are not numeric or integer. These need to be dropped or converted to "
+            , "be used in an lgb.Dataset object. "
+            , col_detail_string
+        )
+        warning(msg)
+    }
+    return(invisible(NULL))
+}
+
+.LGB_CONVERT_DEFAULT_FOR_LOGICAL_NA <- function() {return(-1L)}
+.LGB_CONVERT_DEFAULT_FOR_NON_LOGICAL_NA <- function() {return(0L)}
+
 #' @name lgb.convert
 #' @title Data preparator for LightGBM datasets (integer)
-#' @description Attempts to prepare a clean dataset to prepare to put in a \code{lgb.Dataset}.
-#'              Factors and characters are converted to integer.
-#'              Please use \code{\link{lgb.convert_with_rules}} if you want to apply this transformation to
-#'              other datasets. This is useful if you have a specific need for integer dataset instead
-#'              of numeric dataset.
+#' @description Attempts to prepare a clean dataset to put in a \code{lgb.Dataset}.
+#'              Factor, character, and logical columns are converted to integer.
+#'
+#'              Missing values in factor and character columns will be replaced with 0. Missing
+#'              values in logical columns will be replaced with -1.
+#'
+#'              Please use \code{\link{lgb.convert_with_rules}} if you want to apply this
+#'              transformation to other datasets.
 #'
 #'              NOTE: In previous releases of LightGBM, this function was called \code{lgb.prepare}.
-#' @param data A data.frame or data.table to prepare.
+#' @param data A data.frame or \code{data.table} to prepare. If a \code{data.table} is provided,
+#'             it will be modified in place for speed and too avoid out-of-memory erros.
 #' @return The cleaned dataset. It must be converted to a matrix format (\code{as.matrix})
-#'         for input in \code{lgb.Dataset}.
+#'         for input in \code{lgb.Dataset}. If \code{data} is a \code{data.table}, it will be
+#'         modified in place.
 #'
 #' @examples
 #' data(iris)
@@ -32,59 +82,84 @@
 #' # $ Species     : int  1 1 1 1 1 1 1 1 1 1 ...
 #' }
 #'
+#' @importFrom data.table := is.data.table
 #' @export
 lgb.convert <- function(data) {
 
-  # data.table not behaving like data.frame
-  if (inherits(data, "data.table")) {
+    column_classes <- .get_column_classes(data)
 
-    # Get data classes
-    list_classes <- vapply(data, class, character(1L))
+    is_char <- which(column_classes == "character")
+    is_factor <- which(column_classes == "factor")
+    is_logical <- which(column_classes == "logical")
 
-    # Convert characters to integer
-    is_char <- which(list_classes == "character")
-    if (length(is_char) > 0L) {
-      data[, (is_char) := lapply(.SD, function(x) {as.integer(as.factor(x))}), .SDcols = is_char]
+    is_data_table <- data.table::is.data.table(data)
+    is_data_frame <- is.data.frame(data)
+
+    if (!(is_data_table || is_data_frame)) {
+        stop(
+            "lgb.convert: you provided "
+            , paste(class(data), collapse = " & ")
+            , " but data should have class data.frame or data.table"
+        )
     }
 
-    # Convert factors to integer
-    is_fact <- c(which(list_classes == "factor"), is_char)
-    if (length(is_fact) > 0L) {
-      data[, (is_fact) := lapply(.SD, function(x) {as.integer(x)}), .SDcols = is_fact]
+    # data.table not behaving like data.frame
+    if (is_data_table) {
+
+        if (length(is_char) > 0L) {
+            #data[, (is_char) := lapply(.SD, function(x) {as.integer(as.factor(x))}), .SDcols = is_char]
+            for (col_name in names(is_char)) {
+                data[, (col_name) := as.integer(as.factor(get(col_name)))]
+                data[is.na(get(col_name)), (col_name) := .LGB_CONVERT_DEFAULT_FOR_NON_LOGICAL_NA()]
+            }
+        }
+
+        if (length(is_factor) > 0L) {
+            #data[, (is_factor) := lapply(.SD, function(x) {as.integer(x)}), .SDcols = is_factor]
+            for (col_name in names(is_factor)) {
+                data[, (col_name) := as.integer(get(col_name))]
+                data[is.na(get(col_name)), (col_name) := .LGB_CONVERT_DEFAULT_FOR_NON_LOGICAL_NA()]
+            }
+        }
+
+        if (length(is_logical) > 0L) {
+            # data[, (is_logical) := lapply(.SD, function(x) {as.integer(x)}), .SDcols = is_logical]
+            for (col_name in names(is_logical)) {
+                data[, (col_name) := as.integer(get(col_name))]
+                data[is.na(get(col_name)), (col_name) := .LGB_CONVERT_DEFAULT_FOR_LOGICAL_NA()]
+            }
+        }
+
+    } else if (is_data_frame) {
+
+        if (length(is_char) > 0L) {
+            for (col_name in names(is_char)) {
+                data[[col_name]] <- as.integer(as.factor(data[[col_name]]))
+                data[is.na(data[col_name]), col_name] <- .LGB_CONVERT_DEFAULT_FOR_NON_LOGICAL_NA()
+            }
+            #data[is_char] <- lapply(data[is_char], function(x) {as.integer(as.factor(x))})
+        }
+
+        if (length(is_factor) > 0L) {
+            #data[is_factor] <- lapply(data[is_factor], function(x) {as.integer(x)})
+            for (col_name in names(is_factor)) {
+                data[[col_name]] <- as.integer(data[[col_name]])
+                data[is.na(data[col_name]), col_name] <- .LGB_CONVERT_DEFAULT_FOR_NON_LOGICAL_NA()
+            }
+        }
+
+        if (length(is_logical) > 0L) {
+            #data[is_logical] <- lapply(data[is_logical], function(x) {as.integer(x)})
+            for (col_name in names(is_logical)) {
+                data[[col_name]] <- as.integer(data[[col_name]])
+                data[is.na(data[col_name]), col_name] <- .LGB_CONVERT_DEFAULT_FOR_LOGICAL_NA()
+            }
+        }
+
     }
 
-  } else {
+    .warn_for_unconverted_columns(df = data, function_name = "lgb.convert")
 
-    # Default routine (data.frame)
-    if (inherits(data, "data.frame")) {
-
-      # Get data classes
-      list_classes <- vapply(data, class, character(1L))
-
-      # Convert characters to factors to numeric (integer is more efficient actually)
-      is_char <- which(list_classes == "character")
-      if (length(is_char) > 0L) {
-        data[is_char] <- lapply(data[is_char], function(x) {as.integer(as.factor(x))})
-      }
-
-      # Convert factors to numeric (integer is more efficient actually)
-      is_fact <- which(list_classes == "factor")
-      if (length(is_fact) > 0L) {
-        data[is_fact] <- lapply(data[is_fact], function(x) {as.integer(x)})
-      }
-
-    } else {
-
-      stop(
-        "lgb.convert: you provided "
-        , paste(class(data), collapse = " & ")
-        , " but data should have class data.frame or data.table"
-      )
-
-    }
-
-  }
-
-  return(data)
+    return(data)
 
 }
--- R-package/tests/testthat/test_lgb.convert.R
+++ R-package/tests/testthat/test_lgb.convert.R
@@ -88,7 +88,27 @@ test_that("lgb.convert() should work correctly for a dataset with numeric, facto
     }
 })
 
-test_that("lgb.convert() should work correctly for a dataset with missing values", {
+
+test_that("lgb.convert() should work correctly for a dataset with all logical columns", {
+    testDF <- data.frame(
+        all_trues = rep(TRUE, 5L)
+        , all_falses = rep(FALSE, 5L)
+        , back_and_forth = c(TRUE, FALSE, TRUE, FALSE, TRUE)
+    )
+    testDT <- data.table::as.data.table(testDF)
+    for (input_data in list(testDF, testDT)) {
+        converted_dataset <- lgb.convert(input_data)
+        expect_identical(class(input_data), class(converted_dataset))
+        for (col_name in names(input_data)) {
+            expect_identical(class(converted_dataset[[col_name]]), "integer")
+        }
+        expect_identical(converted_dataset[["all_trues"]], rep(1L, 5L))
+        expect_identical(converted_dataset[["all_falses"]], rep(0L, 5L))
+        expect_identical(converted_dataset[["back_and_forth"]], c(1L, 0L, 1L, 0L, 1L))
+    }
+})
+
+test_that("lgb.convert() should convert missing values and should work with columns that have NAs", {
     testDF <- data.frame(
         character_col = c("a", NA_character_, "c")
         , na_col = rep(NA, 3L)
@@ -106,24 +126,27 @@ test_that("lgb.convert() should work correctly for a dataset with missing values
         expect_identical(class(input_data), class(converted_dataset))
 
         expect_identical(class(converted_dataset[["character_col"]]), "integer")
-        expect_identical(converted_dataset[["character_col"]], c(1L, NA_integer_, 2L))
+        expect_identical(converted_dataset[["character_col"]], c(1L, 0L, 2L))
 
+        # should not convert integer columns
         expect_identical(class(converted_dataset[["integer_col"]]), "integer")
         expect_identical(converted_dataset[["integer_col"]], c(1L, 9L, NA_integer_))
+        expect_identical(class(converted_dataset[["na_int_col"]]), "integer")
+        expect_identical(converted_dataset[["na_int_col"]], rep(NA_integer_, nrow(converted_dataset)))
 
         expect_identical(class(converted_dataset[["factor_col"]]), "integer")
         expect_identical(converted_dataset[["factor_col"]], c(1L, 1L, 2L))
 
-        # NAs of any type should be converted to numeric
-        for (col in c("na_int_col", "na_character_col")) {
-            expect_identical(class(converted_dataset[[col]]), "integer")
-            expect_identical(converted_dataset[[col]], rep(NA_integer_, nrow(converted_dataset)))
-        }
 
-        # today, lgb.convert() does not convert logical columns
-        expect_identical(class(converted_dataset[["na_col"]]), "logical")
+        # even columns of all NAs should be converted if they are character
+        expect_identical(class(converted_dataset[["na_character_col"]]), "integer")
+        expect_identical(converted_dataset[["na_character_col"]], rep(0L, nrow(converted_dataset)))
 
-        # today, lgb.convert() does not convert numeric columns to integer
+        # columns of all logical NAs should have been converted to integer too
+        expect_identical(class(converted_dataset[["na_col"]]), "integer")
+        expect_identical(converted_dataset[["na_col"]], rep(-1L, 3L))
+
+        # lgb.convert() should convert numeric columns to integer
         expect_identical(class(converted_dataset[["na_real_col"]]), "numeric")
         expect_identical(converted_dataset[["na_real_col"]], rep(NA_real_, nrow(converted_dataset)))
         expect_identical(class(converted_dataset[["numeric_col"]]), "numeric")
@@ -144,4 +167,19 @@ test_that("lgb.convert() should modify data.tables in-place", {
     )
     resultDT <- lgb.convert(testDT)
     expect_identical(resultDT, testDT)
+})
+
+test_that("lgb.convert() should warn on the presence of columns it cannot convert", {
+    testDF <- data.frame(
+        character_col = c("a", NA_character_, "c")
+        , posix_col = rep(as.POSIXct(Sys.time()), 3L)
+    )
+    testDT <- data.table::as.data.table(testDF)
+    for (input_data in list(testDF, testDT)) {
+        expect_warning({
+            converted_dataset <- lgb.convert(input_data)
+        }, regexp = "columns are not numeric or integer")
+        expect_identical(converted_dataset[["character_col"]], c(1L, 0L, 2L))
+        expect_identical(converted_dataset[["posix_col"]], input_data[["posix_col"]])
+    }
 })
