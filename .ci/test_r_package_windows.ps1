4a641c675a1da8da064bb666e35714f3b496f788
James Lamb <jaylamb20@gmail.com>
2020-08-06 18:29:49 -0500

    get more logs from Rtools install

--- .ci/test_r_package_windows.ps1
+++ .ci/test_r_package_windows.ps1
@@ -112,7 +112,7 @@ Start-Process -FilePath R-win.exe -NoNewWindow -Wait -ArgumentList "/VERYSILENT 
 Write-Output "Done installing R"
 
 Write-Output "Installing Rtools"
-Start-Process -FilePath Rtools.exe -NoNewWindow -Wait -ArgumentList "/VERYSILENT /DIR=$RTOOLS_INSTALL_PATH" ; Check-Output $?
+Start-Process -FilePath Rtools.exe -NoNewWindow -Wait -ArgumentList "/DIR=$RTOOLS_INSTALL_PATH" ; Check-Output $?
 Write-Output "Done installing Rtools"
 
 Write-Output "Installing dependencies"
