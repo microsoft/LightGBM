c454d5f8ccc7e96350719bd6af43b7ecc5288a8a
James Lamb <jaylamb20@gmail.com>
2020-08-05 20:53:11 -0500

    [R-package] move all examples to dontrun() to fix R CMD CHECK notes (#3270)
    * Move all examples to dontrun
    
    * update docs
    
    * fix nested dontrun
    
    * remove :: in examples
    
    * run_dontrun in pkgdown
    
    Co-authored-by: Nikita Titov <nekit94-08@mail.ru>

--- R-package/R/lgb.convert.R
+++ R-package/R/lgb.convert.R
@@ -12,6 +12,7 @@
 #'         for input in \code{lgb.Dataset}.
 #'
 #' @examples
+#' \dontrun{
 #' data(iris)
 #'
 #' str(iris)
@@ -19,11 +20,10 @@
 #' # Convert all factors/chars to integer
 #' str(lgb.convert(data = iris))
 #'
-#' \dontrun{
 #' # When lightgbm package is installed, and you do not want to load it
 #' # You can still use the function!
 #' lgb.unloader()
-#' str(lightgbm::lgb.convert(data = iris))
+#' str(lgb.convert(data = iris))
 #' # 'data.frame':	150 obs. of  5 variables:
 #' # $ Sepal.Length: num  5.1 4.9 4.7 4.6 5 5.4 4.6 5 4.4 4.9 ...
 #' # $ Sepal.Width : num  3.5 3 3.2 3.1 3.6 3.9 3.4 3.4 2.9 3.1 ...
@@ -31,7 +31,6 @@
 #' # $ Petal.Width : num  0.2 0.2 0.2 0.2 0.2 0.4 0.3 0.2 0.2 0.1 ...
 #' # $ Species     : int  1 1 1 1 1 1 1 1 1 1 ...
 #' }
-#'
 #' @export
 lgb.convert <- function(data) {
 
