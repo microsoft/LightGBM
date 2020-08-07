63c8287928fbccb91b0e169049286af3f5f3823b
Guolin Ke <guolin.ke@outlook.com>
2020-08-07 08:11:12 +0800

    v3.0.0rc1 (#3071)
    * Update VERSION.txt
    
    * Update VERSION.txt
    
    * Update setup.py
    
    * Update VERSION.txt
    
    * Update setup.py
    
    * Update VERSION.txt
    
    * Update .appveyor.yml
    
    * Update DESCRIPTION
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Update DESCRIPTION
    
    * Apply suggestions from code review
    
    * Create VERSION.txt
    
    * Apply suggestions from code review
    
    * Update .gitignore
    
    * Update recreate-configure.sh
    
    * Update test_r_package.sh
    
    * Update build-cran-package.sh
    
    * Delete VERSION.txt
    
    * Create VERSION.txt
    
    * Update main.yml
    
    * Update test_r_package_windows.ps1
    
    * Update configure
    
    * Update test_r_package_windows.ps1
    
    * Update test_r_package.sh
    
    * Delete VERSION.txt
    
    * Apply suggestions from code review
    
    * update configure.ac
    
    Co-authored-by: James Lamb <jaylamb20@gmail.com>

--- .ci/test_r_package.sh
+++ .ci/test_r_package.sh
@@ -125,7 +125,7 @@ fi
 
 cd ${BUILD_DIRECTORY}
 
-PKG_TARBALL="lightgbm_${LGB_VER}.tar.gz"
+PKG_TARBALL="lightgbm_*.tar.gz"
 LOG_FILE_NAME="lightgbm.Rcheck/00check.log"
 if [[ $R_BUILD_TYPE == "cmake" ]]; then
     Rscript build_r.R --skip-install || exit -1
@@ -134,7 +134,6 @@ elif [[ $R_BUILD_TYPE == "cran" ]]; then
     # on Linux, we recreate configure in CI to test if
     # a change in a PR has changed configure.ac
     if [[ $OS_NAME == "linux" ]]; then
-        cp VERSION.txt R-package/src/VERSION.txt
         cd ${BUILD_DIRECTORY}/R-package
         autoconf \
             --output configure \
