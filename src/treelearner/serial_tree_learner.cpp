b50b59caf8d8b60962ab14a7e748a6384edee543
btrotta <btrotta@users.noreply.github.com>
2020-08-14 17:11:09 +1000

    Fix include paths

--- src/treelearner/serial_tree_learner.cpp
+++ src/treelearner/serial_tree_learner.cpp
@@ -6,7 +6,7 @@
 
 // preprocessor definition ensures we use only MPL2-licensed code
 #define EIGEN_MPL2_ONLY
-#include <Dense>
+#include <Eigen/Dense>
 #include <LightGBM/network.h>
 #include <LightGBM/objective_function.h>
 #include <LightGBM/utils/array_args.h>
