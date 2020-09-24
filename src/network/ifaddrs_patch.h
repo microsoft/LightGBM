8b6a3a711cdaca343f72f7c03598948837dc90f2
James Lamb <jaylamb20@gmail.com>
2020-09-23 22:14:50 -0500

    add Solaris patch

---
+++ src/network/ifaddrs_patch.h
@@ -0,0 +1,30 @@
+/* 
+
+- https://lists.samba.org/archive/samba-technical/2009-February/063079.html
+- https://github.com/giampaolo/psutil/blob/master/psutil/arch/solaris/v10/ifaddrs.h
+
+*/
+
+#ifndef __IFADDRS_H__
+#define __IFADDRS_H__
+
+#include <sys/socket.h>
+#include <net/if.h>
+
+#undef  ifa_dstaddr
+#undef  ifa_broadaddr
+#define ifa_broadaddr ifa_dstaddr
+
+struct ifaddrs {
+    struct ifaddrs  *ifa_next;
+    char            *ifa_name;
+    unsigned int     ifa_flags;
+    struct sockaddr *ifa_addr;
+    struct sockaddr *ifa_netmask;
+    struct sockaddr *ifa_dstaddr;
+};
+
+extern int getifaddrs(struct ifaddrs **);
+extern void freeifaddrs(struct ifaddrs *);
+
+#endif
--- src/network/socket_wrapper.hpp
+++ src/network/socket_wrapper.hpp
@@ -25,9 +25,14 @@
 
 #else
 
+// #include <string.h>
+// #include <stdlib.h>
+#include <net/if.h>
+#include <sys/sockio.h>
+
 #include <arpa/inet.h>
 #include <fcntl.h>
-#include <ifaddrs.h>
+// #include <ifaddrs.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
@@ -35,6 +40,8 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#include "ifaddrs_patch.h"
 
 #endif  // defined(_WIN32)
 
@@ -52,6 +59,123 @@ const int INVALID_SOCKET = -1;
 #define SOCKET_ERROR -1
 
 #endif
+
+// ------------------------------------------------------------------
+// TERRIBLE IFADDRS HACK
+// ------------------------------------------------------------------
+
+#define MAX(x,y) ((x)>(y)?(x):(y))
+#define SIZE(p) MAX((p).ss_len,sizeof(p))
+
+
+static struct sockaddr *
+sa_dup (struct sockaddr_storage *sa1)
+{
+    struct sockaddr *sa2;
+    size_t sz = sizeof(struct sockaddr_storage);
+    sa2 = (struct sockaddr *) calloc(1,sz);
+    memcpy(sa2,sa1,sz);
+    return(sa2);
+}
+
+
+void freeifaddrs (struct ifaddrs *ifp)
+{
+    if (NULL == ifp) return;
+    free(ifp->ifa_name);
+    free(ifp->ifa_addr);
+    free(ifp->ifa_netmask);
+    free(ifp->ifa_dstaddr);
+    freeifaddrs(ifp->ifa_next);
+    free(ifp);
+}
+
+
+int getifaddrs (struct ifaddrs **ifap)
+{
+    int sd = -1;
+    char *ccp, *ecp;
+    struct lifconf ifc;
+    struct lifreq *ifr;
+    struct lifnum lifn;
+    struct ifaddrs *cifa = NULL; /* current */
+    struct ifaddrs *pifa = NULL; /* previous */
+    const size_t IFREQSZ = sizeof(struct lifreq);
+
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sd < 0)
+        goto error;
+
+    ifc.lifc_buf = NULL;
+    *ifap = NULL;
+    /* find how much memory to allocate for the SIOCGLIFCONF call */
+    lifn.lifn_family = AF_UNSPEC;
+    lifn.lifn_flags = 0;
+    if (ioctl(sd, SIOCGLIFNUM, &lifn) < 0)
+        goto error;
+
+    /* Sun and Apple code likes to pad the interface count here in case interfaces
+     * are coming up between calls */
+    lifn.lifn_count += 4;
+
+    ifc.lifc_family = AF_UNSPEC;
+    ifc.lifc_len = lifn.lifn_count * sizeof(struct lifreq);
+    ifc.lifc_buf = static_cast<char*>(calloc(1, ifc.lifc_len));
+    if (ioctl(sd, SIOCGLIFCONF, &ifc) < 0)
+        goto error;
+
+    ccp = reinterpret_cast<char *>(ifc.lifc_req);
+    ecp = ccp + ifc.lifc_len;
+
+    while (ccp < ecp) {
+
+        ifr = (struct lifreq *) ccp;
+        cifa = (struct ifaddrs *) calloc(1, sizeof(struct ifaddrs));
+        cifa->ifa_next = NULL;
+        cifa->ifa_name = strdup(ifr->lifr_name);
+
+        if (pifa == NULL) *ifap = cifa; /* first one */
+        else pifa->ifa_next = cifa;
+
+        if (ioctl(sd, SIOCGLIFADDR, ifr, IFREQSZ) < 0)
+            goto error;
+        cifa->ifa_addr = sa_dup(&ifr->lifr_addr);
+
+        if (ioctl(sd, SIOCGLIFNETMASK, ifr, IFREQSZ) < 0)
+            goto error;
+        cifa->ifa_netmask = sa_dup(&ifr->lifr_addr);
+
+        cifa->ifa_flags = 0;
+        cifa->ifa_dstaddr = NULL;
+
+        if (0 == ioctl(sd, SIOCGLIFFLAGS, ifr)) /* optional */
+            cifa->ifa_flags = ifr->lifr_flags;
+
+        if (ioctl(sd, SIOCGLIFDSTADDR, ifr, IFREQSZ) < 0) {
+            if (0 == ioctl(sd, SIOCGLIFBRDADDR, ifr, IFREQSZ))
+                cifa->ifa_dstaddr = sa_dup(&ifr->lifr_addr);
+        }
+        else cifa->ifa_dstaddr = sa_dup(&ifr->lifr_addr);
+
+        pifa = cifa;
+        ccp += IFREQSZ;
+    }
+    free(ifc.lifc_buf);
+    close(sd);
+    return 0;
+error:
+    if (ifc.lifc_buf != NULL)
+        free(ifc.lifc_buf);
+    if (sd != -1)
+        close(sd);
+    freeifaddrs(*ifap);
+    return (-1);
+}
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
 
 #ifdef _WIN32
 #ifndef _MSC_VER
