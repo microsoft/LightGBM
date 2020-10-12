650cb32b7af4c9139dc47d3d39c73cc91a23abb4
Guolin Ke <guolin.ke@outlook.com>
2020-10-12 13:42:34 +0800

    remove UNPROTECT(1);

--- R-package/src/lightgbm_R.cpp
+++ R-package/src/lightgbm_R.cpp
@@ -27,7 +27,6 @@
   catch(std::exception& ex) { R_INT_PTR(call_state)[0] = -1; LGBM_SetLastError(ex.what()); return call_state;} \
   catch(std::string& ex) { R_INT_PTR(call_state)[0] = -1; LGBM_SetLastError(ex.c_str()); return call_state;} \
   catch(...) { R_INT_PTR(call_state)[0] = -1; LGBM_SetLastError("unknown exception"); return call_state;} \
-  UNPROTECT(1); \
   return call_state;
 
 #define CHECK_CALL(x) \
