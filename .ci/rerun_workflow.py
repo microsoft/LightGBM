3e4718b269cc9039cb6dcb20d710a49cf2a87f0c
StrikerRUS <nekit94-12@hotmail.com>
2020-10-18 02:49:04 +0300

    hotfix

--- .ci/rerun_workflow.py
+++ .ci/rerun_workflow.py
@@ -20,7 +20,8 @@ def get_runs():
 def rerun_workflow(runs):
     if runs:
         req = request.Request(url="https://api.github.com/repos/microsoft/LightGBM/actions/runs/{}/rerun".format(runs[0]["id"]),
-                              headers={"accept": "application/vnd.github.v3+json"})
+                              headers={"accept": "application/vnd.github.v3+json"},
+                              method="POST")
         try:
             res = request.urlopen(req)
             if res.getcode() != 201:
