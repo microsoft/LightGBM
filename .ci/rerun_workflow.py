686512e3968ea83afa407cc411835c95427eafb4
StrikerRUS <nekit94-12@hotmail.com>
2020-10-18 02:44:41 +0300

    hotfix

--- .ci/rerun_workflow.py
+++ .ci/rerun_workflow.py
@@ -22,7 +22,7 @@ def rerun_workflow(runs):
         req = request.Request(url="https://api.github.com/repos/microsoft/LightGBM/actions/runs/{}/rerun".format(runs[0]["id"]),
                               headers={"accept": "application/vnd.github.v3+json"})
         try:
-            res = request.urlopen(req).
+            res = request.urlopen(req)
             if res.getcode() != 201:
                 print(res.getcode())
                 exit(1)
