b978aa55681ff3a8381ab6d8f5f461a763470ec6
StrikerRUS <nekit94-12@hotmail.com>
2020-10-18 03:42:20 +0300

    hotfix

--- .ci/rerun_workflow.py
+++ .ci/rerun_workflow.py
@@ -22,7 +22,7 @@ def rerun_workflow(runs):
     if runs:
         req = request.Request(url="https://api.github.com/repos/microsoft/LightGBM/actions/runs/{}/rerun".format(runs[0]["id"]),
                               headers={"accept": "application/vnd.github.v3+json",
-                                       "authorization": "Bearer {}".format(os.getenv("GITHUB_TOKEN"))},
+                                       "authorization": "Token {}".format(os.getenv("GITHUB_TOKEN"))},
                               method="POST")
         try:
             res = request.urlopen(req)
