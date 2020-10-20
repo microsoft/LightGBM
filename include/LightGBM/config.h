c7890dce9b176ce8014fc24301d171ea28af230a
Pavel Metrikov <46672636+metpavel@users.noreply.github.com>
2020-10-20 22:43:53 +0300

    Update config.h
    add description to lambdarank_truncation_level parameter

--- include/LightGBM/config.h
+++ include/LightGBM/config.h
@@ -835,7 +835,8 @@ struct Config {
 
   // check = >0
   // desc = used only in ``lambdarank`` application
-  // desc = used for truncating the max DCG, refer to "truncation level" in the Sec. 3 of `LambdaMART paper <https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/MSR-TR-2010-82.pdf>`__
+  // desc = controls the number of top-results to focus on during training, refer to "truncation level" in the Sec. 3 of `LambdaMART paper <https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/MSR-TR-2010-82.pdf>`
+  // desc = is closely related to the desirable cutoff k in the metric NDCG@k that we aim at optimizing the ranker for. The optimal setting for this parameter is likely to be slightly higher than k (e.g., k + 3) to include more pairs of documents to train on, but perhaps not too high to avoid deviating too much from the desired target metric NDCG@k
   int lambdarank_truncation_level = 20;
 
   // desc = used only in ``lambdarank`` application
