3a5fe6094083d4603586ebe579ba5d0342106917
James Lamb <jaylamb20@gmail.com>
2020-10-28 00:56:33 -0500

    fix lru_cache backport

--- tests/python_package_test/utils.py
+++ tests/python_package_test/utils.py
@@ -2,14 +2,16 @@ try:
     from functools import lru_cache
 except ImportError:
     import warnings
-    from functools import wraps
     warnings.warn("Could not import functools.lru_cache", RuntimeWarning)
 
-    def lru_cache(user_function, maxsize=None):
-        @wraps(user_function)
-        def wrapper(*args, **kwargs):
-            arg_key = tuple(args, [item for item in kwargs.items()])
-            if arg_key not in cache:
-                cache[arg_key] = user_function(*args)
-            return cache[arg_key]
-        return wrapper
+    def lru_cache(maxsize=None):
+        cache = {}
+
+        def _lru_wrapper(user_function):
+            def wrapper(*args, **kwargs):
+                arg_key = (args, tuple([item for item in kwargs.items()]))
+                if arg_key not in cache:
+                    cache[arg_key] = user_function(*args)
+                return cache[arg_key]
+            return wrapper
+        return _lru_wrapper
