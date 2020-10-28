3394222408a2ca004c3c4294aa7f9f844bf72c37
James Lamb <jaylamb20@gmail.com>
2020-10-27 23:09:13 -0500

    fix imports

--- tests/python_package_test/utils.py
+++ tests/python_package_test/utils.py
@@ -1,8 +1,8 @@
-
 try:
     from functools import lru_cache
 except ImportError:
     import warnings
+    from functools import wraps
     warnings.warn("Could not import functools.lru_cache", RuntimeWarning)
 
     def lru_cache(user_function, maxsize=None):
