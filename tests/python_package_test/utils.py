c1cb1b26492ef4c393dc84f6e902ee0863b6cf83
James Lamb <jaylamb20@gmail.com>
2020-10-28 10:10:19 -0500

    missing kwargs

--- tests/python_package_test/utils.py
+++ tests/python_package_test/utils.py
@@ -11,7 +11,7 @@ except ImportError:
             def wrapper(*args, **kwargs):
                 arg_key = (args, tuple([item for item in kwargs.items()]))
                 if arg_key not in cache:
-                    cache[arg_key] = user_function(*args)
+                    cache[arg_key] = user_function(*args, **kwargs)
                 return cache[arg_key]
             return wrapper
         return _lru_wrapper
