9f4c4535d0f10360d5f3463ad70c3a7a43b68a4b
TP Boudreau <tpboudreau@gmail.com>
2020-11-24 19:44:59 +0000

    Remove unused expected failure test

--- tests/python_package_test/test_dual.py
+++ tests/python_package_test/test_dual.py
@@ -19,7 +19,6 @@ def test_cpu_works():
     validation_data = train_data = lgb.Dataset(data, label=label)
 
     param = {"num_leaves": 31, "objective": "binary", "device": "cpu"}
-    # This will raise an exception if it's an unsupported device:
     lgb.train(param, train_data, 10, valid_sets=[validation_data])
 
 
@@ -33,14 +32,5 @@ def test_gpu_works():
     label = np.random.randint(2, size=500)
     validation_data = train_data = lgb.Dataset(data, label=label)
 
-    try:
-        param = {"num_leaves": 31, "objective": "binary", "device": "gpu"}
-        lgb.train(param, train_data, 10, valid_sets=[validation_data])
-    except LightGBMError as e:
-        if str(e) == "No OpenCL device found":
-            # This is fine, it means there's no OpenCL device available,
-            # and OpenCL device is only searched for if we successfully
-            # loaded OpenCL GPU backend.
-            pass
-        else:
-            raise
+    param = {"num_leaves": 31, "objective": "binary", "device": "gpu"}
+    lgb.train(param, train_data, 10, valid_sets=[validation_data])
