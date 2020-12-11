41915ded468eba430ebca5f8eaa2ae61517b1928
TP Boudreau <tpboudreau@gmail.com>
2020-12-11 16:03:16 +0000

    eliminate variable from command

--- .ci/install_opencl.ps1
+++ .ci/install_opencl.ps1
@@ -1,27 +1,25 @@
 
-Write-Output "Agent platform information:"
-Get-WmiObject -Class Win32_ComputerSystem
-Get-WmiObject -Class Win32_Processor
-Get-WmiObject -Class Win32_BIOS
-
 if ($env:TASK -eq "bdist") {
-  Write-Output "Installing OpenCL"
+  Write-Output "Installing OpenCL CPU platform"
+
+  Write-Output "Agent platform information:"
+  Get-WmiObject -Class Win32_ComputerSystem
+  Get-WmiObject -Class Win32_Processor
+  Get-WmiObject -Class Win32_BIOS
+
   Write-Output "Downloading OpenCL runtime"
-  #curl -o AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe http://amd-dev.wpengine.netdna-cdn.com/app-sdk/installers/APPSDKInstaller/3.0.130.135-GA/full/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
-  $installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
-  curl --ipv4 --tlsv1.2 --output $installer https://gamma-rho.com/$installer
+  Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe -Uri https://gamma-rho.com/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
   Write-Output "Installing OpenCL runtime"
-  #Invoke-Command -ScriptBlock {Start-Process ".\AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe" -ArgumentList '/S /V"/quiet /norestart /passive /log amd_opencl_sdk.log"' -Wait}
-  Invoke-Command -ScriptBlock {Start-Process .\$installer -ArgumentList '/S /V"/quiet /norestart /passive /log amd_opencl_sdk.log"' -Wait}
+  Invoke-Command -ScriptBlock {Start-Process 'AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe' -ArgumentList '/S /V"/quiet /norestart /passive /log amd_opencl_sdk.log"' -Wait}
 
   $property = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenCL\Vendors
   if ($property -eq $null) {
-    Write-Output "Unable to install OpenCL runtime"
+    Write-Output "Unable to install OpenCL CPU platform"
     Write-Output "Setting EXIT"
     $host.SetShouldExit(-1)
     Exit -1
   } else {
-    Write-Output "Successfully installed OpenCL runtime"
+    Write-Output "Successfully installed OpenCL CPU platform"
     Write-Output "Current OpenCL drivers:"
     Write-Output $property
   }
