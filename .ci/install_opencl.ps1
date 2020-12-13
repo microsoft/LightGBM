817065c130e39220276415fada6581dd6f13a90e
TP Boudreau <tpboudreau@gmail.com>
2020-12-13 02:40:04 +0000

    rerun to ensure pipeline cache hit

--- .ci/install_opencl.ps1
+++ .ci/install_opencl.ps1
@@ -8,6 +8,7 @@ Get-WmiObject -Class Win32_BIOS
 
 $cache = "$env:PIPELINE_WORKSPACE\opencl_windows-amd_cpu-v3_0_130_135"
 $installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
+
 Write-Output "Running OpenCL installer"
 Invoke-Command -ScriptBlock {Start-Process "$cache\$installer" -ArgumentList '/S /V"/quiet /norestart /passive /log opencl.log"' -Wait}
 
