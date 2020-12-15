761a48c87c3f922325f667f2f9e73e7d945abe0a
TP Boudreau <tpboudreau@gmail.com>
2020-12-15 20:25:30 +0000

    reinsert pipeline cache

--- .ci/install_opencl.ps1
+++ .ci/install_opencl.ps1
@@ -1,24 +1,43 @@
 
 Write-Output "Installing OpenCL CPU platform"
-Write-Output "Agent platform information:"
-Get-WmiObject -Class Win32_ComputerSystem
-Get-WmiObject -Class Win32_Processor
-Get-WmiObject -Class Win32_BIOS
 
+$cache = "$env:PIPELINE_WORKSPACE\opencl_windows-amd_cpu-v3_0_130_135"
 $installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
 
-Write-Output "Downloading OpenCL installer"
-Invoke-WebRequest -OutFile "$installer" -Uri "https://github.com/microsoft/LightGBM/releases/download/v2.0.12/$installer"
-if (-Not (Test-Path ".\$installer")) {
-  Write-Output "Unable to download OpenCL installer"
-  Write-Output "Unable to install OpenCL CPU platform"
-  Write-Output "Setting EXIT"
-  $host.SetShouldExit(-1)
-  Exit -1
+if ($env:OPENCL_INSTALLER_FOUND -ne 'true') {
+  # Pipeline cache miss; download OpenCL platform installer executable
+  # into workspace cache
+
+  Write-Output "Downloading OpenCL platform installer"
+  $parts = @("1", "2", "3", "4", "5", "6", "7", "8", "9", "EXE")
+  foreach ($p in $parts) {
+    Write-Output " - downloading part $($p)"
+    Invoke-WebRequest -OutFile "$installer.$p" -Uri "https://github.com/microsoft/LightGBM/releases/download/v2.0.12/$installer.$p"
+  }
+
+  Write-Output "Reassembling OpenCL platform installer"
+  Start-Process "$installer.EXE" -Wait
+  Start-Sleep -Seconds 10
+
+  Write-Output "Caching OpenCL platform installer"
+  New-Item $cache -ItemType Directory | Out-Null
+  Move-Item -Path "$installer" -Destination "$cache\$installer" | Out-Null
+
+  if (Test-Path "$cache\$installer") {
+    Write-Output "Successfully downloaded OpenCL platform installer"
+  } else {
+    Write-Output "Unable to download OpenCL platform installer"
+    Write-Output "Setting EXIT"
+    $host.SetShouldExit(-1)
+    Exit -1
+  }
 }
 
+# Install OpenCL platform from installer executable expected in workspace cache
+
 Write-Output "Running OpenCL installer"
-Invoke-Command -ScriptBlock {Start-Process "$installer" -ArgumentList '/S /V"/quiet /norestart /passive /log opencl.log"' -Wait}
+Invoke-Command -ScriptBlock {Start-Process "$cache\$installer" -ArgumentList '/S /V"/quiet /norestart /passive /log opencl.log"' -Wait}
+
 $property = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenCL\Vendors
 if ($property -eq $null) {
   Write-Output "Unable to install OpenCL CPU platform"
