dd0310c513fc54a8d723302a92404abf85561c12
James Lamb <jaylamb20@gmail.com>
2021-01-10 22:18:21 -0600

    more graphviz

--- .ci/test.sh
+++ .ci/test.sh
@@ -72,7 +72,7 @@ conda install -q -y -n $CONDA_ENV dask dask-ml distributed joblib matplotlib num
 
 # graphviz must come from conda-forge to avoid this on some linux distros:
 # https://github.com/conda-forge/graphviz-feedstock/issues/18
-conda install -c conda-forge -y python-graphviz
+conda install -q -y -n $CONDA_ENV -c conda-forge python-graphviz
 
 if [[ $OS_NAME == "macos" ]] && [[ $COMPILER == "clang" ]]; then
     # fix "OMP: Error #15: Initializing libiomp5.dylib, but found libomp.dylib already initialized." (OpenMP library conflict due to conda's MKL)
