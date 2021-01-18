17540e39a3069c61ae60e058cfe5e1ee80fd7b99
Nikita Titov <nekit94-08@mail.ru>
2021-01-18 18:15:53 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -220,7 +220,6 @@ def generate_doxygen_xml(app):
         "SKIP_FUNCTION_MACROS=NO",
         "SORT_BRIEF_DOCS=YES",
         "WARN_AS_ERROR=YES",
-        "EXCLUDE_PATTERNS=*/eigen/*"
     ]
     doxygen_input = '\n'.join(doxygen_args)
     doxygen_input = bytes(doxygen_input, "utf-8")
