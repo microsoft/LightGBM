071caeb6207e362f71537673af4b01b8d0b87f30
Nikita Titov <nekit94-08@mail.ru>
2021-01-29 02:47:43 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -80,9 +80,13 @@ else  # Linux
         echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
         apt-get update
         apt-get install --no-install-recommends -y \
-            cmake \
             curl \
             wget
+        curl -sL https://apt.kitware.com/keys/kitware-archive-latest.asc | apt-key add -
+        apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -rs) main" -y
+        apt-get update
+        apt-get install --no-install-recommends -y \
+            cmake
     fi
     if [[ $SETUP_CONDA != "false" ]]; then
         wget -q -O conda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
