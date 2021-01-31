be2fe2f5546ebcfd78c47b9fe1c994a16bf17524
Nikita Titov <nekit94-08@mail.ru>
2021-01-31 18:18:06 +0300

    [python] Start supporting Python 3.9 (#3693)
    * start supporting Python 3.9
    
    * Update .vsts-ci.yml
    
    * Update .vsts-ci.yml
    
    * Update .appveyor.yml
    
    * Update .vsts-ci.yml
    
    * Update .appveyor.yml
    
    * fixed conflicts
    
    * continue

--- python-package/setup.py
+++ python-package/setup.py
@@ -374,4 +374,5 @@ if __name__ == "__main__":
                        'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7',
                        'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
                        'Topic :: Scientific/Engineering :: Artificial Intelligence'])
