9a17f690abd7d55ac9c9add94d5b8251cddbf0d4
James Lamb <jaylamb20@gmail.com>
2021-02-01 20:35:56 -0600

    actually build docs

--- .ci/test.sh
+++ .ci/test.sh
@@ -19,12 +19,16 @@ source activate $CONDA_ENV
 cd $BUILD_DIRECTORY
 
 if [[ $TASK == "check-docs" ]]; then
+    conda install -q -y -n $CONDA_ENV -c conda-forge doxygen
     pip install --user -r $BUILD_DIRECTORY/docs/requirements.txt rstcheck
     # check reStructuredText formatting
     cd $BUILD_DIRECTORY/python-package
     rstcheck --report warning `find . -type f -name "*.rst"` || exit -1
     cd $BUILD_DIRECTORY/docs
     rstcheck --report warning --ignore-directives=autoclass,autofunction,doxygenfile `find . -type f -name "*.rst"` || exit -1
+    cd $BUILD_DIRECTORY/docs
+    # build docs
+    make html || exit -1
     # check the consistency of parameters' descriptions and other stuff
     cp $BUILD_DIRECTORY/docs/Parameters.rst $BUILD_DIRECTORY/docs/Parameters-backup.rst
     cp $BUILD_DIRECTORY/src/io/config_auto.cpp $BUILD_DIRECTORY/src/io/config_auto-backup.cpp
