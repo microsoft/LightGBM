e9f50a5940390bfbd1aa340399a0f7d02e111352
James Lamb <jaylamb20@gmail.com>
2021-03-16 18:10:47 -0500

    [dask] remove unused imports from typing (#4079)

--- python-package/lightgbm/dask.py
+++ python-package/lightgbm/dask.py
@@ -9,7 +9,7 @@ It is based on dask-lightgbm, which was based on dask-xgboost.
 import socket
 from collections import defaultdict
 from copy import deepcopy
-from typing import Any, Callable, Dict, Iterable, List, Optional, Set, Type, Union
+from typing import Any, Callable, Dict, List, Optional, Type, Union
 from urllib.parse import urlparse
 
 import numpy as np
