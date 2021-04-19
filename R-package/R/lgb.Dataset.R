139a4943c966b935d0ef416ebd60986a95ec4add
James Lamb <jaylamb20@gmail.com>
2021-04-19 10:45:08 -0500

    linting

--- R-package/R/lgb.Dataset.R
+++ R-package/R/lgb.Dataset.R
@@ -730,8 +730,10 @@ Dataset <- R6::R6Class(
 #' @description Construct \code{lgb.Dataset} object from dense matrix, sparse matrix
 #'              or local file (that was created previously by saving an \code{lgb.Dataset}).
 #' @param data a \code{matrix} object, a \code{dgCMatrix} object or a character representing a filename
-#' @param params a list of parameters. See \href{https://lightgbm.readthedocs.io/en/latest/Parameters.html#dataset-parameters}{
-#'               The "Dataset Parameters" section of the documentation} for a list of parameters and valid values.
+#' @param params a list of parameters. See
+#'               \href{https://lightgbm.readthedocs.io/en/latest/Parameters.html#dataset-parameters}{
+#'               The "Dataset Parameters" section of the documentation} for a list of parameters
+#'               and valid values.
 #' @param reference reference dataset. When LightGBM creates a Dataset, it does some preprocessing like binning
 #'                  continuous features into histograms. If you want to apply the same bin boundaries from an existing
 #'                  dataset to new \code{data}, pass that existing Dataset to this argument.
@@ -742,8 +744,8 @@ Dataset <- R6::R6Class(
 #' @param free_raw_data LightGBM constructs its data format, called a "Dataset", from tabular data.
 #'                      By default, that Dataset object on the R side does not keep a copy of the raw data.
 #'                      This reduces LightGBM's memory consumption, but it means that the Dataset object
-#'                      cannot be changed after it has been constructed. If you'd prefer to be able to change the Dataset
-#'                      object after construction, set \code{free_raw_data = FALSE}.
+#'                      cannot be changed after it has been constructed. If you'd prefer to be able to
+#'                      change the Dataset object after construction, set \code{free_raw_data = FALSE}.
 #' @param info a list of information of the \code{lgb.Dataset} object
 #' @param ... other information to pass to \code{info} or parameters pass to \code{params}
 #'
