66ec3ca6d2f8b054124998c84ea59ad748b636ef
Willian Z <Willian@Willian-Zhang.com>
2021-04-21 16:44:37 +0800

    ci(regression): fix color on gitlab ci

--- .ci/regression/verify_result.sh
+++ .ci/regression/verify_result.sh
@@ -11,11 +11,26 @@ if [ $(ls ref_data/* | wc -l) -lt 1 ] ; then
     exit 1
 fi
 
-_GREEN=$(tput setaf 2)
-_BLUE=$(tput setaf 4)
-_RED=$(tput setaf 1)
-_RESET=$(tput sgr0)
-_BOLD=$(tput bold)
+colorize() {
+    # prefer terminal safe colored and bold text when tput is supported
+    if tput setaf 0 &>/dev/null; then
+        _RESET="$(tput sgr0)"
+        _BOLD="$(tput bold)"
+        _BLUE="${BOLD}$(tput setaf 4)"
+        _GREEN="${BOLD}$(tput setaf 2)"
+        _RED="${BOLD}$(tput setaf 1)"
+        _YELLOW="${BOLD}$(tput setaf 3)"
+    else
+        _RESET="\e[0m"
+        _BOLD="\e[1m"
+        _BLUE="${BOLD}\e[34m"
+        _GREEN="${BOLD}\e[32m"
+        _RED="${BOLD}\e[31m"
+        _YELLOW="${BOLD}\e[33m"
+    fi
+    readonly _RESET _BOLD _BLUE _GREEN _RED _YELLOW
+}
+colorize;
 
 exit_code=0
 for script in *.sh; do
