cc710457c61b926c73ead151f7dd96d31799b57a
Chen Yufei <cyfdecyf@gmail.com>
2021-04-23 20:01:10 +0800

    test: add test for Common::AtofPrecise.

---
+++ tests/cpp_tests/test_common.cpp
@@ -0,0 +1,100 @@
+#include <gtest/gtest.h>
+
+#include <limits>
+
+#include "../include/LightGBM/utils/common.h"
+
+
+class AtofPreciseTest : public testing::Test {
+ public:
+  struct AtofTestCase {
+    const char* data;
+    double expected;
+  };
+
+  static double TestAtofPrecise(
+      const char* data, double expected, bool test_eq = true) {
+    double got = 0;
+    const char* end = LightGBM::Common::AtofPrecise(data, &got);
+    EXPECT_TRUE(end != data) << "fail to parse: " << data;
+    EXPECT_EQ(*end, '\0') << "not parsing to end: " << data;
+    if (test_eq) {
+      EXPECT_EQ(expected, got) << "parse string: " << data;
+    }
+    return got;
+  }
+};
+
+TEST_F(AtofPreciseTest, Basic) {
+  AtofTestCase test_cases[] = {
+      { "0", 0.0 },
+      { "0E0", 0.0 },
+      { "-0E0", 0.0 },
+      { "-0", -0.0 },
+      { "1", 1.0 },
+      { "1E0", 1.0 },
+      { "-1", -1.0 },
+      { "-1E0", -1.0 },
+      { "123456.0", 123456.0 },
+      { "432E1", 432E1 },
+      { "1.2345678", 1.2345678 },
+      { "2.4414062E-4", 2.4414062E-4 },
+      { "3.0540412E5", 3.0540412E5 },
+      { "3.355445E7", 3.355445E7 },
+      { "1.1754944E-38", 1.1754944E-38 },
+  };
+
+  for (auto const& test : test_cases) {
+    TestAtofPrecise(test.data, test.expected);
+  }
+}
+
+TEST_F(AtofPreciseTest, NaN) {
+  AtofTestCase test_cases[] = {
+      { "nan", std::numeric_limits<double>::quiet_NaN() },
+      { "NaN", std::numeric_limits<double>::quiet_NaN() },
+      { "NAN", std::numeric_limits<double>::quiet_NaN() },
+      // The behavior for parsing -nan depends on implementation.
+      // Thus we skip binary check for negative nan.
+      { "-nan", -std::numeric_limits<double>::quiet_NaN() },
+      { "-NaN", -std::numeric_limits<double>::quiet_NaN() },
+      { "-NAN", -std::numeric_limits<double>::quiet_NaN() },
+  };
+
+  for (auto const& test : test_cases) {
+    double got = TestAtofPrecise(test.data, test.expected, false);
+
+    EXPECT_TRUE(std::isnan(got)) << "not parsed as NaN: " << test.data;
+    if (got > 0) {
+      // See comment in test_cases.
+      EXPECT_EQ(memcmp(&got, &test.expected, sizeof(test.expected)), 0)
+                << "parsed NaN is not the same for every bit: " << test.data ;
+    }
+  }
+}
+
+// From https://stackoverflow.com/a/4609795/306935
+template <typename T>
+int sign(T val) {
+  return (T(0) < val) - (val < T(0));
+}
+
+TEST_F(AtofPreciseTest, Inf) {
+  AtofTestCase test_cases[] = {
+      { "inf", std::numeric_limits<double>::infinity() },
+      { "Inf", std::numeric_limits<double>::infinity() },
+      { "INF", std::numeric_limits<double>::infinity() },
+      { "-inf", -std::numeric_limits<double>::infinity() },
+      { "-Inf", -std::numeric_limits<double>::infinity() },
+      { "-INF", -std::numeric_limits<double>::infinity() },
+  };
+
+  for (auto const& test : test_cases) {
+    double got = TestAtofPrecise(test.data, test.expected, false);
+
+    EXPECT_EQ(sign(test.expected), sign(got)) << "sign differs parsing: " << test.data;
+    EXPECT_TRUE(std::isinf(got)) << "not parsed as infinite: " << test.data;
+    EXPECT_EQ(memcmp(&got, &test.expected, sizeof(test.expected)), 0)
+              << "parsed infinite is not the same for every bit: " << test.data ;
+  }
+}
