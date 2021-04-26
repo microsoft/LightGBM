839ee4e906849676a5323138cf630ed3e854629b
Willian Zhang <willian@willian-zhang.com>
2021-04-26 14:52:02 +0800

    ci(regression): fix regression data precision

--- .ci/regression/dataset_from_examples/gen_bin.py
+++ .ci/regression/dataset_from_examples/gen_bin.py
@@ -6,8 +6,8 @@ data_path = sys.argv[1]
 
 print('Loading data...')
 # load or create your dataset
-df_train = pd.read_csv('../../../examples/regression/regression.train', header=None, sep='\t')
-df_test = pd.read_csv('../../../examples/regression/regression.test', header=None, sep='\t')
+df_train = pd.read_csv('../../../examples/regression/regression.train', header=None, sep='\t', float_precision='round_trip')
+df_test = pd.read_csv('../../../examples/regression/regression.test', header=None, sep='\t', float_precision='round_trip')
 
 y_train = df_train[0]
 y_test = df_test[0]
