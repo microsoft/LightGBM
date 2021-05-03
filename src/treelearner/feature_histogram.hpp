7702a15a6249838106b98559b1fb52e8306a4ebe
Andrew Ziem <ahz001@gmail.com>
2021-05-03 16:07:15 -0600

    Clarify a phrase in a comment
    Co-authored-by: James Lamb <jaylamb20@gmail.com>

--- src/treelearner/feature_histogram.hpp
+++ src/treelearner/feature_histogram.hpp
@@ -359,7 +359,7 @@ class FeatureHistogram {
           continue;
         }
 
-        // mark to is as able to be split
+        // mark as able to be split
         is_splittable_ = true;
         // better split point
         if (current_gain > best_gain) {
