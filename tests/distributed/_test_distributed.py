f5e9d496715cf409f5bdf38e38092e72d0764a3d
José MZ <jmoralz92@gmail.com>
2021-05-26 21:47:17 -0500

    write predict.conf in the predict method. more robust port setup. use subprocess.run and check returncode

--- tests/distributed/_test_distributed.py
+++ tests/distributed/_test_distributed.py
@@ -1,14 +1,23 @@
 import copy
+import io
 import subprocess
-import sys
 from concurrent.futures import ThreadPoolExecutor
-from typing import Dict, List
+from typing import Dict, Generator, List
 
 import numpy as np
 from sklearn.datasets import make_blobs, make_regression
 from sklearn.metrics import accuracy_score
 
 import lightgbm as lgb
+
+
+def _generate_n_ports(n: int) -> Generator[int, None, None]:
+    return (lgb.dask._find_random_open_port() for _ in range(n))
+
+
+def _write_dict(d: Dict, file: io.TextIOWrapper) -> None:
+    for k, v in d.items():
+        file.write(f'{k} = {v}\n')
 
 
 def create_data(task: str, n_samples: int = 1_000) -> np.ndarray:
@@ -25,22 +34,10 @@ def create_data(task: str, n_samples: int = 1_000) -> np.ndarray:
     return dataset
 
 
-def run_and_log(cmd: List[str]) -> None:
-    """Run `cmd` in another process and pipe its logs to this process' stdout."""
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    assert process.stdout is not None
-
-    def stdout_stream():
-        return process.stdout.read(1)
-
-    for c in iter(stdout_stream, b''):
-        sys.stdout.buffer.write(c)
-
-
 class DistributedMockup:
     """Simulate distributed training."""
 
-    default_config = {
+    default_train_config = {
         'task': 'train',
         'pre_partition': True,
         'machine_list_file': 'mlist.txt',
@@ -52,24 +49,34 @@ class DistributedMockup:
         'num_threads': 2,
     }
 
-    def __init__(self, config: Dict = {}):
-        self.config = copy.deepcopy(self.default_config)
-        self.config.update(config)
-        self.n_workers = self.config['num_machines']
+    default_predict_config = {
+        'task': 'predict',
+        'data': 'train.txt',
+        'input_model': 'model0.txt',
+        'output_result': 'predictions.txt',
+    }
 
-    def worker_train(self, i: int) -> None:
+    def worker_train(self, i: int) -> subprocess.CompletedProcess:
         """Start the training process on the `i`-th worker.
 
         If this is the first worker, its logs are piped to stdout.
         """
         cmd = f'./lightgbm config=train{i}.conf'.split()
-        if i == 0:
-            return run_and_log(cmd)
-        subprocess.run(cmd)
+        return subprocess.run(cmd)
 
     def _set_ports(self) -> None:
         """Randomly assign a port for training to each worker and save all ports to mlist.txt."""
-        self.listen_ports = [lgb.dask._find_random_open_port() for _ in range(self.n_workers)]
+        ports = set(_generate_n_ports(self.n_workers))
+        i = 0
+        max_tries = 100
+        while i < max_tries and len(ports) < self.n_workers:
+            n_ports_left = self.n_workers - len(ports)
+            candidates = _generate_n_ports(n_ports_left)
+            ports.update(candidates)
+            i += 1
+        if i == max_tries:
+            raise RuntimeError('Unable to find non-colliding ports.')
+        self.listen_ports = list(ports)
         with open('mlist.txt', 'wt') as f:
             for port in self.listen_ports:
                 f.write(f'127.0.0.1 {port}\n')
@@ -81,36 +88,49 @@ class DistributedMockup:
         for i, partition in enumerate(partitions):
             np.savetxt(f'train{i}.txt', partition, delimiter=',')
 
-    def fit(self, partitions: List[np.ndarray]) -> None:
+    def fit(self, partitions: List[np.ndarray], train_config: Dict = {}) -> None:
         """Run the distributed training process on a single machine.
 
         For each worker i:
-            1. The i-th partition is saved as train{i}.txt
+            1. The i-th partition is saved as train{i}.txt.
             2. A random port is assigned for training.
             3. A configuration file train{i}.conf is created.
             4. The lightgbm binary is called with config=train{i}.conf in another thread.
             5. The trained model is saved as model{i}.txt. Each model file only differs in data and local_listen_port.
         The whole training set is saved as train.txt and the logs from the first worker are piped to stdout.
         """
+        self.train_config = copy.deepcopy(self.default_train_config)
+        self.train_config.update(train_config)
+        self.n_workers = self.train_config['num_machines']
+        self._set_ports()
         self._write_data(partitions)
         self.label_ = np.hstack([partition[:, 0] for partition in partitions])
-        self._set_ports()
         futures = []
         with ThreadPoolExecutor(max_workers=self.n_workers) as executor:
             for i in range(self.n_workers):
                 self.write_train_config(i)
-                futures.append(executor.submit(self.worker_train, i))
-            _ = [f.result() for f in futures]
+                train_future = executor.submit(self.worker_train, i)
+                futures.append(train_future)
+            results = [f.result() for f in futures]
+        for result in results:
+            if result.returncode != 0:
+                raise RuntimeError
 
-    def predict(self) -> np.ndarray:
+    def predict(self, predict_config: Dict = {}) -> np.ndarray:
         """Compute the predictions using the model created in the fit step.
 
         model0.txt is used to predict the training set train.txt using predict.conf.
         The predictions are saved as predictions.txt and are then loaded to return them as a numpy array.
         The logs are piped to stdout.
         """
+        self.predict_config = copy.deepcopy(self.default_predict_config)
+        self.predict_config.update(predict_config)
+        with open('predict.conf', 'wt') as file:
+            _write_dict(self.predict_config, file)
         cmd = './lightgbm config=predict.conf'.split()
-        run_and_log(cmd)
+        result = subprocess.run(cmd)
+        if result.returncode != 0:
+            raise RuntimeError
         y_pred = np.loadtxt('predictions.txt')
         return y_pred
 
@@ -120,12 +140,11 @@ class DistributedMockup:
         Each worker gets a different port and piece of the data, the rest are the
         model parameters contained in `self.config`.
         """
-        with open(f'train{i}.conf', 'wt') as f:
-            f.write(f'output_model = model{i}.txt\n')
-            f.write(f'local_listen_port = {self.listen_ports[i]}\n')
-            f.write(f'data = train{i}.txt\n')
-            for param, value in self.config.items():
-                f.write(f'{param} = {value}\n')
+        with open(f'train{i}.conf', 'wt') as file:
+            file.write(f'output_model = model{i}.txt\n')
+            file.write(f'local_listen_port = {self.listen_ports[i]}\n')
+            file.write(f'data = train{i}.txt\n')
+            _write_dict(self.train_config, file)
 
 
 def test_classifier():
@@ -133,12 +152,12 @@ def test_classifier():
     num_machines = 2
     data = create_data(task='binary-classification')
     partitions = np.array_split(data, num_machines)
-    params = {
+    train_params = {
         'objective': 'binary',
         'num_machines': num_machines,
     }
-    clf = DistributedMockup(params)
-    clf.fit(partitions)
+    clf = DistributedMockup()
+    clf.fit(partitions, train_params)
     y_probas = clf.predict()
     y_pred = y_probas > 0.5
     assert accuracy_score(clf.label_, y_pred) == 1.
@@ -149,11 +168,11 @@ def test_regressor():
     num_machines = 2
     data = create_data(task='regression')
     partitions = np.array_split(data, num_machines)
-    params = {
+    train_params = {
         'objective': 'regression',
         'num_machines': num_machines,
     }
-    reg = DistributedMockup(params)
-    reg.fit(partitions)
+    reg = DistributedMockup()
+    reg.fit(partitions, train_params)
     y_pred = reg.predict()
     np.testing.assert_allclose(y_pred, reg.label_, rtol=0.2, atol=50.)
