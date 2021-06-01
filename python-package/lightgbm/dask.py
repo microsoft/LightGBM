88f3e004d1d8885167b004a56b91c357a207d6d0
Frank Fineis <frank.fineis@gmail.com>
2021-06-01 10:27:22 -0500

    Update python-package/lightgbm/dask.py
    Co-authored-by: Nikita Titov <nekit94-08@mail.ru>

--- python-package/lightgbm/dask.py
+++ python-package/lightgbm/dask.py
@@ -148,7 +148,6 @@ def _train_part(
     local_eval_group = None
 
     if n_evals:
-
         has_eval_sample_weight = any([x.get('eval_sample_weight') is not None for x in list_of_parts])
         has_eval_init_score = any([x.get('eval_init_score') is not None for x in list_of_parts])
 
