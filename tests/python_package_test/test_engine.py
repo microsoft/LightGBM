550608e956db9ac36e220a3cc6fb3bd5e9e154e1
Belinda Trotta <btrotta@users.noreply.github.com>
2021-06-01 17:59:17 +1000

    Update tests/python_package_test/test_engine.py
    Co-authored-by: Nikita Titov <nekit94-08@mail.ru>

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -2812,5 +2812,9 @@ def test_dump_model():
     params['linear_tree'] = True
     train_data = lgb.Dataset(X, label=y)
     bst = lgb.train(params, train_data, num_boost_round=5)
-    dumped_model = bst.dump_model(5, 0)
-    assert "leaf_coeff" in str(dumped_model)
+    dumped_model_str = str(bst.dump_model(5, 0))
+    assert "leaf_features" in dumped_model_str
+    assert "leaf_coeff" in dumped_model_str
+    assert "leaf_const" in dumped_model_str
+    assert "leaf_value" in dumped_model_str
+    assert "leaf_count" in dumped_model_str
