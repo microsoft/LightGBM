f86220d433541d3b05e725eec36913118e9fdc49
James Lamb <jaylamb20@gmail.com>
2021-06-16 00:31:59 -0500

    isort

--- python-package/lightgbm/dask.py
+++ python-package/lightgbm/dask.py
@@ -17,9 +17,9 @@ import numpy as np
 import scipy.sparse as ss
 
 from .basic import _LIB, LightGBMError, _choose_param_value, _ConfigAliases, _log_info, _log_warning, _safe_call
-from .compat import (DASK_INSTALLED, PANDAS_INSTALLED, SKLEARN_INSTALLED, Client, LGBMNotFittedError, concat, dask_bag_from_delayed,
-                     dask_Array, dask_array_concatenate, dask_array_from_delayed, dask_DataFrame, dask_Series, default_client,
-                     delayed, pd_DataFrame, pd_Series, wait)
+from .compat import (DASK_INSTALLED, PANDAS_INSTALLED, SKLEARN_INSTALLED, Client, LGBMNotFittedError, concat,
+                     dask_Array, dask_array_concatenate, dask_array_from_delayed, dask_bag_from_delayed, dask_DataFrame,
+                     dask_Series, default_client, delayed, pd_DataFrame, pd_Series, wait)
 from .sklearn import LGBMClassifier, LGBMModel, LGBMRanker, LGBMRegressor, _lgbmmodel_doc_fit, _lgbmmodel_doc_predict
 
 _DaskCollection = Union[dask_Array, dask_DataFrame, dask_Series]
