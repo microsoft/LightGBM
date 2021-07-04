184aaa0059e73a543f9b5d1f116f638fd640d475
Nikita Titov <nekit94-08@mail.ru>
2021-07-04 06:50:44 +0300

    [ci] ensure interactive_plot_example notebook is run in interactive mode at CI (#4432)

--- .ci/test_windows.ps1
+++ .ci/test_windows.ps1
@@ -112,5 +112,6 @@ if (($env:TASK -eq "regular") -or (($env:APPVEYOR -eq "true") -and ($env:TASK -e
     python $file ; Check-Output $?
   }  # run all examples
   cd $env:BUILD_SOURCESDIRECTORY/examples/python-guide/notebooks
+  (Get-Content "interactive_plot_example.ipynb").replace('INTERACTIVE = False', 'assert False, \"Interactive mode disabled\"') | Set-Content "interactive_plot_example.ipynb"
   jupyter nbconvert --ExecutePreprocessor.timeout=180 --to notebook --execute --inplace *.ipynb ; Check-Output $?  # run all notebooks
 }
