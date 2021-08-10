10deff608ebcd95b60abd310472bad181d9768e2
James Lamb <jaylamb20@gmail.com>
2021-08-09 21:20:47 -0500

    relatiev paths

--- .ci/test_r_package_solaris.sh
+++ .ci/test_r_package_solaris.sh
@@ -7,7 +7,7 @@ apt-get install --no-install-recommends -y \
   libxml2-dev \
   libssl-dev
 
-log_file="$GITHUB_WORKSPACE/rhub_logs.txt"
+log_file="rhub_logs.txt"
 Rscript -e "install.packages('rhub', dependencies = c('Depends', 'Imports', 'LinkingTo'), repos = 'https://cran.r-project.org', Ncpus = parallel::detectCores())" || exit -1
 
-Rscript $GITHUB_WORKSPACE/.ci/run_rhub_solaris_checks.R $(pwd)/lightgbm_*.tar.gz $log_file || exit -1
+Rscript ./.ci/run_rhub_solaris_checks.R lightgbm_*.tar.gz $log_file || exit -1
