18cbc567ee73db822119fadf02378b83e0d9b255
David Cortes <david.cortes.rivera@gmail.com>
2021-09-06 23:41:43 -0300

    move R_API_BEGIN to correct place

--- R-package/src/lightgbm_R.cpp
+++ R-package/src/lightgbm_R.cpp
@@ -751,13 +751,13 @@ SEXP LGBM_BoosterSaveModel_R(SEXP handle,
 SEXP LGBM_BoosterSaveModelToString_R(SEXP handle,
   SEXP num_iteration,
   SEXP feature_importance_type) {
+  R_API_BEGIN();
   SEXP model_str;
   int64_t out_len = 0;
   int64_t buf_len = 1024 * 1024;
   int num_iter = Rf_asInteger(num_iteration);
   int importance_type = Rf_asInteger(feature_importance_type);
   std::vector<char> inner_char_buf(buf_len);
-  R_API_BEGIN();
   CHECK_CALL(LGBM_BoosterSaveModelToString(R_ExternalPtrAddr(handle), 0, num_iter, importance_type, buf_len, &out_len, inner_char_buf.data()));
   // if the model string was larger than the initial buffer, allocate a bigger buffer and try again
   if (out_len > buf_len) {
