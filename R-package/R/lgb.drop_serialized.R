730f2e6fdc247999194ab310ebf7f5c038d530c9
david-cortes <david.cortes.rivera@gmail.com>
2021-10-15 18:50:36 +0300

    Update R-package/R/lgb.drop_serialized.R
    Co-authored-by: James Lamb <jaylamb20@gmail.com>

--- R-package/R/lgb.drop_serialized.R
+++ R-package/R/lgb.drop_serialized.R
@@ -10,7 +10,9 @@
 #' @seealso \link{lgb.restore_handle}, \link{lgb.make_serializable}.
 #' @export
 lgb.drop_serialized <- function(model) {
-  stopifnot(lgb.is.Booster(model))
+  if (!lgb.is.Booster(x = model)) { 
+    stop("lgb.drop_serialized: model should be an ", sQuote("lgb.Booster")) 
+  } 
   model$drop_raw()
   return(invisible(model))
 }
