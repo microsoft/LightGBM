2904014121970d78b9cab1d6a7d3445c559f26dd
hzy46 <email@example.com>
2021-10-26 17:36:16 +0800

    use omp_get_max_threads

--- include/LightGBM/utils/openmp_wrapper.h
+++ include/LightGBM/utils/openmp_wrapper.h
@@ -25,7 +25,7 @@ inline int OMP_NUM_THREADS() {
   return ret;
 }
 
-static const int default_omp_num_threads = omp_get_num_threads();
+static const int default_omp_num_threads = omp_get_max_threads();
 
 inline void omp_reset_num_threads() {
   omp_set_num_threads(default_omp_num_threads);
