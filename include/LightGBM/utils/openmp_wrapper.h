1598f97ce5642776b3c381166c4eb57bb5f03c0a
hzy46 <email@example.com>
2021-10-26 16:36:25 +0800

    fix

--- include/LightGBM/utils/openmp_wrapper.h
+++ include/LightGBM/utils/openmp_wrapper.h
@@ -25,7 +25,8 @@ inline int OMP_NUM_THREADS() {
   return ret;
 }
 
-static const int default_omp_num_threads = OMP_NUM_THREADS();
+static const int default_omp_num_threads = omp_get_num_threads();
+
 
 inline void omp_reset_num_threads() {
   omp_set_num_threads(default_omp_num_threads);
