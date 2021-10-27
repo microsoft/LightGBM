86c6d829b4234f8f666a8c4c832af48a6066b8db
skwwt <tong.wu@live.com>
2021-10-27 14:47:20 +0800

    fix warning

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -2889,8 +2889,9 @@ def test_dump_model_hook():
     assert "leaf_value" not in dumped_model_str
     assert "LV" in dumped_model_str
 
+
 def test_force_split_with_feature_fraction():
-    X, y = load_boston(return_X_y=True) 
+    X, y = load_boston(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
     lgb_train = lgb.Dataset(X_train, y_train)
 
@@ -2903,15 +2904,14 @@ def test_force_split_with_feature_fraction():
         f.write(json.dumps(forced_split))
 
     params = {
-            "objective": "regression",
-            "feature_fraction": 0.6,
-            "force_col_wise": True,
-            "feature_fraction_seed": 1,
-            "forcedsplits_filename": "forced_split.json"
-        }
+        "objective": "regression",
+        "feature_fraction": 0.6,
+        "force_col_wise": True,
+        "feature_fraction_seed": 1,
+        "forcedsplits_filename": "forced_split.json"
+    }
 
     gbm = lgb.train(params, lgb_train)
     ret = mean_absolute_error(y_test, gbm.predict(X_test))
 
     assert ret < 2.0
-    
