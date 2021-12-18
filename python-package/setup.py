e99b91cac9ae26ef6da707532041187cff303a17
Nikita Titov <nekit94-08@mail.ru>
2021-12-18 19:31:38 +0300

    Update setup.py

--- python-package/setup.py
+++ python-package/setup.py
@@ -379,4 +379,5 @@ if __name__ == "__main__":
                        'Programming Language :: Python :: 3.7',
                        'Programming Language :: Python :: 3.8',
                        'Programming Language :: Python :: 3.9',
+                       'Programming Language :: Python :: 3.10',
                        'Topic :: Scientific/Engineering :: Artificial Intelligence'])
