73d7db76785a5a4b13bacf4c1026dbeb6ba62b64
Guangda Liu <v-guangdaliu@microsoft.com>
2022-01-15 08:49:48 +0000

    Modify warnning about using goss as boosting type

--- src/boosting/sample_strategy.cpp
+++ src/boosting/sample_strategy.cpp
@@ -13,10 +13,7 @@ SampleStrategy* SampleStrategy::CreateSampleStrategy(const Config* config, const
   bool use_goss_as_boosting = config->boosting == std::string("goss");
   bool use_goss_as_strategy = config->data_sample_strategy == std::string("goss");
   if (use_goss_as_boosting) {
-    Log::Warning("Setting goss as `boosting_type` is not recommended. Please set `data_sample_strategy = goss` instead.");
-    if (use_goss_as_strategy) {
-      Log::Warning("Both `boosting_type` and `data_sample_strategy` are set as GOSS. Only one time of sampling will be conducted. Please check and modify your config file.");
-    }
+    Log::Warning("Found boosting_type=goss. For backwards compatibility reasons, LightGBM interprets this as boosting_type=gbdt, data_sample_strategy=goss. To suppress this warning, set data_sample_strategy=goss instead.");
   }
   if (use_goss_as_boosting || use_goss_as_strategy) {
     return new GOSS(config, train_data, num_tree_per_iteration);
