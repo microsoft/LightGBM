6a0b91b4dee356acacaeb156d230e0425d687d2c
Antoni Baum <antoni.baum@protonmail.com>
2022-02-17 10:09:37 +0100

    Fix

--- python-package/lightgbm/callback.py
+++ python-package/lightgbm/callback.py
@@ -309,7 +309,7 @@ def early_stopping(stopping_rounds: int, first_metric_only: bool = False, verbos
         def _final_iteration_check(self, env: CallbackEnv, eval_name_splitted: List[str], i: int) -> None:
             if env.iteration == env.end_iteration - 1:
                 if self.verbose:
-                    best_score_str = '\t'.join([_format_eval_result(x) for x in best_score_list[i]])
+                    best_score_str = '\t'.join([_format_eval_result(x) for x in self.best_score_list[i]])
                     _log_info('Did not meet early stopping. '
                             f'Best iteration is:\n[{self.best_iter[i] + 1}]\t{best_score_str}')
                     if self.first_metric_only:
