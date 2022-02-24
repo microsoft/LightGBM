72bceb434bbbff991d56a0c24272393b6fb762ea
Antoni Baum <antoni.baum@protonmail.com>
2022-02-24 17:06:55 +0000

    Add early stopping callback picklability test

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -18,7 +18,7 @@ from sklearn.model_selection import GroupKFold, TimeSeriesSplit, train_test_spli
 
 import lightgbm as lgb
 
-from .utils import load_boston, load_breast_cancer, load_digits, load_iris, make_synthetic_regression
+from .utils import load_boston, load_breast_cancer, load_digits, load_iris, make_synthetic_regression, pickle_obj, unpickle_obj
 
 decreasing_generator = itertools.count(0, -1)
 
@@ -3291,3 +3291,18 @@ def test_record_evaluation_with_cv(train_metric):
                 np.testing.assert_allclose(
                     cv_hist[key], eval_result[dataset][f'{metric}-{agg}']
                 )
+
+@pytest.mark.parametrize('serializer', ["pickle", "joblib", "cloudpickle"])
+def test_early_stopping_callback_is_picklable(serializer, tmp_path):
+    callback = lgb.early_stopping(stopping_rounds=5)
+    tmp_file = tmp_path / "early_stopping.pkl"
+    pickle_obj(
+        obj=callback,
+        filepath=tmp_file,
+        serializer=serializer
+    )
+    callback_from_disk = unpickle_obj(
+        filepath=tmp_file,
+        serializer=serializer
+    )
+    assert callback.stopping_rounds == callback_from_disk.stopping_rounds
