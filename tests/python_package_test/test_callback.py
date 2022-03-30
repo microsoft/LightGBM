bd08187590c8a9cd4262b3bc6c51aa18d4361b56
Nikita Titov <nekit94-12@hotmail.com>
2022-03-30 03:02:51 +0300

    make `log_evaluation` callback pickleable

--- tests/python_package_test/test_callback.py
+++ tests/python_package_test/test_callback.py
@@ -20,3 +20,19 @@ def test_early_stopping_callback_is_picklable(serializer, tmp_path):
         serializer=serializer
     )
     assert callback.stopping_rounds == callback_from_disk.stopping_rounds
+
+
+@pytest.mark.parametrize('serializer', ["pickle", "joblib", "cloudpickle"])
+def test_log_evaluation_callback_is_picklable(serializer, tmp_path):
+    callback = lgb.log_evaluation(period=42)
+    tmp_file = tmp_path / "log_evaluation.pkl"
+    pickle_obj(
+        obj=callback,
+        filepath=tmp_file,
+        serializer=serializer
+    )
+    callback_from_disk = unpickle_obj(
+        filepath=tmp_file,
+        serializer=serializer
+    )
+    assert callback.period == callback_from_disk.period
