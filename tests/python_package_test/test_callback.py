f4ed73588be0c35521f4396bed5d7d5c0e1c74ef
Nikita Titov <nekit94-12@hotmail.com>
2022-03-30 21:45:13 +0300

    make `record_evaluation` callback picklable

--- tests/python_package_test/test_callback.py
+++ tests/python_package_test/test_callback.py
@@ -5,38 +5,51 @@ import lightgbm as lgb
 
 from .utils import pickle_obj, unpickle_obj
 
+SERIALIZERS = ["pickle", "joblib", "cloudpickle"]
 
-@pytest.mark.parametrize('serializer', ["pickle", "joblib", "cloudpickle"])
-def test_early_stopping_callback_is_picklable(serializer, tmp_path):
+
+def pickle_and_unpickle_object(obj, serializer):
+    with lgb.basic._TempFile() as tmp_file:
+        pickle_obj(
+            obj=obj,
+            filepath=tmp_file.name,
+            serializer=serializer
+        )
+        obj_from_disk = unpickle_obj(
+            filepath=tmp_file.name,
+            serializer=serializer
+        )
+    return obj_from_disk
+
+
+@pytest.mark.parametrize('serializer', SERIALIZERS)
+def test_early_stopping_callback_is_picklable(serializer):
     rounds = 5
     callback = lgb.early_stopping(stopping_rounds=rounds)
-    tmp_file = tmp_path / "early_stopping.pkl"
-    pickle_obj(
-        obj=callback,
-        filepath=tmp_file,
-        serializer=serializer
-    )
-    callback_from_disk = unpickle_obj(
-        filepath=tmp_file,
-        serializer=serializer
-    )
+    callback_from_disk = pickle_and_unpickle_object(obj=callback, serializer=serializer)
+    assert callback_from_disk.order == 30
+    assert callback_from_disk.before_iteration is False
     assert callback.stopping_rounds == callback_from_disk.stopping_rounds
     assert callback.stopping_rounds == rounds
 
 
-@pytest.mark.parametrize('serializer', ["pickle", "joblib", "cloudpickle"])
-def test_log_evaluation_callback_is_picklable(serializer, tmp_path):
+@pytest.mark.parametrize('serializer', SERIALIZERS)
+def test_log_evaluation_callback_is_picklable(serializer):
     periods = 42
     callback = lgb.log_evaluation(period=periods)
-    tmp_file = tmp_path / "log_evaluation.pkl"
-    pickle_obj(
-        obj=callback,
-        filepath=tmp_file,
-        serializer=serializer
-    )
-    callback_from_disk = unpickle_obj(
-        filepath=tmp_file,
-        serializer=serializer
-    )
+    callback_from_disk = pickle_and_unpickle_object(obj=callback, serializer=serializer)
+    assert callback_from_disk.order == 10
+    assert callback_from_disk.before_iteration is False
     assert callback.period == callback_from_disk.period
     assert callback.period == periods
+
+
+@pytest.mark.parametrize('serializer', SERIALIZERS)
+def test_record_evaluation_callback_is_picklable(serializer):
+    results = {}
+    callback = lgb.record_evaluation(eval_result=results)
+    callback_from_disk = pickle_and_unpickle_object(obj=callback, serializer=serializer)
+    assert callback_from_disk.order == 20
+    assert callback_from_disk.before_iteration is False
+    assert callback.eval_result == callback_from_disk.eval_result
+    assert callback.eval_result is results
