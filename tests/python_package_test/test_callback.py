617d207a0b371c7aed206baff42f18a47a2eef72
Nikita Titov <nekit94-12@hotmail.com>
2022-03-30 18:12:56 +0300

    make callback tests stricter

--- tests/python_package_test/test_callback.py
+++ tests/python_package_test/test_callback.py
@@ -8,7 +8,8 @@ from .utils import pickle_obj, unpickle_obj
 
 @pytest.mark.parametrize('serializer', ["pickle", "joblib", "cloudpickle"])
 def test_early_stopping_callback_is_picklable(serializer, tmp_path):
-    callback = lgb.early_stopping(stopping_rounds=5)
+    rounds = 5
+    callback = lgb.early_stopping(stopping_rounds=rounds)
     tmp_file = tmp_path / "early_stopping.pkl"
     pickle_obj(
         obj=callback,
@@ -20,11 +21,13 @@ def test_early_stopping_callback_is_picklable(serializer, tmp_path):
         serializer=serializer
     )
     assert callback.stopping_rounds == callback_from_disk.stopping_rounds
+    assert callback.stopping_rounds == rounds
 
 
 @pytest.mark.parametrize('serializer', ["pickle", "joblib", "cloudpickle"])
 def test_log_evaluation_callback_is_picklable(serializer, tmp_path):
-    callback = lgb.log_evaluation(period=42)
+    periods = 42
+    callback = lgb.log_evaluation(period=periods)
     tmp_file = tmp_path / "log_evaluation.pkl"
     pickle_obj(
         obj=callback,
@@ -36,3 +39,4 @@ def test_log_evaluation_callback_is_picklable(serializer, tmp_path):
         serializer=serializer
     )
     assert callback.period == callback_from_disk.period
+    assert callback.period == periods
