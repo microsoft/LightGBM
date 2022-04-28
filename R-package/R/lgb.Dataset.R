0e6816fd6b53a0144d041645c926c243d418d93d
José Morales <jmoralz92@gmail.com>
2022-04-28 10:00:27 -0500

    check if is filename instead of matrix

--- R-package/R/lgb.Dataset.R
+++ R-package/R/lgb.Dataset.R
@@ -169,8 +169,8 @@ Dataset <- R6::R6Class(
           } else {
 
             # Check if more categorical features were output over the feature space
-            data_is_filename <- is.matrix(private$raw_data) || methods::is(private$raw_data, "dgCMatrix")
-            if (data_is_matrix && max(private$categorical_feature) > ncol(private$raw_data)) {
+            data_is_not_filename <- !is.character(private$raw_data)
+            if (data_is_not_filename && max(private$categorical_feature) > ncol(private$raw_data)) {
               stop(
                 "lgb.self.get.handle: supplied a too large value in categorical_feature: "
                 , max(private$categorical_feature)
