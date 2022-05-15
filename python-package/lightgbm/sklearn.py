5b6841e70ec873746925a5edba39b0b7c1f249a5
David Cortes <david.cortes.rivera@gmail.com>
2022-05-15 15:12:33 +0200

    corrections for predict-time n_jobs

--- python-package/lightgbm/sklearn.py
+++ python-package/lightgbm/sklearn.py
@@ -852,8 +852,11 @@ class LGBMModel(_LGBMModelBase):
 
         # number of threads can have values with special meaning which is only applied
         # in the scikit-learn interface, these should not reach the c++ side as-is
-        predict_params = _choose_param_value("num_threads", predict_params, self.n_jobs)
-        predict_params["num_threads"] = self._process_n_jobs(predict_params["num_threads"])
+        num_threads_aliases = _ConfigAliases.get("num_threads")
+        for alias in num_threads_aliases:
+            if alias in predict_params:
+                n_jobs = predict_params.pop(alias)
+        predict_params["num_threads"] = self._process_n_jobs(n_jobs)
 
         return self._Booster.predict(X, raw_score=raw_score, start_iteration=start_iteration, num_iteration=num_iteration,
                                      pred_leaf=pred_leaf, pred_contrib=pred_contrib, **predict_params)
