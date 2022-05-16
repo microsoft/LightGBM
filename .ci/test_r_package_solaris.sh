37d598a73e457a4967589d580379ab8b6ca76795
James Lamb <jaylamb20@gmail.com>
2022-05-15 21:54:49 -0500

    [ci] switch to r-devel in Solaris builds (fixes #5216)

--- .ci/test_r_package_solaris.sh
+++ .ci/test_r_package_solaris.sh
@@ -7,9 +7,11 @@ apt-get install --no-install-recommends -y \
 
 # installation of dependencies needs to happen before building the package,
 # since `R CMD build` needs to install the package to build vignettes
-Rscript -e "install.packages(c('R6', 'data.table', 'jsonlite', 'knitr', 'Matrix', 'RhpcBLASctl', 'rmarkdown', 'rhub', 'testthat'), dependencies = c('Depends', 'Imports', 'LinkingTo'), repos = 'https://cran.r-project.org', Ncpus = parallel::detectCores())" || exit -1
+RDscript -e "install.packages(c('R6', 'data.table', 'jsonlite', 'knitr', 'Matrix', 'RhpcBLASctl', 'rmarkdown', 'rhub', 'testthat'), dependencies = c('Depends', 'Imports', 'LinkingTo'), repos = 'https://cran.r-project.org', Ncpus = parallel::detectCores())" || exit -1
 
-sh build-cran-package.sh || exit -1
+sh build-cran-package.sh \
+    --r-executable=RDvalgrind \
+    || exit -1
 
 log_file="rhub_logs.txt"
-Rscript ./.ci/run_rhub_solaris_checks.R lightgbm_*.tar.gz $log_file || exit -1
+RDscript ./.ci/run_rhub_solaris_checks.R lightgbm_*.tar.gz $log_file || exit -1
