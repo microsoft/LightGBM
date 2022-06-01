072ea474877b1b365c5e1e6f4b89228aeaf3507a
José Morales <jmoralz92@gmail.com>
2022-05-31 23:05:34 -0500

    test with feature_name and 'auto'

--- tests/python_package_test/test_basic.py
+++ tests/python_package_test/test_basic.py
@@ -644,20 +644,22 @@ def test_custom_objective_safety():
 
 
 @pytest.mark.parametrize('dtype', [np.float32, np.float64])
-def test_no_copy_when_single_float_dtype_dataframe(dtype):
+@pytest.mark.parametrize('feature_name', [['x1', 'x2'], 'auto'])
+def test_no_copy_when_single_float_dtype_dataframe(dtype, feature_name):
     pd = pytest.importorskip('pandas')
     X = np.random.rand(10, 2).astype(dtype)
     df = pd.DataFrame(X)
-    built_data = lgb.basic._data_from_pandas(df, 'auto', None, None)[0]
+    built_data = lgb.basic._data_from_pandas(df, feature_name, None, None)[0]
     assert built_data.dtype == dtype
     assert np.shares_memory(X, built_data)
 
 
-def test_categorical_code_conversion_doesnt_modify_original_data():
+@pytest.mark.parametrize('feature_name', [['x1'], 'auto'])
+def test_categorical_code_conversion_doesnt_modify_original_data(feature_name):
     pd = pytest.importorskip('pandas')
     X = np.random.choice(['a', 'b'], 100).reshape(-1, 1)
     df = pd.DataFrame(X.copy(), columns=['x1'], dtype='category')
-    data = lgb.basic._data_from_pandas(df, 'auto', None, None)[0]
+    data = lgb.basic._data_from_pandas(df, feature_name, None, None)[0]
     # check that the original data wasn't modified
     np.testing.assert_equal(df['x1'], X[:, 0])
     # check that the built data has the codes
