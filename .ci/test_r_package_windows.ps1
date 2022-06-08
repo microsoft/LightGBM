0975f8b7858cd80148323a15bd0af8bb7185e447
James Lamb <jaylamb20@gmail.com>
2022-06-07 22:00:50 -0500

    try a different path

--- .ci/test_r_package_windows.ps1
+++ .ci/test_r_package_windows.ps1
@@ -77,7 +77,7 @@ if ($env:R_MAJOR_VERSION -eq "3") {
 } elseif ($env:R_MAJOR_VERSION -eq "4") {
   $RTOOLS_INSTALL_PATH = "C:\rtools42"
   $env:RTOOLS_BIN = "$RTOOLS_INSTALL_PATH\usr\bin"
-  $env:RTOOLS_MINGW_BIN = "$RTOOLS_INSTALL_PATH\mingw64\bin"
+  $env:RTOOLS_MINGW_BIN = "$RTOOLS_INSTALL_PATH\x86_64-w64-mingw32.static.posix\bin"
   $env:RTOOLS_EXE_FILE = "rtools42-5253-5107.exe"
   $env:R_WINDOWS_VERSION = "4.2.0"
 } else {
