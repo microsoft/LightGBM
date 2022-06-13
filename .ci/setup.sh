ee6070f428af32e456ddc4aa482faabf77fe98ba
Nikita Titov <nekit94-08@mail.ru>
2022-06-13 22:00:53 +0300

    Install PoCL in Docker for old Ubuntu 14.04

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -74,17 +74,6 @@ else  # Linux
         if [[ $IN_UBUNTU_LATEST_CONTAINER == "true" ]]; then
             sudo apt-get install --no-install-recommends -y \
                 pocl-opencl-icd
-        else
-            sudo apt-get install --no-install-recommends -y \
-                libhwloc-dev \
-                libtinfo-dev \
-                ocl-icd-dev \
-                pkg-config \
-                zlib1g-dev
-            git clone --depth 1 --branch v1.8 https://github.com/pocl/pocl.git
-            cmake -B pocl/build -S pocl -DCMAKE_BUILD_TYPE=release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-stdlib=libc++ -DPOCL_INSTALL_ICD_VENDORDIR=/etc/OpenCL/vendors -DPOCL_DEBUG_MESSAGES=OFF -DSTATIC_LLVM=ON -DINSTALL_OPENCL_HEADERS=OFF -DENABLE_SPIR=OFF -DENABLE_POCLCC=OFF -DENABLE_TESTS=OFF -DENABLE_EXAMPLES=OFF
-            cmake --build pocl/build -j4
-            sudo cmake --install pocl/build
         fi
     fi
     if [[ $TASK == "cuda" || $TASK == "cuda_exp" ]]; then
