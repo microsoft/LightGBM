445e1bd7dd12199674b5edb667551b0cb8c549ad
Scott Votaw <svotaw@microsoft.com>
2022-06-15 10:33:48 -0700

    doc fixes

--- include/LightGBM/c_api.h
+++ include/LightGBM/c_api.h
@@ -165,6 +165,7 @@ LIGHTGBM_C_EXPORT int LGBM_DatasetCreateByReference(const DatasetHandle referenc
  * \param ref_buffer A binary representation of the dataset schema (feature groups, bins, etc)
  * \param ref_buffer_size The size of the reference array in bytes
  * \param num_row Number of total rows teh dataset will contain
+ * \param num_classes Number of classes (if multiclass and sending intial scores)
  * \param parameters Additional parameters
  * \param[out] out Created dataset
  * \return 0 when succeed, -1 when failure happens
