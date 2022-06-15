6cca968dd9c2b0e91551cc7343a1af33a786316a
Scott Votaw <svotaw@microsoft.com>
2022-06-14 17:04:03 -0700

    missing files

---
+++ include/LightGBM/utils/byte_buffer.h
@@ -0,0 +1,63 @@
+/*!
+ * Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE file in the project root for license information.
+ */
+#ifndef LIGHTGBM_UTILS_BYTE_BUFFER_H_
+#define LIGHTGBM_UTILS_BYTE_BUFFER_H_
+
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <LightGBM/export.h>
+#include <LightGBM/utils/serialize.h>
+
+namespace LightGBM {
+
+/*!
+ * \brief An implementation for serializing binary data to an auto-expanding memory buffer
+ */
+struct ByteBuffer : public BinaryWriter {
+  ByteBuffer() {}
+
+  ByteBuffer(size_t initial_size) {
+    buffer_.reserve(initial_size);
+  }
+
+  size_t Write(const void* data, size_t bytes) {
+    const char* mem_ptr = static_cast<const char*>(data);
+    for (size_t i = 0; i < bytes; ++i)
+    {
+      buffer_.push_back(mem_ptr[i]);
+    }
+
+    return bytes;
+  }
+
+  LIGHTGBM_EXPORT void Reserve(size_t capacity) {
+    buffer_.reserve(capacity);
+  }
+
+  LIGHTGBM_EXPORT size_t GetSize() {
+    return buffer_.size();
+  }
+
+  LIGHTGBM_EXPORT char GetAt(size_t index) {
+    return buffer_.at(index);
+  }
+
+  LIGHTGBM_EXPORT char* Data() {
+    return buffer_.data();
+  }
+
+  private:
+    std::vector<char> buffer_;
+};
+
+}  // namespace LightGBM
+
+#endif   // LightGBM_UTILS_BYTE_BUFFER_H_
---
+++ include/LightGBM/utils/serialize.h
@@ -0,0 +1,45 @@
+/*!
+ * Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE file in the project root for license information.
+ */
+#ifndef LIGHTGBM_UTILS_SERIALIZE_H_
+#define LIGHTGBM_UTILS_SERIALIZE_H_
+
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>  // TODO check these includes
+#include <memory>
+#include <vector>
+
+namespace LightGBM {
+
+/*!
+ * \brief An interface for serializing binary data to a buffer
+ */
+struct BinaryWriter {
+  virtual size_t Write(const void* data, size_t bytes) = 0;
+  size_t AlignedWrite(const void* data, size_t bytes, size_t alignment = 8) {
+    auto ret = Write(data, bytes);
+    if (bytes % alignment != 0) {
+      size_t padding = AlignedSize(bytes, alignment) - bytes;
+      std::vector<char> tmp(padding, 0);
+      ret += Write(tmp.data(), padding);
+    }
+    return ret;
+  }
+
+  static size_t AlignedSize(size_t bytes, size_t alignment = 8) {
+    if (bytes % alignment == 0) {
+      return bytes;
+    }
+    else {
+      return bytes / alignment * alignment + alignment;
+    }
+  }
+};
+
+}  // namespace LightGBM
+
+#endif   // LightGBM_UTILS_SERIALIZE_H_
