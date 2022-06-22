7b7b6c1f63b8b6810e974ee01c58a12fd3d5ce3f
david-cortes <david.cortes.rivera@gmail.com>
2022-06-22 21:07:40 +0300

    Update R-package/R/lgb.Booster.R
    Co-authored-by: James Lamb <jaylamb20@gmail.com>

--- R-package/R/lgb.Booster.R
+++ R-package/R/lgb.Booster.R
@@ -954,7 +954,7 @@ predict.lgb.Booster <- function(object,
 #'
 #'              \bold{The object will be modified in-place}.
 #' @param csr Whether the prediction function is going to be called on sparse CSR inputs.
-#'            If `FALSE`, will be assumed that predictions are going to be called on single-row
+#'            If \code{FALSE}, will be assumed that predictions are going to be called on single-row
 #'            regular R matrices.
 #' @return The same `model` that was passed as input, as invisible, with the desired
 #'         configuration stored inside it and available to be used in future calls to
