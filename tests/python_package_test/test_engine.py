32f564c26017fbae7c78663046dffd7ca0eec177
Belinda Trotta <btrotta@users.noreply.github.com>
2022-07-13 19:11:21 +1000

    Set random seed

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -3048,6 +3048,7 @@ def test_interaction_constraints():
 
 def test_linear_trees_num_threads():
     # check that number of threads does not affect result
+    np.random.seed(0)
     x = np.arange(0, 1000, 0.1)
     y = 2 * x + np.random.normal(0, 0.1, len(x))
     x = x[:, np.newaxis]
