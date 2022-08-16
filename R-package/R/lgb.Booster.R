321d35a5152deebbc889af150b23838337c83d45
david-cortes <david.cortes.rivera@gmail.com>
2022-08-16 15:55:14 +0300

    Update R-package/R/lgb.Booster.R
    Co-authored-by: James Lamb <jaylamb20@gmail.com>

--- R-package/R/lgb.Booster.R
+++ R-package/R/lgb.Booster.R
@@ -948,7 +948,7 @@ predict.lgb.Booster <- function(object,
 #'
 #'          Predictions about feature contributions do not allow a fast route for CSR inputs,
 #'          and as such, this function will produce an error if passing `csr=TRUE` and
-#'          `predcontrib=TRUE` together.
+#'          \code{predcontrib = TRUE} together.
 #' @inheritParams lgb_predict_shared_params
 #' @param model LighGBM model object (class \code{lgb.Booster}).
 #'
