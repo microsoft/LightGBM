3ce22367a8c34bc64cb9b25ce15e83eb0fcfcf9d
shiyu1994 <shiyu_k1994@qq.com>
2022-09-02 15:00:09 +0800

    fix test_regression

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -111,7 +111,7 @@ def test_rf():
     assert evals_result['valid_0']['binary_logloss'][-1] == pytest.approx(ret)
 
 
-@pytest.mark.parametrize('objective_and_threshold', ['regression', 'regression_l1', 'huber'])
+@pytest.mark.parametrize('objective', ['regression', 'regression_l1', 'huber'])
 def test_regression(objective):
     X, y = load_boston(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
