a253616402e2c9e6ebda28535a2f5742bca77910
James Lamb <jaylamb20@gmail.com>
2022-09-21 22:45:10 -0500

    ugh forgot the file

---
+++ test-inet.sh
@@ -0,0 +1,17 @@
+echo "------ testing test program ----"
+
+cat > conftest.cpp <<EOL
+#include <ws2tcpip.h>
+int main() {
+  void* p = inet_pton;
+  return 0;
+}
+EOL
+
+g++ -std=gnu++11 -I"c:/rtools42/x86_64-w64-mingw32.static.posix/include" -o conftest conftest.cpp || exit 123
+echo "------ done compiling ----"
+
+./conftest || exit 123
+rm -f ./conftest
+rm -f ./conftest.cpp
+echo "------ done running ----"
