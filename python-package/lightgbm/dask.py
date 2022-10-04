c37bb196a8969874ee868d0aaaad295df630760e
James Lamb <jaylamb20@gmail.com>
2022-10-03 23:50:39 -0500

    avoid calling free_network() during Dask teardown

--- python-package/lightgbm/dask.py
+++ python-package/lightgbm/dask.py
@@ -302,38 +302,33 @@ def _train_part(
         if eval_class_weight:
             kwargs['eval_class_weight'] = [eval_class_weight[i] for i in eval_component_idx]
 
-    try:
-        model = model_factory(**params)
-        if is_ranker:
-            model.fit(
-                data,
-                label,
-                sample_weight=weight,
-                init_score=init_score,
-                group=group,
-                eval_set=local_eval_set,
-                eval_sample_weight=local_eval_sample_weight,
-                eval_init_score=local_eval_init_score,
-                eval_group=local_eval_group,
-                eval_names=local_eval_names,
-                **kwargs
-            )
-        else:
-            model.fit(
-                data,
-                label,
-                sample_weight=weight,
-                init_score=init_score,
-                eval_set=local_eval_set,
-                eval_sample_weight=local_eval_sample_weight,
-                eval_init_score=local_eval_init_score,
-                eval_names=local_eval_names,
-                **kwargs
-            )
-
-    finally:
-        if model.booster_:
-            model.booster_.free_network()
+    model = model_factory(**params)
+    if is_ranker:
+        model.fit(
+            data,
+            label,
+            sample_weight=weight,
+            init_score=init_score,
+            group=group,
+            eval_set=local_eval_set,
+            eval_sample_weight=local_eval_sample_weight,
+            eval_init_score=local_eval_init_score,
+            eval_group=local_eval_group,
+            eval_names=local_eval_names,
+            **kwargs
+        )
+    else:
+        model.fit(
+            data,
+            label,
+            sample_weight=weight,
+            init_score=init_score,
+            eval_set=local_eval_set,
+            eval_sample_weight=local_eval_sample_weight,
+            eval_init_score=local_eval_init_score,
+            eval_names=local_eval_names,
+            **kwargs
+        )
 
     if n_evals:
         # ensure that expected keys for evals_result_ and best_score_ exist regardless of padding.
