b9cfbfd96a755a70fcc7d1066bdf744165d66340
James Lamb <jaylamb20@gmail.com>
2022-11-20 23:57:12 -0600

    add scipy

--- .ci/test.sh
+++ .ci/test.sh
@@ -131,7 +131,8 @@ conda install -q -y -n $CONDA_ENV \
     pytest \
     "python=$PYTHON_VERSION[build=*cpython]" \
     python-graphviz \
-    scikit-learn || exit -1
+    scikit-learn \
+    scipy || exit -1
 
 if [[ $OS_NAME == "macos" ]] && [[ $COMPILER == "clang" ]]; then
     # fix "OMP: Error #15: Initializing libiomp5.dylib, but found libomp.dylib already initialized." (OpenMP library conflict due to conda's MKL)
