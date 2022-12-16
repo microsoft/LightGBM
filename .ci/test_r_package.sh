3a6b7cf4b8430268a36bcf859c69e43ddfaedddd
James Lamb <jaylamb20@gmail.com>
2022-12-15 21:36:23 -0600

    add key-management stuff

--- .ci/test_r_package.sh
+++ .ci/test_r_package.sh
@@ -36,6 +36,12 @@ fi
 #
 # `devscripts` is required for 'checkbashisms' (https://github.com/r-lib/actions/issues/111)
 if [[ $OS_NAME == "linux" ]]; then
+    sudo apt-get update
+    sudo apt-get install \
+        --no-install-recommends \
+        -y \
+            dirmngr \
+            gpg
     sudo apt-key adv \
         --keyserver keyserver.ubuntu.com \
         --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 || exit -1
