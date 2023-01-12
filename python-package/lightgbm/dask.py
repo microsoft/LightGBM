f9390f292c6166d704cd1b43a782aa5ccbcb7fc5
IdoKendo <ryuusuke@gmail.com>
2023-01-12 09:14:49 +0200

    [python-package] move mypy fixes for eval_at and basic to a seperate commit

--- python-package/lightgbm/dask.py
+++ python-package/lightgbm/dask.py
@@ -407,7 +407,7 @@ def _train(
     eval_init_score: Optional[List[_DaskCollection]] = None,
     eval_group: Optional[List[_DaskVectorLike]] = None,
     eval_metric: Optional[_LGBM_ScikitEvalMetricType] = None,
-    eval_at: Union[List[int], Tuple[int, ...], None] = None,
+    eval_at: Optional[Union[List[int], Tuple[int]]] = None,
     **kwargs: Any
 ) -> LGBMModel:
     """Inner train routine.
@@ -1039,7 +1039,7 @@ class _DaskLGBMModel:
         eval_init_score: Optional[List[_DaskCollection]] = None,
         eval_group: Optional[List[_DaskVectorLike]] = None,
         eval_metric: Optional[_LGBM_ScikitEvalMetricType] = None,
-        eval_at: Union[List[int], Tuple[int, ...], None] = None,
+        eval_at: Optional[Iterable[int]] = None,
         **kwargs: Any
     ) -> "_DaskLGBMModel":
         if not DASK_INSTALLED:
@@ -1541,7 +1541,7 @@ class DaskLGBMRanker(LGBMRanker, _DaskLGBMModel):
         eval_init_score: Optional[List[_DaskVectorLike]] = None,
         eval_group: Optional[List[_DaskVectorLike]] = None,
         eval_metric: Optional[_LGBM_ScikitEvalMetricType] = None,
-        eval_at: Union[List[int], Tuple[int, ...]] = (1, 2, 3, 4, 5),
+        eval_at: Union[List[int], Tuple[int]] = (1, 2, 3, 4, 5),
         **kwargs: Any
     ) -> "_DaskLGBMModel":
         """Docstring is inherited from the lightgbm.LGBMRanker.fit."""
