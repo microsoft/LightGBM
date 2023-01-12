ebdce8720489513aa41a61aa46b1d312cd6a8fc2
James Lamb <jaylamb20@gmail.com>
2023-01-11 20:57:11 -0600

    add all CI back

--- .ci/test_r_package.sh
+++ .ci/test_r_package.sh
@@ -77,18 +77,13 @@ fi
 
 # Installing R precompiled for Mac OS 10.11 or higher
 if [[ $OS_NAME == "macos" ]]; then
-    # rm -f /usr/local/bin/2to3*
-    # rm -f /usr/local/bin/idle3.*
     if [[ $R_BUILD_TYPE == "cran" ]]; then
         brew install automake || exit -1
     fi
-    echo "--- installing checkbashisms ---"
-    brew install checkbashisms || exit -1
-    echo "--- installing qpdf ---"
-    brew install qpdf || exit -1
-    echo "--- installing basictex ---"
+    brew install \
+        checkbashisms \
+        qpdf || exit -1
     brew install --cask basictex || exit -1
-    echo "--- tlmgr stuff ---"
     export PATH="/Library/TeX/texbin:$PATH"
     sudo tlmgr --verify-repo=none update --self || exit -1
     sudo tlmgr --verify-repo=none install inconsolata helvetic rsfs || exit -1
