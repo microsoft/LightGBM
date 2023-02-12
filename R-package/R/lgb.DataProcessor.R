135786ed68b2f1de5b1b94164e66d742c66bb53d
David Cortes <david.cortes.rivera@gmail.com>
2023-02-12 20:06:20 +0100

    check for aliases of num_class

--- R-package/R/lgb.DataProcessor.R
+++ R-package/R/lgb.DataProcessor.R
@@ -42,10 +42,20 @@ DataProcessor <- R6::R6Class(
               )
             )
           }
-          if ("num_class" %in% names(params)) {
-            warning("'num_class' was passed as parameter, but it is set automatically when passing factors as labels.")
+          aliases_num_class <- .PARAMETER_ALIASES()$num_class
+          present_num_class <- intersect(aliases_num_class, names(params))
+          data_num_class <- length(self$factor_levels)
+          if (length(present_num_class)) {
+            for (num_class_alias in present_num_class) {
+              if (params[[num_class_alias]] != data_num_class) {
+                warning("'num_class' was passed as parameter, but it is set automatically when passing factors as labels.")
+                break
+              }
+            }
+            params <- params[setdiff(names(params), present_num_class)]
           }
-          params$num_class <- length(self$factor_levels)
+
+          params$num_class <- data_num_class
 
         }
         out$objective <- objective
