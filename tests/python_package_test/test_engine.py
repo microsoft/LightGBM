0812403e488a5a9e9c5358b5a1b6fa8523663f6c
Yu Shi <shiyu_k1994@qq.com>
2023-04-26 16:20:13 +0000

    refactor test_quantized_training

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -4060,16 +4060,14 @@ def test_train_raises_informative_error_for_params_of_wrong_type():
 def test_quantized_training():
     X, y = make_synthetic_regression()
     ds = lgb.Dataset(X, label=y)
-    bst = lgb.train({'num_leaves': 15, 'verbose': -1, 'seed': 0}, ds, num_boost_round=10)
+    bst_params = {'num_leaves': 15, 'verbose': -1, 'seed': 0}
+    bst = lgb.train(bst_params, ds, num_boost_round=10)
     rmse = np.sqrt(np.mean((bst.predict(X) - y) ** 2))
-    quantized_training_params = {
-        'num_leaves': 15,
-        'verbose': -1,
-        'seed': 0,
+    bst_params.update({
         'use_quantized_grad': True,
         'num_grad_quant_bins': 30,
         'quant_train_renew_leaf': True,
-    }
-    quant_bst = lgb.train(quantized_training_params, ds, num_boost_round=10)
+    })
+    quant_bst = lgb.train(bst_params, ds, num_boost_round=10)
     quant_rmse = np.sqrt(np.mean((quant_bst.predict(X) - y) ** 2))
     assert quant_rmse < rmse + 6.0
