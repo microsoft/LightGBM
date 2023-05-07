5a19c102c86d3d989b5e753abb7e4296fc624ae9
James Lamb <jaylamb20@gmail.com>
2023-05-07 00:32:41 -0500

    update config

--- .ci/lint-python.sh
+++ .ci/lint-python.sh
@@ -5,14 +5,14 @@ flake8 \
     --config=./python-package/setup.cfg \
     --ignore=E501,W503 \
     --exclude=./.nuget,./external_libs,./python-package/build,./python-package/compile \
-    $(pwd) \
+    . \
 || exit -1
 echo "done running flake8"
 
 echo "running pydocstyle"
 pydocstyle \
     --config=./python-package/pyproject.toml \
-    $(pwd) \
+    . \
 || exit -1
 echo "done running pydocstyle"
 
@@ -20,13 +20,13 @@ echo "running isort"
 isort \
     --check-only \
     --settings-path=./python-package/pyproject.toml \
-    $(pwd) \
+    . \
 || exit -1
 echo "done running isort"
 
 echo "running mypy"
 mypy \
     --config-file=./python-package/pyproject.toml \
-    $(pwd)/python-package \
+    ./python-package \
 || true
 echo "done running mypy"
