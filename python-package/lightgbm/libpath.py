3d995e87ed5cd5f30a8cebc421b9211521828730
James Lamb <jaylamb20@gmail.com>
2023-05-09 00:03:39 -0500

    change strategy for paths in CMakeLists

--- python-package/lightgbm/libpath.py
+++ python-package/lightgbm/libpath.py
@@ -17,10 +17,8 @@ def find_lib_path() -> List[str]:
     """
     curr_path = Path(__file__).absolute()
     dll_path = [curr_path,
-                curr_path.parent,
                 curr_path.parents[1],
-                curr_path.parents[2],
-                curr_path.parents[1] / 'lib']
+                curr_path.parents[0] / 'lib']
     if system() in ('Windows', 'Microsoft'):
         dll_path.append(curr_path.parents[1] / 'Release')
         dll_path.append(curr_path.parents[1] / 'windows' / 'x64' / 'DLL')
