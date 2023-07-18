f3e959ac7feb5ea9d3a88edd85c8954bbb780f66
James Lamb <jaylamb20@gmail.com>
2023-07-17 21:21:48 -0500

    more changes

--- src/io/multi_val_dense_bin.hpp
+++ src/io/multi_val_dense_bin.hpp
@@ -343,7 +343,10 @@ class MultiValDenseBin : public MultiValBin {
   std::vector<uint32_t> offsets_;
   std::vector<VAL_T, Common::AlignmentAllocator<VAL_T, 32>> data_;
 
-  MultiValDenseBin<VAL_T>(const MultiValDenseBin<VAL_T>& other)
+  // https://oleksandrkvl.github.io/2021/04/02/cpp-20-overview.html
+  // https://github.com/PixarAnimationStudios/OpenUSD/issues/2183
+  // https://github.com/PixarAnimationStudios/OpenUSD/pull/2242/files
+  MultiValDenseBin(const MultiValDenseBin& other)
     : num_data_(other.num_data_), num_bin_(other.num_bin_), num_feature_(other.num_feature_),
       offsets_(other.offsets_), data_(other.data_) {
   }
