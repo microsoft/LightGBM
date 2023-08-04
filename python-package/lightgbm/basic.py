5d836ed466928e87a0386b3d1e99d6012a91ecf3
shiyu1994 <shiyu_k1994@qq.com>
2023-08-04 20:13:23 +0800

    Update python-package/lightgbm/basic.py
    Co-authored-by: James Lamb <jaylamb20@gmail.com>

--- python-package/lightgbm/basic.py
+++ python-package/lightgbm/basic.py
@@ -1507,7 +1507,7 @@ class Dataset:
             sum(group) = n_samples.
             For example, if you have a 100-document dataset with ``group = [10, 20, 40, 10, 10, 10]``, that means that you have 6 groups,
             where the first 10 records are in the first group, records 11-30 are in the second group, records 31-70 are in the third group, etc.
-        position : list, numpy 1-D array, pandas Series or None, optional (default=None)
+        position : numpy 1-D array, pandas Series or None, optional (default=None)
             Position of items used in unbiased learning-to-rank task.
         init_score : list, list of lists (for multi-class task), numpy array, pandas Series, pandas DataFrame (for multi-class task), or None, optional (default=None)
             Init score for Dataset.
