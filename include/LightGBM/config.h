fe31d4ecda2c857128128688c3dbd2eaa0020d04
Thomas BESSOU <thomas.bessou@hotmail.fr>
2023-08-07 01:47:43 +0200

    fix missing file change

--- include/LightGBM/config.h
+++ include/LightGBM/config.h
@@ -33,6 +33,10 @@ const int kDefaultNumLeaves = 31;
 
 struct Config {
  public:
+  Config() {}
+  Config(std::unordered_map<std::string, std::string> parameters_map) {
+    Set(parameters_map);
+  }
   std::string ToString() const;
   /*!
   * \brief Get string value by specific name of key
