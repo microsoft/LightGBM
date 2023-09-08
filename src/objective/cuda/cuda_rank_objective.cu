62aa30bbc57dc2bed7388eca0cf430a92974866c
Jeff Daily <jeff.daily@amd.com>
2023-09-08 19:54:54 +0000

    accidental inclusion of hip headers

--- src/objective/cuda/cuda_rank_objective.cu
+++ src/objective/cuda/cuda_rank_objective.cu
@@ -1,4 +1,3 @@
-#include "hip/hip_runtime.h"
 /*!
  * Copyright (c) 2021 Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE file in the project root for
--- src/treelearner/cuda/cuda_leaf_splits.cu
+++ src/treelearner/cuda/cuda_leaf_splits.cu
@@ -1,5 +1,3 @@
-#include "hip/hip_runtime.h"
-#include "hip/hip_runtime.h"
 /*!
  * Copyright (c) 2021 Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE file in the project root for
