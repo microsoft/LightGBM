7386ecd60b8881382afeeb38b7e0fc70769d25ac
James Lamb <jaylamb20@gmail.com>
2023-09-22 13:04:25 -0500

    fix compatibility with np.testing.assert_array_equal()

--- tests/python_package_test/test_basic.py
+++ tests/python_package_test/test_basic.py
@@ -15,7 +15,7 @@ from sklearn.model_selection import train_test_split
 import lightgbm as lgb
 from lightgbm.compat import PANDAS_INSTALLED, pd_DataFrame, pd_Series
 
-from .utils import dummy_obj, load_breast_cancer, mse_obj
+from .utils import dummy_obj, load_breast_cancer, mse_obj, np_assert_array_equal
 
 
 def test_basic(tmp_path):
@@ -529,12 +529,12 @@ def test_dataset_construction_overwrites_user_provided_metadata_fields():
     assert dtrain.label == [1, 2]
     assert dtrain.get_label() == [1, 2]
     if getenv('TASK', '') != 'cuda':
-        np.testing.assert_array_equal(
+        np_assert_array_equal(
             dtrain.position,
             np.array([0.0, 1.0], dtype=np.float32),
             strict=True
         )
-        np.testing.assert_array_equal(
+        np_assert_array_equal(
             dtrain.get_position(),
             np.array([0.0, 1.0], dtype=np.float32),
             strict=True
@@ -554,37 +554,37 @@ def test_dataset_construction_overwrites_user_provided_metadata_fields():
     assert dtrain.group == expected_group
     assert dtrain.get_group() == expected_group
     # get_field("group") returns a numpy array with boundaries, instead of size
-    np.testing.assert_array_equal(
+    np_assert_array_equal(
         dtrain.get_field("group"),
         np.array([0, 1, 2], dtype=np.int32),
         strict=True
     )
 
     expected_init_score = np.array([0.312, 0.708])
-    np.testing.assert_array_equal(dtrain.init_score, expected_init_score, strict=True)
-    np.testing.assert_array_equal(dtrain.get_init_score(), expected_init_score, strict=True)
-    np.testing.assert_array_equal(dtrain.get_field("init_score"), expected_init_score, strict=True)
+    np_assert_array_equal(dtrain.init_score, expected_init_score, strict=True)
+    np_assert_array_equal(dtrain.get_init_score(), expected_init_score, strict=True)
+    np_assert_array_equal(dtrain.get_field("init_score"), expected_init_score, strict=True)
 
     expected_label = np.array([1, 2], dtype=np.float32)
-    np.testing.assert_array_equal(dtrain.label, expected_label, strict=True)
-    np.testing.assert_array_equal(dtrain.get_label(), expected_label, strict=True)
-    np.testing.assert_array_equal(dtrain.get_field("label"), expected_label, strict=True)
+    np_assert_array_equal(dtrain.label, expected_label, strict=True)
+    np_assert_array_equal(dtrain.get_label(), expected_label, strict=True)
+    np_assert_array_equal(dtrain.get_field("label"), expected_label, strict=True)
 
     if getenv('TASK', '') != 'cuda':
         expected_position = np.array([0.0, 1.0], dtype=np.float32)
-        np.testing.assert_array_equal(dtrain.position, expected_position, strict=True)
-        np.testing.assert_array_equal(dtrain.get_position(), expected_position, strict=True)
+        np_assert_array_equal(dtrain.position, expected_position, strict=True)
+        np_assert_array_equal(dtrain.get_position(), expected_position, strict=True)
         # NOTE: "position" is converted to int32 on the C++ side
-        np.testing.assert_array_equal(
+        np_assert_array_equal(
             dtrain.get_field("position"),
             np.array([0.0, 1.0], dtype=np.int32),
             strict=True
         )
 
     expected_weight = np.array([0.5, 1.5], dtype=np.float32)
-    np.testing.assert_array_equal(dtrain.weight, expected_weight, strict=True)
-    np.testing.assert_array_equal(dtrain.get_weight(), expected_weight, strict=True)
-    np.testing.assert_array_equal(dtrain.get_field("weight"), expected_weight, strict=True)
+    np_assert_array_equal(dtrain.weight, expected_weight, strict=True)
+    np_assert_array_equal(dtrain.get_weight(), expected_weight, strict=True)
+    np_assert_array_equal(dtrain.get_field("weight"), expected_weight, strict=True)
 
 
 def test_choose_param_value():
