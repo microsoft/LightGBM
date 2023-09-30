b71e04fb4ce0e6caa2d5d0f469f354f3e043f83c
James Lamb <jaylamb20@gmail.com>
2023-09-29 19:20:23 -0500

    [python-package] simplify Dataset._compare_params_for_warning()

--- python-package/lightgbm/basic.py
+++ python-package/lightgbm/basic.py
@@ -2200,8 +2200,8 @@ class Dataset:
 
     @staticmethod
     def _compare_params_for_warning(
-        params: Optional[Dict[str, Any]],
-        other_params: Optional[Dict[str, Any]],
+        params: Dict[str, Any],
+        other_params: Dict[str, Any],
         ignore_keys: Set[str]
     ) -> bool:
         """Compare two dictionaries with params ignoring some keys.
@@ -2222,10 +2222,6 @@ class Dataset:
         compare_result : bool
           Returns whether two dictionaries with params are equal.
         """
-        if params is None:
-            params = {}
-        if other_params is None:
-            other_params = {}
         for k in other_params:
             if k not in ignore_keys:
                 if k not in params or params[k] != other_params[k]:
