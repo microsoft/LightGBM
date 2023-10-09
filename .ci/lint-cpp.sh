4a64f459490d34aa8844ec7e3b7d46ad920c05c2
James Lamb <jaylamb20@gmail.com>
2023-10-08 21:31:48 -0500

    add a CI script

--- .ci/lint-cpp.sh
+++ .ci/lint-cpp.sh
@@ -18,3 +18,25 @@ cmakelint \
     ${cmake_files} \
 || exit -1
 echo "done running cmakelint"
+
+echo "checking that all OpenMP pragmas specify num_threads()"
+get_omp_pragmas_without_num_threads() {
+    grep \
+        -n \
+        -R \
+        --include='*.c' \
+        --include='*.cc' \
+        --include='*.cpp' \
+        --include='*.h' \
+        --include='*.hpp' \
+        'pragma omp parallel' \
+    | grep -v ' num_threads'
+}
+PROBLEMATIC_LINES=$(
+    get_omp_pragmas_without_num_threads
+)
+if test "${PROBLEMATIC_LINES}" != ""; then
+    get_omp_pragmas_without_num_threads
+    echo "All '#pragma omp parallel' uses should specify num_threads() explicitly."
+    exit -1
+fi
