585fd8e7bb24a45577e518b55870c717a6c63664
James Lamb <jaylamb20@gmail.com>
2023-12-21 23:43:49 -0600

    revert MAKEFLAGS change

--- .ci/test_r_package_valgrind.sh
+++ .ci/test_r_package_valgrind.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-MAKEFLAGS=-j2 \
 RDscriptvalgrind -e "install.packages(c('R6', 'data.table', 'jsonlite', 'Matrix', 'RhpcBLASctl', 'testthat'), repos = 'https://cran.rstudio.com')" || exit -1
-
 sh build-cran-package.sh \
   --r-executable=RDvalgrind \
   --no-build-vignettes \
