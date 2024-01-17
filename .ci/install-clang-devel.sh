706690977118f67a5556fb7d508e9242a92f41dd
James Lamb <jaylamb20@gmail.com>
2024-01-16 22:56:36 -0800

    just copy everything

--- .ci/install-clang-devel.sh
+++ .ci/install-clang-devel.sh
@@ -67,10 +67,10 @@ CXX20 += -stdlib=libc++
 CFLAGS = -O3 -Wall -pedantic -Wp,-D_FORTIFY_SOURCE=3
 CPPFLAGS = -isystem /usr/local/clang/include
 CXXFLAGS = -O3 -Wall -pedantic -frtti -Wp,-D_FORTIFY_SOURCE=3
-CXX11FLAGS = \$CXXFLAGS
-CXX14FLAGS = \$CXXFLAGS
-CXX17FLAGS = \$CXXFLAGS
-CXX20FLAGS = \$CXXFLAGS
+CXX11FLAGS = -O3 -Wall -pedantic -frtti -Wp,-D_FORTIFY_SOURCE=3
+CXX14FLAGS = -O3 -Wall -pedantic -frtti -Wp,-D_FORTIFY_SOURCE=3
+CXX17FLAGS = -O3 -Wall -pedantic -frtti -Wp,-D_FORTIFY_SOURCE=3
+CXX20FLAGS = -O3 -Wall -pedantic -frtti -Wp,-D_FORTIFY_SOURCE=3
 EOF
 
 echo ""
