9c95f1e17d8a165c6ce08eb14ea0f6745b5af51c
James Lamb <jaylamb20@gmail.com>
2024-02-09 22:34:48 -0600

    add isort

--- .ci/lint-python.sh
+++ .ci/lint-python.sh
@@ -4,14 +4,6 @@ echo "running pre-commit checks"
 pre-commit run --all-files || exit 1
 echo "done running pre-commit checks"
 
-echo "running isort"
-isort \
-    --check-only \
-    --settings-path=./python-package/pyproject.toml \
-    . \
-|| exit -1
-echo "done running isort"
-
 echo "running mypy"
 mypy \
     --config-file=./python-package/pyproject.toml \
