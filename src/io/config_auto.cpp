848fd58b01829b8159f75af7fd88e9551e7d24aa
Alberto Veneri <alberto.veneri@unive.it>
2024-02-14 17:57:42 +0100

    Fix missing parenthesis

--- src/io/config_auto.cpp
+++ src/io/config_auto.cpp
@@ -926,6 +926,7 @@ const std::unordered_map<std::string, std::vector<std::string>>& Config::paramet
     {"num_gpu", {}},
   });
   return map;
+}
 const std::string Config::DumpAliases() {
   std::stringstream str_buf;
   str_buf << "{";
