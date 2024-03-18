9ad25d61a7d30135ed9dd0dc94f7beab21b25175
James Lamb <jaylamb20@gmail.com>
2024-03-18 13:14:13 -0500

    [ci] prevent trailing whitespace, ensure files end with newline

--- tests/cpp_tests/testutils.cpp
+++ tests/cpp_tests/testutils.cpp
@@ -277,7 +277,7 @@ namespace LightGBM {
   /*!
    * Pushes data from 1 thread into a Dataset based on thread_id and nrows.
    * e.g. with 100 rows, thread 0 will push rows 0-49, and thread 2 will push rows 50-99.
-   * Note that rows are still pushed in microbatches within their range. 
+   * Note that rows are still pushed in microbatches within their range.
    */
   void TestUtils::PushSparseBatch(DatasetHandle dataset_handle,
                                   int32_t nrows,
