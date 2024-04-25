4b97dd26ede5a0f720ea740ca2dda0c07813cca4
Oliver Borchert <oliver.borchert@quantco.com>
2024-04-25 10:35:06 +0200

    debug

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -26,6 +26,7 @@ if [[ $OS_NAME == "macos" ]]; then
     fi
     if [[ $TASK == "mpi" ]]; then
         brew install open-mpi
+        tree /usr/local/Cellar/open-mpi
     fi
     if [[ $TASK == "swig" ]]; then
         brew install swig
