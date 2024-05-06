e66b00ae87e86712b3f1b7f70e2441aed6621ab0
James Lamb <jaylamb20@gmail.com>
2024-05-05 23:30:15 -0500

    Python changes

--- python-package/lightgbm/engine.py
+++ python-package/lightgbm/engine.py
@@ -14,7 +14,6 @@ from . import callback
 from .basic import (
     Booster,
     Dataset,
-    LGBMDeprecationWarning,
     LightGBMError,
     _choose_param_value,
     _ConfigAliases,
@@ -59,7 +58,7 @@ def _emit_dataset_kwarg_warning(calling_function: str, argname: str) -> None:
         f"a future release. Set '{argname}' when calling lightgbm.Dataset() instead. "
         "See https://github.com/microsoft/LightGBM/issues/6435."
     )
-    warnings.warn(msg, category=LGBMDeprecationWarning, stacklevel=2)
+    warnings.warn(msg, category=FutureWarning, stacklevel=2)
 
 
 def train(
