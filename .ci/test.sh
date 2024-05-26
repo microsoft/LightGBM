77f1f29dcd6ec59d99f899e6076037cc02074daa
James Lamb <jaylamb20@gmail.com>
2024-05-26 12:46:34 -0500

    get more logs

--- .ci/test.sh
+++ .ci/test.sh
@@ -156,7 +156,6 @@ find "${HOME}" -type f -name 'libgomp.so.*' || true
 echo "--- finding libomp.so (${CONDA}) ..."
 find "${CONDA}" -type f -name 'libomp.so.*' || true
 find "${CONDA}" -type f -name 'libgomp.so.*' || true
-exit 123
 
 if [[ $OS_NAME == "macos" ]] && [[ $COMPILER == "clang" ]]; then
     # fix "OMP: Error #15: Initializing libiomp5.dylib, but found libomp.dylib already initialized." (OpenMP library conflict due to conda's MKL)
