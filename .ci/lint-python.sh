ca65937496184afd600ac055b7149ca590166596
James Lamb <jaylamb20@gmail.com>
2024-06-13 15:07:51 -0500

    fix R CMake installation, try to re-enable pre-commit

--- .ci/lint-python.sh
+++ .ci/lint-python.sh
@@ -2,11 +2,9 @@
 
 set -e -E -u -o pipefail
 
-# this can be re-enabled when this is fixed:
-# https://github.com/tox-dev/filelock/issues/337
-# echo "running pre-commit checks"
-# pre-commit run --all-files || exit 1
-# echo "done running pre-commit checks"
+echo "running pre-commit checks"
+pre-commit run --all-files || exit 1
+echo "done running pre-commit checks"
 
 echo "running mypy"
 mypy \
