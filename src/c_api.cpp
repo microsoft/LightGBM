c73e08d00e2b241dfbd6e084c1b0366e9b72cac7
Nikita Titov <nekit94-08@mail.ru>
2024-07-10 17:49:59 +0300

    Update c_api.cpp

--- src/c_api.cpp
+++ src/c_api.cpp
@@ -1464,7 +1464,7 @@ int LGBM_DatasetCreateFromCSR(const void* indptr,
   }
   OMP_INIT_EX();
   #pragma omp parallel for num_threads(OMP_NUM_THREADS()) schedule(static)
-  for (int i = 0; i < nindptr - 1; ++i) {
+  for (int i = 0; i < static_cast<int>(nindptr - 1); ++i) {
     OMP_LOOP_EX_BEGIN();
     const int tid = omp_get_thread_num();
     auto one_row = get_row_fun(i);
