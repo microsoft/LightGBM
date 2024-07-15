592f3f84a225ceb09e893a3c4b0f0110a4490ed7
José Morales <jmoralz92@gmail.com>
2024-07-15 17:49:23 -0600

    check compute only for USE_GPU

--- cmake/Submodules.cmake
+++ cmake/Submodules.cmake
@@ -1,5 +1,8 @@
-function(assert_submodules_initialized)
-    set(submodules compute eigen fast_double_parser fmt)
+function(assert_submodules_initialized use_gpu)
+    set(submodules eigen fast_double_parser fmt)
+    if (use_gpu)
+        list(APPEND submodules compute)
+    endif()
     foreach(submodule ${submodules})
         if(NOT EXISTS "${CMAKE_SOURCE_DIR}/external_libs/${submodule}/CMakeLists.txt")
             message(
