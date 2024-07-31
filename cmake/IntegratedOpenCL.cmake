01555d62189e301a641f210d2d924be7739fd5ae
Nikita Titov <nekit94-08@mail.ru>
2024-07-31 14:08:09 +0300

    try abbreviate-paths

--- cmake/IntegratedOpenCL.cmake
+++ cmake/IntegratedOpenCL.cmake
@@ -185,6 +185,7 @@ ExternalProject_Add(
     cxxflags="${BOOST_FLAGS}"
     architecture=x86
     address-model=64
+    --abbreviate-paths
   INSTALL_COMMAND ""
   # BUILD_BYPRODUCTS is necessary to support 'Ninja' builds.
   # ref:
