2168a303f997e25990e3fe970c79b134f6b09b6e
Nikita Titov <nekit94-08@mail.ru>
2024-07-31 18:32:32 +0300

    Update test_windows.ps1

--- .ci/test_windows.ps1
+++ .ci/test_windows.ps1
@@ -8,7 +8,7 @@ function Check-Output {
 
 $env:CONDA_ENV = "test-env"
 $env:LGB_VER = (Get-Content $env:BUILD_SOURCESDIRECTORY\VERSION.txt).trim()
-$env:PROCESSOR_ARCHITECTURE = "em64t"
+$env:PROCESSOR_ARCHITECTURE = "AMD64"
 # Use custom temp directory to avoid
 # > warning MSB8029: The Intermediate directory or Output directory cannot reside under the Temporary directory
 # > as it could lead to issues with incremental build.
