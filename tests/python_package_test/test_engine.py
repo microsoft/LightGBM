cc6f68865c408f6da77a392f53729d531ffd2e07
shiyu1994 <shiyu_k1994@qq.com>
2024-09-06 10:42:16 +0800

    Update tests/python_package_test/test_engine.py
    Co-authored-by: Nikita Titov <nekit94-08@mail.ru>

--- tests/python_package_test/test_engine.py
+++ tests/python_package_test/test_engine.py
@@ -4530,6 +4530,5 @@ def test_bagging_by_query_in_lambdarank():
     params.update({"bagging_by_query": False, "bagging_fraction": 0.1, "bagging_freq": 1})
     gbm_no_bagging_by_query = lgb.train(params, lgb_train, num_boost_round=50, valid_sets=[lgb_test])
     ndcg_score_no_bagging_by_query = gbm_no_bagging_by_query.best_score["valid_0"]["ndcg@5"]
-    print(ndcg_score_bagging_by_query, ndcg_score, ndcg_score_no_bagging_by_query)
     assert ndcg_score_bagging_by_query >= ndcg_score - 0.1
     assert ndcg_score_no_bagging_by_query >= ndcg_score - 0.1
