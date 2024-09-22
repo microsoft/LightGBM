7a8c6d0b8fbbac1c4288c38c4502ade5319b9643
James Lamb <jaylamb20@gmail.com>
2024-09-22 01:38:41 -0500

    load libgomp.so.1 earlier

--- python-package/lightgbm/__init__.py
+++ python-package/lightgbm/__init__.py
@@ -4,53 +4,10 @@
 Contributors: https://github.com/microsoft/LightGBM/graphs/contributors.
 """
 
-import platform
-
-# gcc's libgomp tries to allocate a small amount of aligned static thread-local storage ("TLS")
-# when it's dynamically loaded.
-#
-# If it's not able to find a block of aligned memory large enough, loading fails like this:
-#
-#   > ../lib/libgomp.so.1: cannot allocate memory in static TLS block
-#
-# On aarch64 Linux, processes and loaded libraries share the same pool of static TLS,
-# which makes such failures more likely on that architecture.
-# (ref: https://bugzilla.redhat.com/show_bug.cgi?id=1722181#c6)
-#
-# Therefore, the later in a process libgomp.so is loaded, the greater the risk that loading
-# it will fail in this way... so lightgbm tries to dlopen() it immediately, before any
-# other imports or computation.
-#
-# This should generally be safe to do ... many other dynamically-loaded libraries have fallbacks
-# that allow successful loading if there isn't sufficient static TLS available.
-#
-# libgomp.so absolutely needing it, by design, makes it a special case
-# (ref: https://gcc.gcc.gnu.narkive.com/vOXMQqLA/failure-to-dlopen-libgomp-due-to-static-tls-data).
-#
-# other references:
-#
-#   * https://github.com/microsoft/LightGBM/pull/6654#issuecomment-2352014275
-#   * https://github.com/microsoft/LightGBM/issues/6509
-#   * https://maskray.me/blog/2021-02-14-all-about-thread-local-storage
-#   * https://bugzilla.redhat.com/show_bug.cgi?id=1722181#c6
-#
-if platform.system().lower() == "linux" and platform.processor().lower() == "aarch64":
-    import ctypes
-
-    try:
-        # this issue seems specific to libgomp, so no need to attempt e.g. libomp or libiomp
-        _ = ctypes.CDLL("libgomp.so.1", ctypes.RTLD_GLOBAL)
-    except:  # noqa: E722
-        # this needs to be try-catched, to handle these situations:
-        #
-        #   * LightGBM built without OpenMP (-DUSE_OPENMP=OFF)
-        #   * non-gcc OpenMP used (e.g. clang/libomp, icc/libiomp)
-        #   * no file "libgomp.so" available to the linker (e.g. maybe only "libgomp.so.1")
-        #
-        pass
-
 from pathlib import Path
 
+# .basic is intentionally loaded as early as possible, to dlopen() lib_lightgbm.{dll,dylib,so}
+# and its dependencies as early as possible
 from .basic import Booster, Dataset, Sequence, register_logger
 from .callback import EarlyStopException, early_stopping, log_evaluation, record_evaluation, reset_parameter
 from .engine import CVBooster, cv, train
