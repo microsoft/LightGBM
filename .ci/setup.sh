1b51e5a356ff473836280d25bfa134d980825874
Nikita Titov <nekit94-08@mail.ru>
2024-10-10 23:06:02 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -4,7 +4,7 @@ set -e -E -u -o pipefail
 
 brew install cmake gcc
 
-git clone --recursive https://github.com/microsoft/LightGBM
+git clone --recursive -b test-install https://github.com/microsoft/LightGBM
 cd LightGBM
 export CXX=g++-14 CC=gcc-14  # replace "7" with version of gcc installed on your machine
 cmake -B build -S . -DBUILD_CPP_TEST=ON
