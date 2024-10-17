0df8685441f203bb81b4950381a0e9a6e956f14c
Nikita Titov <nekit94-08@mail.ru>
2024-10-18 00:28:56 +0300

    Update setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -10,7 +10,7 @@ cmake --version
 
 curl -O -L https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip
 unzip ninja-linux.zip
-ls -lh ./
+ls -lh .
 #sudo ln -sf ./ninja /usr/local/bin/ninja
 #ninja --version
 
