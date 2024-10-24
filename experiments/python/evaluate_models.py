d6a04538843a068553eb9f10a003a68b6f7f3813
jsten07 <j_sten07@uni-muenster.de>
2024-10-24 17:20:11 +0200

    evaluate different model depths

--- experiments/python/evaluate_models.py
+++ experiments/python/evaluate_models.py
@@ -15,13 +15,25 @@ def GetValueFromTXT(filename, key):
 	ret = 0.0
 	for line in input.readlines():
 		if key + '=' in line:
-			ret = float(line.split(key+'=')[-1])
+			if key == 'num_leaves':
+				ret += float(line.split(key+'=')[-1])
+			else:
+				ret = float(line.split(key+'=')[-1])
+		elif key + ': ' in line and key != 'num_leaves':
+			ret = float(line.split(key+': ')[-1][:-2])
 	return ret
 
-accuracies = []
+accuracies_t = []
+accuracies_d = []
 trees = []
-no_features = []
-no_thresholds = []
+max_trees = []
+max_depth = []
+no_features_t = []
+no_thresholds_t = []
+no_features_d = []
+no_thresholds_d = []
+no_leaves_t = []
+no_leaves_d = []
 
 sorted_ids = []
 
@@ -30,16 +42,24 @@ sorted_dir = sorted(os.listdir("../results"), key=lambda x: int(x.split(".")[1])
 print(sorted_dir)
 
 for fn in sorted_dir:
-    if fn.endswith(".out"): 
-        accuracies.append(GetAccuracyFromLightGBM('../results/'+fn, 'auc'))
-    if fn.endswith(".txt"):
-        trees.append(GetValueFromTXT('../results/'+fn, 'Tree'))
-        no_features.append(GetValueFromTXT('../results/'+fn, 'tt_feature_count'))
-        no_thresholds.append(GetValueFromTXT('../results/'+fn, 'tt_threshold_count'))
-    else:
-        continue
+	if fn.endswith("trees.out"):
+		accuracies_t.append(GetAccuracyFromLightGBM('../results/'+fn, 'auc'))
+	if fn.endswith("depth.out"):
+		accuracies_d.append(GetAccuracyFromLightGBM('../results/'+fn, 'auc'))
+	if fn.endswith("trees.txt"):
+		trees.append(GetValueFromTXT('../results/'+fn, 'Tree'))
+		max_trees.append(GetValueFromTXT('../results/'+fn, 'num_iterations'))
+		no_features_t.append(GetValueFromTXT('../results/'+fn, 'tt_feature_count'))
+		no_thresholds_t.append(GetValueFromTXT('../results/'+fn, 'tt_threshold_count'))
+		no_leaves_t.append(GetValueFromTXT('../results/'+fn, 'num_leaves'))
+	if fn.endswith("depth.txt"):
+		max_depth.append(GetValueFromTXT('../results/'+fn, 'max_depth'))
+		no_features_d.append(GetValueFromTXT('../results/'+fn, 'tt_feature_count'))
+		no_thresholds_d.append(GetValueFromTXT('../results/'+fn, 'tt_threshold_count'))
+		no_leaves_d.append(GetValueFromTXT('../results/'+fn, 'num_leaves'))
+	else:
+		continue
 
-print(accuracies)
 print(trees)
 
 # plt.plot(trees, accuracies, label = 'AUC')
@@ -51,22 +71,46 @@ print(trees)
 # plt.show()
 
 
-fig, ax1 = plt.subplots()
+fig1, ax1 = plt.subplots()
 
 color = 'tab:red'
-ax1.set_xlabel('No of trees')
+ax1.set_xlabel('Max trees')
+# TODO: actual no. of trees
 ax1.set_ylabel('AUC')
-ax1.plot(trees, accuracies, color=color)
+ax1.plot(max_trees, accuracies_t, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 
 ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
 
 color = 'tab:blue'
 ax2.set_ylabel('count')  # we already handled the x-label with ax1
-ax2.plot(trees, no_features, label="no. features" )
-ax2.plot(trees, no_thresholds, label="no. thresholds")
+ax2.plot(max_trees, no_features_t, label="no. features" )
+ax2.plot(max_trees, no_thresholds_t, label="no. thresholds")
+ax2.plot(max_trees, no_leaves_t, label="no. leaves")
 # ax2.tick_params(axis='y', labelcolor=color)
 
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig1.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.legend()
+plt.show()
+
+
+fig2, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('Max depth')
+ax1.set_ylabel('AUC')
+ax1.plot(max_depth, accuracies_d, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+
+color = 'tab:blue'
+ax2.set_ylabel('count')  # we already handled the x-label with ax1
+ax2.plot(max_depth, no_features_d, label="no. features" )
+ax2.plot(max_depth, no_thresholds_d, label="no. thresholds")
+ax2.plot(max_depth, no_leaves_d, label="no. leaves")
+# ax2.tick_params(axis='y', labelcolor=color)
+
+fig2.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.legend()
 plt.show()
--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if cmake -B build -S . -DUSE_CUDA=1 -DUSE_DEBUG=ON; then
+if cmake -B build -S . -DUSE_CUDA=0 -DUSE_DEBUG=ON; then
     if cmake --build build -j4; then
         echo "build complete"
     else
@@ -16,7 +16,15 @@ cd experiments || exit
 # "../lightgbm" config=train.conf num_trees=20 > train.output
 END=40
 for i in $(seq 5 5 $END); do 
-    if "../lightgbm" config=train.conf num_trees=$i output_model=results/model.$i.txt > results/train.$i.out; then
+    if "../lightgbm" config=train.conf num_trees=$i output_model=results/model.$i.trees.txt > results/train.$i.trees.out; then
+        echo "Training model $i complete"
+    else
+        echo "Training model $i failed"
+        exit 1  
+    fi
+done
+for i in $(seq 2 2 10); do 
+    if "../lightgbm" config=train.conf num_trees=10 max_depth=$i output_model=results/model.$i.depth.txt > results/train.$i.depth.out; then
         echo "Training model $i complete"
     else
         echo "Training model $i failed"
