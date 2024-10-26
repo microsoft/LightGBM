18f872f4858ace546fd911247f9730377ebead87
Nikita Titov <nekit94-08@mail.ru>
2024-10-26 21:14:42 +0300

    update

--- .ci/lint-powershell.ps1
+++ .ci/lint-powershell.ps1
@@ -1,10 +1,3 @@
 param([Parameter(Mandatory=$true)][string]$scripts_dir)
 
-Invoke-ScriptAnalyzer -Path $scripts_dir -Severity Warning -Recurse -Outvariable issues -EnableExit
-$errors = $issues.Where({$_.Severity -eq 'Error'})
-$warnings = $issues.Where({$_.Severity -eq 'Warning'})
-if ($errors) {
-    Write-Error "There were $($errors.Count) errors and $($warnings.Count) warnings total." -ErrorAction Stop
-} else {
-    Write-Output "There were $($errors.Count) errors and $($warnings.Count) warnings total."
-}
+Invoke-ScriptAnalyzer -Path $scripts_dir -Severity Warning -Recurse -EnableExit
