0dbe8c3e29ab01fd455d027e870e3279794ccc53
Nikita Titov <nekit94-08@mail.ru>
2024-10-26 21:25:53 +0300

    update

--- .ci/lint-powershell.ps1
+++
@@ -1,3 +0,0 @@
-param([Parameter(Mandatory=$true)][string]$scripts_dir)
-
-Invoke-ScriptAnalyzer -Path $scripts_dir -Severity Warning -Recurse -EnableExit
--- .ci/test.sh
+++ .ci/test.sh
@@ -112,7 +112,7 @@ if [[ $TASK == "lint" ]]; then
     # source activate "${CONDA_ENV}"
     pwsh -command "Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -SkipPublisherCheck"
     echo "Linting PowerShell code"
-    pwsh -command "Invoke-ScriptAnalyzer -Path "${BUILD_DIRECTORY}"/.ci -Severity Warning -Recurse -EnableExit" || exit 1
+    pwsh -command "Invoke-ScriptAnalyzer -Path "${BUILD_DIRECTORY}"/.ci -Severity Error -Recurse -EnableExit" || exit 1
     # echo "Linting Python code"
     # bash ./.ci/lint-python.sh || exit 1
     # echo "Linting R code"
