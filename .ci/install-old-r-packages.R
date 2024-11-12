8fd01c193ff1e73c58fb013198a79e56d293d089
James Lamb <jaylamb20@gmail.com>
2024-11-11 23:54:12 -0600

    try even more packages

--- .ci/install-old-r-packages.R
+++ .ci/install-old-r-packages.R
@@ -4,27 +4,36 @@
 #    as of the last R 3.6 release.
 #
 
-cran_archive <- "https://cran.r-project.org/src/contrib/Archive"
+.install_packages <- function(packages) {
+    install.packages(
+        pkgs = paste("https://cran.r-project.org/src/contrib/Archive", packages, sep = "/")
+        , repos = NULL
+    )
+}
 
-packages <- c(
+.install_packages(c(
     "brio/brio_1.1.4.tar.gz"
     , "cli/cli_3.6.2.tar.gz"
+    , "crayon/crayon_1.5.2.tar.gz"
     , "diffobj/diffobj_0.3.4.tar.gz"
+    , "digest/digest_0.6.36.tar.gz"
+    , "fansi/fansi_1.0.5.tar.gz"
     , "evaluate/evaluate_0.23.tar.gz"
     , "glue/glue_1.7.0.tar.gz"
+    , "jsonlite/jsonlite_1.8.8.tar.gz"
     , "lattice/lattice_0.20-41.tar.gz"
     , "lifecycle/lifecycle_1.0.3.tar.gz"
+    , "magrittr/magrittr_2.0.2.tar.gz"
+    , "pkgconfig/pkgconfig_2.0.2.tar.gz"
+    , "pkgload/pkgload_1.3.4.tar.gz"
     , "pillar/pillar_1.8.1.tar.gz"
+    , "processx/processx_3.8.3.tar.gz"
     , "R6/R6_2.5.0.tar.gz"
     , "rematch2/rematch2_2.1.1.tar.gz"
     , "rlang/rlang_1.1.3.tar.gz"
     , "testthat/testthat_3.2.1.tar.gz"
     , "tibble/tibble_3.2.0.tar.gz"
-    , "waldo/waldo_0.5.3.tar.gz"
+    , "utf8/utf8_1.2.3.tar.gz"
     , "vctrs/vctrs_0.6.4.tar.gz"
-)
-
-install.packages(
-    pkgs = paste(cran_archive, packages, sep = "/")
-    , repos = NULL
-)
+    , "waldo/waldo_0.5.3.tar.gz"
+))
