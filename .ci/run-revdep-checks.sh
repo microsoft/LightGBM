a43cb3a1fd5b97367180dc4c713325845e3fa7d9
James Lamb <jaylamb20@gmail.com>
2024-12-01 23:01:24 -0600

    try pre-installing to speed this up

--- .ci/run-revdep-checks.sh
+++ .ci/run-revdep-checks.sh
@@ -8,9 +8,4 @@ mkdir -p "${CHECKS_OUTPUT_DIR}"
 sh build-cran-package.sh --no-build-vignettes
 mv ./lightgbm_*.tar.gz "${CHECKS_OUTPUT_DIR}/"
 
-# pre-install all of the dependencies... tools::check_packages_in_dir()
-# is hard-coded to compile them all from source
-# (https://github.com/wch/r-source/blob/594b842678e932088b16ec0cd3c39714a141eed9/src/library/tools/R/checktools.R#L295)
-Rscript ./.ci/download-r-revdeps.R
-
 Rscript ./.ci/run-r-revdepchecks.R "${CHECKS_OUTPUT_DIR}"
