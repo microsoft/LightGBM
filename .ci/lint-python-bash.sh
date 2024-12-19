4adf207fdaed541a9d444c399bd06732bdea17a7
Nikita Titov <nekit94-08@mail.ru>
2024-12-20 00:36:11 +0300

    disable some rules instead of skipping yamllint hook

--- .ci/lint-python-bash.sh
+++ .ci/lint-python-bash.sh
@@ -3,7 +3,7 @@
 set -e -E -u -o pipefail
 
 echo "running pre-commit checks"
-SKIP=yamllint pre-commit run --all-files || exit 1
+pre-commit run --all-files || exit 1
 echo "done running pre-commit checks"
 
 echo "running mypy"
