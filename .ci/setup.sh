1b7950dc1412071932d2c587d17e13bd8a7ac3dc
Nikita Titov <nekit94-08@mail.ru>
2024-12-20 19:45:09 +0300

    dev

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -11,14 +11,14 @@ export SKBUILD_LOGGING_LEVEL="INFO"
 # brew install libomp open-mpi
 # export CXX=g++-14 CC=gcc-14
 
-# sudo apt-get update
-# sudo apt-get install --no-install-recommends -y \
-#     libboost1.74-dev \
-#     libboost-filesystem1.74-dev
-    # ocl-icd-opencl-dev
+sudo apt-get update
+sudo apt-get install --no-install-recommends -y \
+    libboost1.74-dev \
+    libboost-filesystem1.74-dev \
+    ocl-icd-opencl-dev
     # pocl-opencl-icd
 
-mkdir -p /etc/OpenCL/vendors && echo "libnvidia-opencl.so.1" > /etc/OpenCL/vendors/nvidia.icd
+mkdir -p /etc/OpenCL/vendors && echo "/usr/lib/x86_64-linux-gnu/libOpenCL.so" > /etc/OpenCL/vendors/nvidia.icd
 
 curl \
     -sL \
@@ -30,7 +30,7 @@ conda update -q -y conda
 
 
 pip install pytest numpy pandas scipy scikit-learn psutil cloudpickle
-pip install -v --no-cache-dir lightgbm --no-binary lightgbm --config-settings=cmake.define.USE_CUDA=ON
+pip install -v --no-binary lightgbm lightgbm --config-settings=cmake.define.USE_GPU=ON
 
 cd "${BUILD_DIRECTORY}"
 pytest ./tests/python_package_test || exit 1
