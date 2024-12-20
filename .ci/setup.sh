b33262b5d9eca418862c3978d9e117bf2434e6c9
Nikita Titov <nekit94-08@mail.ru>
2024-12-21 02:28:32 +0300

    dev

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -30,7 +30,7 @@ conda update -q -y conda
 
 
 pip install pytest numpy pandas scipy scikit-learn psutil cloudpickle
-pip install lightgbm -v --no-binary lightgbm
+pip install lightgbm --no-binary lightgbm --config-settings=cmake.define.USE_HOMEBREW_FALLBACK=OFF
 
 cd "${BUILD_DIRECTORY}"
 pytest ./tests/python_package_test || exit 1
