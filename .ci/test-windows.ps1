677c2ed2bee389fa5d677c86d52cf2b1f56862f8
Nikita Titov <nekit94-08@mail.ru>
2024-12-20 20:10:02 +0300

    dev

--- .ci/test-windows.ps1
+++ .ci/test-windows.ps1
@@ -15,29 +15,10 @@ Remove-Item $env:TMPDIR -Force -Recurse -ErrorAction Ignore
 conda init powershell
 conda activate
 conda config --set always_yes yes --set changeps1 no
-conda update -q -y conda "python=$env:PYTHON_VERSION[build=*cpython]"
-
-if ($env:PYTHON_VERSION -eq "3.7") {
-    $env:CONDA_REQUIREMENT_FILE = "$env:BUILD_SOURCESDIRECTORY/.ci/conda-envs/ci-core-py37.txt"
-} elseif ($env:PYTHON_VERSION -eq "3.8") {
-    $env:CONDA_REQUIREMENT_FILE = "$env:BUILD_SOURCESDIRECTORY/.ci/conda-envs/ci-core-py38.txt"
-} else {
-    $env:CONDA_REQUIREMENT_FILE = "$env:BUILD_SOURCESDIRECTORY/.ci/conda-envs/ci-core.txt"
-}
-
-$condaParams = @(
-    "-y",
-    "-n", "$env:CONDA_ENV",
-    "--file", "$env:CONDA_REQUIREMENT_FILE",
-    "python=$env:PYTHON_VERSION[build=*cpython]"
-)
-conda create @condaParams ; Assert-Output $?
-
-if ($env:TASK -ne "bdist") {
-    conda activate $env:CONDA_ENV
-}
 
 Set-Location "$env:BUILD_SOURCESDIRECTORY"
+
+pip install pytest numpy pandas scipy scikit-learn psutil cloudpickle
 pip install -v lightgbm
 
 $tests = "$env:BUILD_SOURCESDIRECTORY/tests/python_package_test"
