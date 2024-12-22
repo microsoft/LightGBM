73334ca3699deaf3e700ccd1d44fa9016cf96a54
Nikita Titov <nekit94-08@mail.ru>
2024-12-22 23:54:40 +0300

    Update build-python.sh

--- build-python.sh
+++ build-python.sh
@@ -152,7 +152,7 @@ while [ $# -gt 0 ]; do
     --mingw)
         export CMAKE_GENERATOR='MinGW Makefiles'
         # ref: https://stackoverflow.com/a/45104058/3986677
-        BUILD_ARGS="${BUILD_ARGS} --config-setting=cmake.define.CMAKE_SH=CMAKE_SH-NOTFOUND --config-setting=cmake.args=\"-GMinGW Makefiles\""
+        BUILD_ARGS="${BUILD_ARGS} --config-setting=cmake.define.CMAKE_SH=CMAKE_SH-NOTFOUND --config-setting=cmake.args='${CMAKE_GENERATOR}'"
         ;;
     --mpi)
         BUILD_ARGS="${BUILD_ARGS} --config-setting=cmake.define.USE_MPI=ON"
