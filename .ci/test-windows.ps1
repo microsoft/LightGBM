d8e0454385f422c4265c303498437a24d3574342
Nikita Titov <nekit94-08@mail.ru>
2024-12-22 22:49:02 +0300

    dev

--- .ci/test-windows.ps1
+++ .ci/test-windows.ps1
@@ -3,6 +3,7 @@ Remove-Item $env:TMPDIR -Force -Recurse -ErrorAction Ignore
 [Void][System.IO.Directory]::CreateDirectory($env:TMPDIR)
 
 
+echo 44
 
 $env:MINICONDA="C:\Miniconda3-x64"
 $env:PATH="$env:MINICONDA;$env:MINICONDA\Scripts;$env:PATH"
@@ -12,15 +13,21 @@ $env:BUILD_SOURCESDIRECTORY="$env:APPVEYOR_BUILD_FOLDER"
 conda init powershell
 conda activate
 conda config --set always_yes yes --set changeps1 no
-python.exe -m pip install --upgrade pip
+conda config --remove channels defaults
+conda config --add channels nodefaults
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda init powershell
 
-# conda config --remove channels defaults ; conda config --add channels nodefaults ; conda config --add channels conda-forge ; conda config --set channel_priority strict ; conda init sh'
+python.exe -m pip install --upgrade pip
 
 Set-Location "$env:BUILD_SOURCESDIRECTORY"
 
 pip --version
 pip install pytest numpy pandas scipy scikit-learn psutil cloudpickle
-pip install lightgbm -v --no-binary lightgbm --config-settings=cmake.define.__BUILD_FOR_PYTHON=true --config-settings=cmake.args="-GVisual Studio 14 2015" --config-settings=cmake.args="-AWin32"
+
+New-Item -Path "$env:BUILD_SOURCESDIRECTORY/.ci" -ItemType Directory
+curl --output $env:BUILD_SOURCESDIRECTORY/.ci/setup.sh "https://raw.githubusercontent.com/microsoft/LightGBM/refs/heads/ci/test/.ci/setup.sh"
 
 
 $tests = "$env:BUILD_SOURCESDIRECTORY/tests/python_package_test"
