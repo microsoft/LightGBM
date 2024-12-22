bca3db57502d761b16a226c3b82161a6881b73eb
Nikita Titov <nekit94-08@mail.ru>
2024-12-22 23:04:55 +0300

    dev

--- .ci/test.sh
+++ .ci/test.sh
@@ -9,6 +9,9 @@ pip install pytest numpy pandas scipy scikit-learn psutil cloudpickle
 
 
 
+git clone --recursive https://github.com/microsoft/LightGBM.git
+cd LightGBM
+# export CXX=g++-14 CC=gcc-14  # macOS users, if you decided to compile with gcc, don't forget to specify compilers
 sh ./build-python.sh install
 
 
