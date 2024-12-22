37e6d8898cd71339cfd43076cfa33179693d0dbd
Nikita Titov <nekit94-08@mail.ru>
2024-12-22 22:40:08 +0300

    dev

--- .ci/test-windows.ps1
+++ .ci/test-windows.ps1
@@ -2,13 +2,19 @@ $env:TMPDIR = "$env:USERPROFILE\tmp"
 Remove-Item $env:TMPDIR -Force -Recurse -ErrorAction Ignore
 [Void][System.IO.Directory]::CreateDirectory($env:TMPDIR)
 
+
+
+$env:MINICONDA="C:\Miniconda3-x64"
+$env:PATH="$env:MINICONDA;$env:MINICONDA\Scripts;$env:PATH"
+$env:BUILD_SOURCESDIRECTORY="$env:APPVEYOR_BUILD_FOLDER"
+
 # setup for Python
 conda init powershell
 conda activate
 conda config --set always_yes yes --set changeps1 no
 python.exe -m pip install --upgrade pip
 
-# export MINICONDA="C:\Miniconda3-x64" ; export PATH="$MINICONDA;$MINICONDA\bin;$PATH" ; export BUILD_SOURCESDIRECTORY="$APPVEYOR_BUILD_FOLDER" ; conda config --remove channels defaults ; conda config --add channels nodefaults ; conda config --add channels conda-forge ; conda config --set channel_priority strict ; conda init sh'
+# conda config --remove channels defaults ; conda config --add channels nodefaults ; conda config --add channels conda-forge ; conda config --set channel_priority strict ; conda init sh'
 
 Set-Location "$env:BUILD_SOURCESDIRECTORY"
 
