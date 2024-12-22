ddb52b98414bd9bc4f4bd3529d046d424e6008dc
Nikita Titov <nekit94-08@mail.ru>
2024-12-23 02:13:30 +0300

    Update build-python.sh

--- build-python.sh
+++ build-python.sh
@@ -342,7 +342,7 @@ if test "${BUILD_SDIST}" = true; then
     python -m build \
         --sdist \
         --outdir ../dist \
-        ${BUILD_ARGS} \
+        $(echo "${BUILD_ARGS}" | sed -e 's@#@ @g'  | xargs -n 1) \
         .
 fi
 
@@ -353,7 +353,7 @@ if test "${BUILD_WHEEL}" = true; then
     python -m build \
         --wheel \
         --outdir ../dist \
-        ${BUILD_ARGS} \
+        $(echo "${BUILD_ARGS}" | sed -e 's@#@ @g'  | xargs -n 1) \
         .
 fi
 
