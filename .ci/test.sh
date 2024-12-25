f839398f225da0db427a76e58196f6fd0fc0fae0
Nikita Titov <nekit94-08@mail.ru>
2024-12-25 07:42:55 +0300

    dev

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 109
+echo 110
 
 export SKBUILD_LOGGING_LEVEL="INFO"
 
@@ -8,10 +8,9 @@ pip --version
 pip install pytest numpy pandas scipy scikit-learn psutil cloudpickle
 
 
-# git clone --recursive -b docs/install-py https://github.com/microsoft/LightGBM.git
-# cd LightGBM
-# sh ./build-python.sh install --mingw
-pip install lightgbm -v --no-binary lightgbm --config-settings=cmake.args="-AWin32" --config-setting=cmake.define.__BUILD_FOR_PYTHON=ON
+git clone --recursive -b docs/install-py https://github.com/microsoft/LightGBM.git
+cd LightGBM
+sh ./build-python.sh install --bit32
 
 
 tests="$BUILD_SOURCESDIRECTORY/tests/python_package_test"
