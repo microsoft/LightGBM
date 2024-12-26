34b83a98055265e88ba3d919690bc02440b112e9
Nikita Titov <nekit94-08@mail.ru>
2024-12-26 05:52:54 +0300

    dev

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo 215
+echo 216
 
 ARCH=$(uname -m)
 
@@ -35,7 +35,7 @@ pip install pytest numpy pandas scipy scikit-learn psutil cloudpickle
 cd $GITHUB_WORKSPACE/.ci
 git clone --recursive -b docs/install-py https://github.com/microsoft/LightGBM.git
 cd LightGBM
-sh ./build-python.sh install --debug
+sh ./build-python.sh install --sanitizers="address;leak"
 
 
 pytest $GITHUB_WORKSPACE/tests/python_package_test || exit 1
