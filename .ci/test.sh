4a6a2d73f2960fea066e87dbcf643c544b656946
Nikita Titov <nekit94-08@mail.ru>
2024-12-26 04:37:00 +0300

    dev

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 203
+echo 204
 
 export SKBUILD_LOGGING_LEVEL="INFO"
 
@@ -10,7 +10,7 @@ pip install pytest numpy pandas scipy scikit-learn psutil cloudpickle
 
 git clone --recursive -b docs/install-py https://github.com/microsoft/LightGBM.git
 cd LightGBM
-sh ./build-python.sh install --mingw
+sh ./build-python.sh install --bit32
 
 
 tests="$BUILD_SOURCESDIRECTORY/tests/python_package_test"
