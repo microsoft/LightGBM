66f5fe59ca85bdb8f71929d5f8e158e5c116afea
José Morales <jmoralz92@gmail.com>
2025-01-02 17:49:19 -0600

    [python-package] do not copy column-major numpy arrays when creating Dataset from list of arrays

--- python-package/lightgbm/basic.py
+++ python-package/lightgbm/basic.py
@@ -2342,6 +2342,7 @@ class Dataset:
             ptr_data = (ctypes.POINTER(ctypes.c_double) * len(mats))()
         else:
             ptr_data = (ctypes.POINTER(ctypes.c_float) * len(mats))()
+        layouts = (ctypes.c_int * len(mats))()
 
         holders = []
         type_ptr_data = -1
@@ -2355,15 +2356,13 @@ class Dataset:
 
             nrow[i] = mat.shape[0]
 
-            if mat.dtype == np.float32 or mat.dtype == np.float64:
-                mats[i] = np.asarray(mat.reshape(mat.size), dtype=mat.dtype)
-            else:  # change non-float data to float data, need to copy
-                mats[i] = np.array(mat.reshape(mat.size), dtype=np.float32)
+            mat, layout = _np2d_to_np1d(mat)
 
-            chunk_ptr_data, chunk_type_ptr_data, holder = _c_float_array(mats[i])
+            chunk_ptr_data, chunk_type_ptr_data, holder = _c_float_array(mat)
             if type_ptr_data != -1 and chunk_type_ptr_data != type_ptr_data:
                 raise ValueError("Input chunks must have same type")
             ptr_data[i] = chunk_ptr_data
+            layouts[i] = layout
             type_ptr_data = chunk_type_ptr_data
             holders.append(holder)
 
@@ -2375,7 +2374,7 @@ class Dataset:
                 ctypes.c_int(type_ptr_data),
                 nrow.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
                 ctypes.c_int32(ncol),
-                ctypes.c_int(_C_API_IS_ROW_MAJOR),
+                layouts,
                 _c_str(params_str),
                 ref_dataset,
                 ctypes.byref(self._handle),
