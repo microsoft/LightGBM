41d2e279f8a9102047b3aa09288aea85703709a2
nprokoptsev <nprokoptsev@gmail.com>
2025-02-15 15:59:17 -0800

    fix

--- python-package/lightgbm/basic.py
+++ python-package/lightgbm/basic.py
@@ -2020,8 +2020,7 @@ class Dataset:
                 "use_missing",
                 "weight_column",
                 "position_column",
-                "zero_as_missing",
-                "position_column"
+                "zero_as_missing"
             )
             return {k: v for k, v in self.params.items() if k in dataset_params}
         else:
