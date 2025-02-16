41b81b05c1b939c9f91634d50efec7fac9ed11b6
nprokoptsev <nprokoptsev@gmail.com>
2025-02-16 13:58:32 -0800

    fix ruff errors

--- python-package/lightgbm/basic.py
+++ python-package/lightgbm/basic.py
@@ -2043,7 +2043,7 @@ class Dataset:
                 "use_missing",
                 "weight_column",
                 "position_column",
-                "zero_as_missing"
+                "zero_as_missing",
             )
             return {k: v for k, v in self.params.items() if k in dataset_params}
         else:
