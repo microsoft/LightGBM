3b6afde494b07c538aa4d8b75172650f1f3afbd0
EunHo Lee <ehlee8276@gmail.com>
2025-05-16 13:50:35 -0700

    Add TypeError message to function:test_list_to_1d_numpy
    `match` error message argument for the pytest.raises for `test_list_to_1d_numpy`

--- tests/python_package_test/test_basic.py
+++ tests/python_package_test/test_basic.py
@@ -685,7 +685,7 @@ def test_list_to_1d_numpy(collection, dtype, rng):
             lgb.basic._list_to_1d_numpy(y, dtype=np.float32, name="list")
         return
     elif isinstance(y, list) and isinstance(y[0], list):
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError, match="expected 1D array"):
             lgb.basic._list_to_1d_numpy(y, dtype=np.float32, name="list")
         return
     elif isinstance(y, pd_Series) and y.dtype == object:
