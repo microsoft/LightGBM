b7ceb3fe783b3f12a4f6f43be6f025b611c100b5
James Lamb <jaylamb20@gmail.com>
2025-07-28 15:12:24 -0500

    [ci] combine pip installs in Python test scripts (#6981)
    * [ci] combine pip installs in Python test scripts
    
    * remove most CI
    
    * Revert "remove most CI"
    
    This reverts commit 8c0c1ec7b6e4e9f69190965abbf58b760efc8c6d.

--- .ci/test-python-oldest.sh
+++ .ci/test-python-oldest.sh
@@ -2,27 +2,12 @@
 
 set -e -E -u -o pipefail
 
-# oldest versions of dependencies published after
-# minimum supported Python version's first release,
-# for which there are wheels compatible with the
-# python:{version} image
-#
-# see https://devguide.python.org/versions/
-#
-echo "installing lightgbm's dependencies"
+echo "installing lightgbm and its dependencies"
 pip install \
-  'cffi==1.15.1' \
-  'numpy==1.19.0' \
-  'pandas==1.1.3' \
-  'pyarrow==6.0.1' \
-  'scikit-learn==0.24.2' \
-  'scipy==1.6.0' \
-|| exit 1
-echo "done installing lightgbm's dependencies"
-
-echo "installing lightgbm"
-pip install --no-deps dist/*.whl || exit 1
-echo "done installing lightgbm"
+    --prefer-binary \
+    --upgrade \
+    -r ./.ci/pip-envs/requirements-oldest.txt \
+    dist/*.whl
 
 echo "installed package versions:"
 pip freeze
