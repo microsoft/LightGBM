0d96917e9e2a36772ba4d0e3f2eabe9f093640c9
James Lamb <jaylamb20@gmail.com>
2025-08-20 23:07:24 -0500

    [ci] separate configs and scripts for linting and docs CI

---
+++ .ci/lint-all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e -E -u -o pipefail
+
+pwsh -command "Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -SkipPublisherCheck"
+echo "Linting PowerShell code"
+pwsh -file ./.ci/lint-powershell.ps1 || exit 1
+
+conda create -q -y -n test-env \
+    "python=3.13[build=*_cp*]" \
+    'biome>=1.9.3' \
+    'cpplint>=1.6.0' \
+    'matplotlib-base>=3.9.1' \
+    'mypy>=1.11.1' \
+    'pre-commit>=3.8.0' \
+    'pyarrow-core>=17.0' \
+    'scikit-learn>=1.5.2' \
+    'r-lintr>=3.1.2'
+
+# shellcheck disable=SC1091
+source activate test-env
+
+echo "Linting Python and bash code"
+bash ./.ci/run-pre-commit-mypy.sh || exit 1
+
+echo "Linting R code"
+Rscript ./.ci/lint-r-code.R "$(pwd)" || exit 1
+
+echo "Linting C++ code"
+bash ./.ci/lint-cpp.sh || exit 1
+
+echo "Linting JavaScript code"
+bash ./.ci/lint-js.sh || exit 1
