3bae70395ec815b2540e6c3a9cecd9b57cc8c39f
Nikita Titov <nekit94-08@mail.ru>
2025-09-09 16:23:14 +0300

    Update test-docs.sh

--- .ci/test-docs.sh
+++ .ci/test-docs.sh
@@ -16,5 +16,5 @@ make -C docs html || exit 1
 if [[ $TASK == "check-links" ]]; then
     # check docs for broken links
     conda install -y -n test-env 'lychee>=0.20.1'
-    lychee --config=./docs/.lychee.toml --dump-inputs --exclude-path="(^|/)docs/.*\.rst" "**/*.rst" "**/*.md" "./R-package/**/*.rd" "./docs/_build/html/*.html" || exit 1
+    lychee --config=./docs/.lychee.toml --dump-inputs --exclude-path="(^|/)docs/.*\.rst" "**/*.rst" "**/*.md" "./R-package/**/*.Rd" "./docs/_build/html/*.html" || exit 1
 fi
