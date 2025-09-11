fdaad7d8671785307d2a343161fa81b9cebe526b
Nikita Titov <nekit94-08@mail.ru>
2025-09-11 12:50:22 +0300

    dev

--- .ci/test-docs.sh
+++ .ci/test-docs.sh
@@ -18,15 +18,14 @@ if [[ $TASK == "check-links" ]]; then
     conda install -y -n test-env 'lychee>=0.20.1'
     # to see all gained files add "--dump-inputs" flag
     # to see all gained links add "--dump" flag
-    lychee_args=(
-        "--config=./docs/.lychee.toml"
-        "--exclude-path=(^|/)docs/.*\.rst"
-        "--github-token=${SECRETS_WORKFLOW}"
-        "--"
-        "**/*.rst"
-        "**/*.md"
-        "./R-package/**/*.Rd"
-        "./docs/_build/html/*.html"
-    )
-    lychee "${lychee_args[@]}" || exit 1
+    lychee \
+        "--config=./docs/.lychee.toml" \
+        "--exclude-path=(^|/)docs/.*\.rst" \
+        "--github-token=${SECRETS_WORKFLOW}" \
+        "--" \
+        "**/*.rst" \
+        "**/*.md" \
+        "./R-package/**/*.Rd" \
+        "./docs/_build/html/*.html" \
+    || exit 1
 fi
