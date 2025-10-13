37e64e347d888b1ad06e75223656f2f8e0653a91
Nikita Titov <nekit94-08@mail.ru>
2025-10-14 01:55:03 +0300

    dev

--- cmake/modules/FindLibR.cmake
+++ cmake/modules/FindLibR.cmake
@@ -240,8 +240,9 @@ endif()
 include(FindPackageHandleStandardArgs)
 
 if(WIN32 AND MSVC)
-  # lint_cmake: -package/stdargs
+# lint_cmake: -package/stdargs
   find_package_handle_standard_args(
+    # lint_cmake: -package/stdargs
     LibR DEFAULT_MSG
     LIBR_HOME
     LIBR_EXECUTABLE
