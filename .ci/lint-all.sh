2c6b74afb438f5db7c7fcf383bc14364e950bcb2
Nikita Titov <nekit94-08@mail.ru>
2025-10-18 07:42:51 +0300

    [ci] move js and json linter `biome` to pre-commit (#7059)

--- .ci/lint-all.sh
+++ .ci/lint-all.sh
@@ -8,7 +8,6 @@ pwsh -file ./.ci/lint-powershell.ps1 || exit 1
 
 conda create -q -y -n test-env \
     "python=3.13[build=*_cp*]" \
-    'biome>=1.9.3' \
     'pre-commit>=3.8.0' \
     'r-lintr>=3.1.2'
 
@@ -20,6 +19,3 @@ pre-commit run --all-files || exit 1
 
 echo "Linting R code"
 Rscript ./.ci/lint-r-code.R "$(pwd)" || exit 1
-
-echo "Linting JavaScript code"
-bash ./.ci/lint-js.sh || exit 1
