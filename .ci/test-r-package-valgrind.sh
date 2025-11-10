079ce4d8a7e15ac7c6b3fe1d7a722358e676ba43
James Lamb <jaylamb20@gmail.com>
2025-11-10 01:45:09 -0600

    short-circuit

--- .ci/test-r-package-valgrind.sh
+++ .ci/test-r-package-valgrind.sh
@@ -2,6 +2,10 @@
 
 set -e -E -u -o pipefail
 
+# TODO: remove before merging
+echo "[DEBUG] failing with exit code 1"
+exit 1
+
 RDscriptvalgrind ./.ci/install-r-deps.R --test || exit 1
 sh build-cran-package.sh \
   --r-executable=RDvalgrind \
