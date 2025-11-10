5029294ffcd39b3284cdee88494290de616e937b
James Lamb <jaylamb20@gmail.com>
2025-11-10 00:36:34 -0600

    restore all CI

--- .ci/test-r-package-valgrind.sh
+++ .ci/test-r-package-valgrind.sh
@@ -2,10 +2,6 @@
 
 set -e -E -u -o pipefail
 
-# TODO: remove before merging
-echo "[DEBUG] exiting with status 1"
-exit 1
-
 RDscriptvalgrind ./.ci/install-r-deps.R --test || exit 1
 sh build-cran-package.sh \
   --r-executable=RDvalgrind \
