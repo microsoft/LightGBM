9f8cc6ebf2a92330b450631095cd7ec102088f27
James Lamb <jaylamb20@gmail.com>
2025-11-11 00:59:49 -0600

    remove debugging code

--- .ci/test-r-package-valgrind.sh
+++ .ci/test-r-package-valgrind.sh
@@ -2,10 +2,6 @@
 
 set -e -E -u -o pipefail
 
-# TODO: remove before merging
-echo "[DEBUG] failing with exit code 0"
-exit 0
-
 RDscriptvalgrind ./.ci/install-r-deps.R --test || exit 1
 sh build-cran-package.sh \
   --r-executable=RDvalgrind \
