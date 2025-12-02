7e3bb787f9d51a34882df0a150ec9d3eacbaffdd
James Lamb <jaylamb20@gmail.com>
2025-12-01 23:41:27 -0600

    produce sdist artifacts

--- .ci/download-artifacts.sh
+++ .ci/download-artifacts.sh
@@ -14,7 +14,6 @@
 #         - commit.txt
 #         - lib_lightgbm.so
 #         - lightgbm-4.6.0-py3-none-manylinux_2_28_x86_64.whl
-#         - lightgbm-4.6.0.tar.gz
 #         - LightGBM-complete_source_code_tar_gz.tar.gz
 #         - LightGBM.4.6.0.nupkg
 #
