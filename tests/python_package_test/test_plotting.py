e3d52709fd6a79ec92607bfe5c9e74b3f77472c2
daguirre11 <96506367+daguirre11@users.noreply.github.com>
2026-02-23 16:14:37 -0800

    [python-package] Added test for plot_metric ylim (#7164)

--- tests/python_package_test/test_plotting.py
+++ tests/python_package_test/test_plotting.py
@@ -603,3 +603,13 @@ def test_plot_metrics(params, breast_cancer_split, train_data):
 
     with pytest.raises(TypeError, match="xlim must be a tuple of 2 elements."):
         lgb.plot_metric(evals_result0, metric="binary_logloss", xlim="not a tuple")
+
+    ax6 = lgb.plot_metric(evals_result0, metric="binary_logloss", ylim=(0, 15), title=None, xlabel=None, ylabel=None)
+    assert isinstance(ax6, matplotlib.axes.Axes)
+    assert ax6.get_title() == ""
+    assert ax6.get_xlabel() == ""
+    assert ax6.get_ylabel() == ""
+    assert ax6.get_ylim() == (0, 15)
+
+    with pytest.raises(TypeError, match="ylim must be a tuple of 2 elements."):
+        lgb.plot_metric(evals_result0, metric="binary_logloss", ylim="not a tuple")
