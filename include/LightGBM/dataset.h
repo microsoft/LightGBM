48d21a05b7a6203a95ea0ad08b5277d8d4dc0d7d
venti <1308199824@qq.com>
2026-05-30 14:27:12 +0800

    fix: clarify error message when whitespace in feature names causes collision
    Closes #7230
    
    When feature names differ only by whitespace (space vs underscore),
    LightGBM normalizes spaces to underscores during preprocessing,
    which can create name collisions. The previous error message did not
    mention this preprocessing step, leaving users confused.
    
    New message explains that whitespace was replaced with '_' and
    advises users to ensure feature names are unique and do not contain
    whitespace.

--- include/LightGBM/dataset.h
+++ include/LightGBM/dataset.h
@@ -915,7 +915,7 @@ class Dataset {
         std::replace(feature_name.begin(), feature_name.end(), ' ', '_');
       }
       if (feature_name_set.count(feature_name) > 0) {
-        Log::Fatal("Feature (%s) appears more than one time.", feature_name.c_str());
+        Log::Fatal("Feature (%s) appears more than one time. After preprocessing (replacing whitespace with '_'), multiple features resolved to this name. Ensure feature names are unique and do not contain whitespace.", feature_name.c_str());
       }
       feature_name_set.insert(feature_name);
     }
