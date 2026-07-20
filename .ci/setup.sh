cce744d6dfa2bf36cb0c42979058a6cec78afc38
Mohammed Sheikh <Mohammed.Sheikh1@ibm.com>
2026-07-20 12:52:00 +0530

    add -u flag to Miniforge installer to handle existing installations

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -185,7 +185,7 @@ if [[ "${TASK}" != "cpp-tests" ]] && [[ "${TASK}" != "r-package" ]] && [[ "${TAS
             -sL \
             -o miniforge.sh \
             "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-${ARCH}.sh"
-        sh miniforge.sh -b -p "${CONDA}"
+        sh miniforge.sh -b -u -p "${CONDA}"
     fi
     conda config --set always_yes yes --set changeps1 no
     conda update -q -y conda
