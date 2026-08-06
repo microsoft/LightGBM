61ed9a2b4fddf1b77cc3eb6d6a6c50b4a43e5491
aminemanai2003 <amine.manai@esprit.tn>
2026-08-06 12:45:17 +0100

    [ci] [R-package] replace version in docs build

--- .ci/build-docs.R
+++ .ci/build-docs.R
@@ -1,19 +1,46 @@
-loadNamespace("pkgdown")
-loadNamespace("roxygen2")
+build_docs <- function() {
+    loadNamespace("pkgdown")
+    loadNamespace("roxygen2")
 
-roxygen2::roxygenize(
-    "R-package/"
-    , load = "installed"
-)
+    description_path <- file.path("R-package", "DESCRIPTION")
+    description_contents <- readLines(description_path)
+    on.exit(writeLines(description_contents, description_path), add = TRUE)
 
-pkgdown::build_site(
-    "R-package/"
-    , lazy = FALSE
-    , install = FALSE
-    , devel = FALSE
-    , examples = TRUE
-    , run_dont_run = TRUE
-    , seed = 42L
-    , preview = FALSE
-    , new_process = TRUE
-)
+    lgb_version <- readLines("VERSION.txt", n = 1L)
+    lgb_version <- gsub("rc", "-", lgb_version, fixed = TRUE)
+    docs_description <- gsub(
+        "~~VERSION~~"
+        , lgb_version
+        , description_contents
+        , fixed = TRUE
+    )
+    docs_description <- gsub(
+        "~~DATE~~"
+        , as.character(Sys.Date())
+        , docs_description
+        , fixed = TRUE
+    )
+    if (any(grepl("~~(VERSION|DATE)~~", docs_description))) {
+        stop("Failed to replace placeholders in R-package/DESCRIPTION")
+    }
+    writeLines(docs_description, description_path)
+
+    roxygen2::roxygenize(
+        "R-package/"
+        , load = "installed"
+    )
+
+    pkgdown::build_site(
+        "R-package/"
+        , lazy = FALSE
+        , install = FALSE
+        , devel = FALSE
+        , examples = TRUE
+        , run_dont_run = TRUE
+        , seed = 42L
+        , preview = FALSE
+        , new_process = TRUE
+    )
+}
+
+build_docs()
