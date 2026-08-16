798ee9a3c19e28df3164f83f59844b05c293b766
siqi.he <siqi.he@upstart.com>
2026-08-16 20:42:45 +0000

    fix illegal access in ShufflePrefixSumExclusive

--- include/LightGBM/cuda/cuda_algorithms.hpp
+++ include/LightGBM/cuda/cuda_algorithms.hpp
@@ -98,7 +98,7 @@ __device__ __forceinline__ T ShufflePrefixSumExclusive(T value, T* shared_mem_bu
   if (threadIdx.x == 0) {
     exclusive_result = 0;
   } else if (threadIdx.x % warpSize == 0) {
-    exclusive_result = shared_mem_buffer[warpLane - 1];
+    exclusive_result = shared_mem_buffer[warpID - 1];
   }
   return exclusive_result;
 }
